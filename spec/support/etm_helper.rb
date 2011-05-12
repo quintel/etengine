f99009926f9c83cec54e13a1291fc7653c7fffe1
Paolo Zaccagnini <paozac@gmail.com>
2011-05-12 11:46:08 +0200

    removed unused test helpers

--- spec/support/etm_helper.rb
+++ spec/support/etm_helper.rb
@@ -1,17 +1,7 @@
-module EtmHelper
-  
+module EtmHelper  
   # Stubs methods that are needed to just render the etm layout
   def stub_etm_layout_methods!
     ApplicationController.stub!(:ensure_valid_browser)
     Current.stub!(:graph)
   end
-  
-  def logged_in_user
-    controller.assigns["user_session"].andand.user
-  end
-    
-  def log_in(user)
-    controller.stub!(:current_user).and_return(user)
-  end
-  
 end
