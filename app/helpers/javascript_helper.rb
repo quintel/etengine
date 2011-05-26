f547c428cd569fd118b79539e47fbad31330e9d1
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-05-26 14:03:57 +0200

    cleaned some helpers

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -14,23 +14,7 @@ module JavascriptHelper
       param.nil? ? 'null' : param.inspect
     end.join(', ')
   end
-  
-  def init_etm_javascript_framework
-    javascript_tag "var ETM = new MainController();"
-  end
 
-  def create_input_element(input_element)
-    input_element_id = dom_id(input_element)
-    "ETM.inputElementsController.addInputElement(new InputElement(%s.input_element), {'element':$('#%s')});" % [input_element.to_json, input_element_id]
-  end
-  
-  def update_input_element(input_element)
-    script = "var a = ETM.inputElementsController.getInputElementById('%s');" % input_element.id
-    script << "if(a) a.updateAttributes(%s.input_element); else console.log('Updating input element with id: %s, but not on page');" % [input_element.to_json, input_element.id]
-    script.html_safe
-  end
-  
-  
   def update_constraint(constraint)
     ''
   end
