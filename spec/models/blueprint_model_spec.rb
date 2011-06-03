4b042e9d66988ebe8c9886a7c56dda6e1fe9fedf
Paolo Zaccagnini <paozac@gmail.com>
2011-06-03 10:54:03 +0200

    more tests

--- spec/models/blueprint_model_spec.rb
+++ spec/models/blueprint_model_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper'
 
 describe BlueprintModel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :blueprints }
+  it { should validate_presence_of :title}
 end
-
-# == Schema Information
-#
-# Table name: blueprint_models
-#
-#  id         :integer(4)      not null, primary key
-#  title      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
