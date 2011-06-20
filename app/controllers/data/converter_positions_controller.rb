c06acab4a5bd862621982618522e750d646f960b
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-06-20 14:27:35 +0200

    assign blueprint_layout when saving new converter_position. fixes #22

--- app/controllers/data/converter_positions_controller.rb
+++ app/controllers/data/converter_positions_controller.rb
@@ -6,7 +6,7 @@ class Data::ConverterPositionsController  < Data::DataController
         if bcp = ConverterPosition.find_by_converter_id(converter_id)
           bcp.update_attributes attributes
         else
-          ConverterPosition.create attributes.merge(:converter_id => converter_id)
+          ConverterPosition.create attributes.merge(:converter_id => converter_id, :blueprint_layout_id => params[:blueprint_layout_id])
         end
       end
     end
