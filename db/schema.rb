26bc05e0b1007d275622bef0ea0d2a6c4f079e04
Paolo Zaccagnini <paozac@gmail.com>
2011-07-06 15:40:18 +0200

    gqueries: add units, remove bad gqueries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707054537) do
+ActiveRecord::Schema.define(:version => 20110707133607) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
