32257929c35b4201bebd5a2a100bc4f1e324197e
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-07 12:50:46 +0700

    created gqueries for peak load check

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707043107) do
+ActiveRecord::Schema.define(:version => 20110707054537) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
