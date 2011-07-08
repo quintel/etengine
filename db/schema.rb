8c41d8f41429bfa812007f295465b4d3bd422797
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-08 11:08:01 +0700

    rename peak load gquery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707133608) do
+ActiveRecord::Schema.define(:version => 20110708040526) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
