5e6403034d4b6dd2f390c41c0f0a104118e3df77
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-08 11:43:05 +0700

    fix gqueries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110708040526) do
+ActiveRecord::Schema.define(:version => 20110708043815) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
