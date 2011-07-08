dfef15a329a5af6ecb45837d65c134896c6c7d3c
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-08 12:07:22 +0700

    limit to 500 gquereis for now

--- db/migrate/20110708043815_fix_gqueries.rb
+++ db/migrate/20110708043815_fix_gqueries.rb
@@ -2,11 +2,11 @@ class FixGqueries < ActiveRecord::Migration
   def self.up
     [144,652,653,655,656,657].each do |id|
       gquery = Gquery.find(id)
-      gquery.query.gsub!("of_heat", "of_steam_hot_water")
-      gquery.query.gsub!("of_steam_hotwater", "of_steam_hot_water")
+      gquery.query = gquery.query.gsub("of_imported_heat", "of_imported_steam_hot_water")
+      gquery.query = gquery.query.gsub("of_steam_hotwater", "of_steam_hot_water")
       gquery.save
     end
-    #Gquery.find(679).destroy
+    Gquery.find(679).destroy
     Gquery.find(514).destroy
     Gquery.find(515).destroy
   end
