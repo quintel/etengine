0c04100c8803fff778f57058edc2443efc487205
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-18 18:25:54 +0700

    removed Rails.logger and added calculate future benchmark

--- app/models/gql/update/command_base.rb
+++ app/models/gql/update/command_base.rb
@@ -14,7 +14,6 @@ class CommandBase
       Rails.logger.warn(@errors.values.join('\n'))
       return nil
     end
-    Rails.logger.warn("updating: #{object} ##{@attr_name} with #{value}")
     object[@attr_name] = value
   end
 
