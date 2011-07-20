06d37ad4cdc12ae4bea88582d83a29cf714982fb
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-07-20 14:07:44 +0700

    Fix admin.interface.js

--- app/javascripts/init/admin.interface.js
+++ app/javascripts/init/admin.interface.js
@@ -2,34 +2,38 @@ $(document).ready(function() {
 
   $('input#search').keyup(function(ev) {
     var search = $(this).val();
-    $.cookie($(this).attr('data-search_scope'), search);
-    var condition = $.map(search.split(' '), function(el) {
-      if (el != "")
-      return "[data-search*="+el+"]";
+    var cookie_key = $(this).attr('data-search_scope');
+
+    if (cookie_key != undefined) {
+
+      $.cookie(cookie_key, search);
+
+      var condition = $.map( search.split(' '), function(el) {
+        if (el != "") { return "[data-search*="+el+"]"; }
+        $('.tree li').hide();
+        $('.tree li'+condition).show();
       }).join('');
-      console.log(condition);
-      $('.tree li').hide();
-      $('.tree li'+condition).show();
-    })
-    var stored_search = $.cookie($('input#search').attr('data-search_scope'));
-    $('input#search').val(stored_search).keyup();
+
+      var stored_search = $.cookie($('input#search').attr('data-search_scope'));
+      $('input#search').val(stored_search).keyup();
 
     
-    $('.gql_operator').click(function(ev) {
-      window.open()
-    })
+      $('.gql_operator').click(function(ev) {
+        window.open()
+      })
     
 
-    $('.gql_operator').mouseover(function(ev) {
-      // E.g.: VALUE( ... )
-      // operator: VALUE
-      // statement: ( ...)
-      var statement = $(this).next().text();
-      var query = $(this).text() + statement;
-      $(this).next().css('background', '#666');
-    })      
-    $('.gql_operator').mouseout(function(ev) {
-      $(this).next().css('background', 'none');
-    })
-
+      $('.gql_operator').mouseover(function(ev) {
+        // E.g.: VALUE( ... )
+        // operator: VALUE
+        // statement: ( ...)
+        var statement = $(this).next().text();
+        var query = $(this).text() + statement;
+        $(this).next().css('background', '#666');
+      })      
+      $('.gql_operator').mouseout(function(ev) {
+        $(this).next().css('background', 'none');
+      })
+    }
+  })
 })
