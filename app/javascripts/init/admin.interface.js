b34603adf6a75cc1abbe1de10ad4600b199e2a2b
Paolo Zaccagnini <paozac@gmail.com>
2011-07-27 11:11:42 +0200

    admin-data sprocket cleanup

--- app/javascripts/init/admin.interface.js
+++ app/javascripts/init/admin.interface.js
@@ -5,35 +5,32 @@ $(document).ready(function() {
     var cookie_key = $(this).attr('data-search_scope');
 
     if (cookie_key != undefined) {
-
       $.cookie(cookie_key, search);
-
-      var condition = $.map( search.split(' '), function(el) {
-        if (el != "") { return "[data-search*="+el+"]"; }
-        $('.tree li').hide();
-        $('.tree li'+condition).show();
-      }).join('');
-
-      var stored_search = $.cookie($('input#search').attr('data-search_scope'));
-      $('input#search').val(stored_search).keyup();
-
-    
-      $('.gql_operator').click(function(ev) {
-        window.open()
-      })
-    
-
-      $('.gql_operator').mouseover(function(ev) {
-        // E.g.: VALUE( ... )
-        // operator: VALUE
-        // statement: ( ...)
-        var statement = $(this).next().text();
-        var query = $(this).text() + statement;
-        $(this).next().css('background', '#666');
-      })      
-      $('.gql_operator').mouseout(function(ev) {
-        $(this).next().css('background', 'none');
-      })
     }
-  })
-})
+    
+    var condition = $.map( search.split(' '), function(el) {
+      if (el != "") { return "[data-search*="+el+"]"; }
+    }).join('');
+    
+    $('.tree li').hide();
+    $('.tree li'+condition).show();
+  });
+      
+  var stored_search = $.cookie($('input#search').attr('data-search_scope'));
+  $('input#search').val(stored_search).keyup();
+
+  $('.gql_operator').click(function(ev) {
+    window.open(); // ? - PZ Wed 27 Jul 2011 11:10:13 CEST
+  });
+
+  $('.gql_operator').mouseover(function(ev) {
+    var statement = $(this).next().text();
+    var query = $(this).text() + statement;
+    $(this).next().css('background', '#666');
+  });
+  
+  $('.gql_operator').mouseout(function(ev) {
+    $(this).next().css('background', 'none');
+  });
+
+});
