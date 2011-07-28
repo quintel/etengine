1e98fcfc1f42207ea4f8f52bce694cf058a2d80a
Paolo Zaccagnini <paozac@gmail.com>
2011-07-28 11:38:51 +0200

    migration to update inputs hardcoded gql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110728094209) do
+ActiveRecord::Schema.define(:version => 20110728094210) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
