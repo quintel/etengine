2de94ba2c5fc97e809cb70a2f6ccddaf2e1f7944
Paolo Zaccagnini <paozac@gmail.com>
2011-08-02 14:54:12 +0200

    whitespace

--- app/models/qernel/converter_api/network.rb
+++ app/models/qernel/converter_api/network.rb
@@ -1,39 +1,38 @@
 class Qernel::ConverterApi
   def heating_peak_load_delta
-	dataset_fetch_handle_nil(:heating_peak_load_delta) do
-	  simult_we *
-	  electricity_input_conversion *
-	  typical_electricity_production_capacity *
-	  share_of_heating_demand_households_energetic
-	end
+    dataset_fetch_handle_nil(:heating_peak_load_delta) do
+      simult_we *
+      electricity_input_conversion *
+      typical_electricity_production_capacity *
+      share_of_heating_demand_households_energetic
+    end
   end
   attributes_required_for :heating_peak_load_delta, [  
-	:simult_we,
-	:electricity_input_conversion,
-	:typical_electricity_production_capacity,
-	:share_of_heating_demand_households_energetic
+    :simult_we,
+    :electricity_input_conversion,
+    :typical_electricity_production_capacity,
+    :share_of_heating_demand_households_energetic
   ]
 
   ##
   # TODO: change delta_in_capacity_in_mj_s to delta_in_capacity_in_mw in queries
   def delta_in_capacity_in_mw
-	dataset_fetch_handle_nil(:delta_in_capacity_in_mw) do
-		(number_of_units - peak_load_units_present) * typical_electricity_production_capacity 
-	end
+    dataset_fetch_handle_nil(:delta_in_capacity_in_mw) do
+      (number_of_units - peak_load_units_present) * typical_electricity_production_capacity 
+    end
   end
   attributes_required_for :delta_in_capacity_in_mw, [
-	:number_of_units,
-	:peak_load_units_present,
-	:typical_electricity_production_capacity
+    :number_of_units,
+    :peak_load_units_present,
+    :typical_electricity_production_capacity
   ]
-
-alias delta_in_capacity_in_mj_s delta_in_capacity_in_mw
+  alias delta_in_capacity_in_mj_s delta_in_capacity_in_mw
 
 
   def peak_load_units_delta_for_mv_hv
-	dataset_fetch_handle_nil(:peak_load_units_delta_for_mv_hv) do
-	  (number_of_units - peak_load_units_present)
-	end
+    dataset_fetch_handle_nil(:peak_load_units_delta_for_mv_hv) do
+      (number_of_units - peak_load_units_present)
+    end
   end
   attributes_required_for :peak_load_units_delta_for_mv_hv, [:number_of_units, :peak_load_units_present]
 
@@ -44,14 +43,14 @@ alias delta_in_capacity_in_mj_s delta_in_capacity_in_mw
   alias peak_load_heat_units_delta_for_mv_hv peak_load_units_delta_for_mv_hv
 
   def peak_load_capacity_delta_for_mv_hv
-	dataset_fetch_handle_nil(:peak_load_capacity_delta_for_mv_hv) do
-	  (simult_we * peak_load_units_delta_for_mv_hv * typical_electricity_production_capacity)
-	end
+    dataset_fetch_handle_nil(:peak_load_capacity_delta_for_mv_hv) do
+      (simult_we * peak_load_units_delta_for_mv_hv * typical_electricity_production_capacity)
+    end
   end
   attributes_required_for :peak_load_capacity_delta_for_mv_hv, [
-	:simult_we,
-	:peak_load_units_delta_for_mv_hv,
-	:typical_electricity_production_capacity
+    :simult_we,
+    :peak_load_units_delta_for_mv_hv,
+    :typical_electricity_production_capacity
   ]
 
 end
