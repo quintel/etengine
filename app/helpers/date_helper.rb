e2e7acc5fac51bbd262b5f98ffcffcf3f61942b2
Paolo Zaccagnini <paozac@gmail.com>
2011-08-11 14:50:36 +0200

    show year in data/gqueries. closes #45

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -16,10 +16,4 @@ module DateHelper
   def format_date_nicely_veryshort(date)
     return h(date.strftime("%d-%m-%Y"))
   end
-
-   def format_date_with_time(date)
-  	day = date.strftime("%d")
-  	day = day.last if day.first == '0'
-  	return h(day + " " + Date::MONTHNAMES[date.strftime("%m").to_i] + " " + date.strftime("%H:%M"))
-  end
 end
