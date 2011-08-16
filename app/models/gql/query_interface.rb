d2aaf0bf9ee852a96d44397b7871d5c6739a7298
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-16 21:48:56 +0700

    renaming names

--- app/models/gql/query_interface.rb
+++ app/models/gql/query_interface.rb
@@ -15,7 +15,7 @@ class QueryInterface
 
   # The implementation of #query, #subquery is done in GqueryBase,
   # so that we can extend it with caching strategies defined in
-  # e.g. GqueryCache and GqueryMemoizedSubqueries.
+  # e.g. QueryCache and GqueryMemoizedSubqueries.
   include Base
   include GraphApi
 
@@ -38,17 +38,17 @@ class QueryInterface
   # The order of includes is important.
   #
   # calling #subquery:
-  # GqueryMemoizedSubqueries#subquery
+  # QueryMemoization#subquery
   #   returns subquery when memoized, if not:
   #   (we can memoize results of future graph, because after the request, the memoization is lost)
-  # GqueryCache#subquery
+  # QueryCache#subquery
   #   returns memcached result of subquery for present graph, if not memcached:
   #   (we don't want to persistently cache the future graph as the results always change)
   # GqueryBase#subquery
   #   Executes the subquery
   #
-  include GqueryCache # optional
-  include MemoizedSubqueries # optional
+  include QueryCache # optional
+  include QueryMemoization # optional
 
 
 end
