f54ee27ba529baaf171ad37268578da88103f4c1
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-17 11:42:47 +0700

    TRASH: comments

--- app/models/qernel/graph_api.rb
+++ app/models/qernel/graph_api.rb
@@ -32,13 +32,6 @@ class GraphApi
   # Still needed here, for updating converters
   #
   def potential_roof_pv_production
-    # PRODUCT(
-    #  DIVIDE(
-    #    AREA(roof_surface_available_pv),
-    #    V(local_solar_pv_grid_connected_energy_energetic;land_use_per_unit)
-    #  ),
-    #  V(local_solar_pv_grid_connected_energy_energetic;typical_electricity_production_per_unit)
-    # )    
     c = graph.converter(:local_solar_pv_grid_connected_energy_energetic).query
     
     roof_surface        = area.roof_surface_available_pv
