8687dd1899726aa89f90bacef791a5b2fff8c2d3
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-19 09:59:58 +0700

    clean tests

--- spec/models/gql/gql_spec.rb
+++ spec/models/gql/gql_spec.rb
@@ -53,49 +53,61 @@ describe Gql do
       @q.query("V(rgt; demand)").should == 120.0
     end
 
-    it "should UPDATE" do
-      @q.query("UPDATE(V(lft), demand, 130)")
-      @q.query("V(lft; demand)").should == 130.0
+    describe "UPDATE with static values" do
+      it "should UPDATE" do
+        @q.query("UPDATE(V(lft), demand, 130)")
+        @q.query("V(lft; demand)").should == 130.0
+      end
+
+      it "should UPDATE multiple" do
+        @q.query("UPDATE(V(lft, rgt), demand, 130)")
+        @q.query("V(lft; demand)").should == 130.0
+        @q.query("V(rgt; demand)").should == 130.0
+      end
     end
 
-    it "should UPDATE multiple" do
-      @q.query("UPDATE(V(lft, rgt), demand, 130)")
-      @q.query("V(lft; demand)").should == 130.0
-      @q.query("V(rgt; demand)").should == 130.0
-    end
-
-    it "should UPDATE referencing an attribute from UPDATE_OBJECT()" do
-      @q.query("UPDATE(V(lft), demand, PRODUCT(V(UPDATE_OBJECT();demand),3))")
-      @q.query("V(lft; demand)").should == 300.0
-    end
-
-    it "should UPDATE mulitple referencing an attribute from UPDATE_OBJECT()" do
-      @q.query("UPDATE(V(lft, rgt), demand, PRODUCT(V(UPDATE_OBJECT();demand),3))")
-      @q.query("V(lft; demand)").should == 300.0
-      @q.query("V(rgt; demand)").should == 360.0
-    end
-
-    it "should UPDATE mulitple referencing an attribute from UPDATE_COLLECTION()" do
-      # updates the demand with how many objects there are to be update
-      @q.query("UPDATE(V(lft, rgt), demand, COUNT(UPDATE_COLLECTION()))")
-      @q.query("V(lft; demand)").should == 2
-      @q.query("V(rgt; demand)").should == 2
-    end
-
-    it "should UPDATE mulitple using EACH" do
-      @q.query("EACH( UPDATE(V(lft),demand,1), UPDATE(V(rgt),demand,2) )")
-      @q.query("V(lft; demand)").should == 1.0
-      @q.query("V(rgt; demand)").should == 2.0
+    describe "UPDATE with dynamic USER_INPUT()" do
+      it "should UPDATE using USER_INPUT()" do
+        @q.query("UPDATE(V(lft),demand,USER_INPUT())", 300)
+        @q.query("V(lft; demand)").should == 300.0
+      end
     end
 
 
-    it "should UPDATE using USER_INPUT()" do
-      @q.query("UPDATE(V(lft),demand,USER_INPUT())", 300)
-      @q.query("V(lft; demand)").should == 300.0
+    describe "UPDATE_OBJECT" do
+      it "should UPDATE referencing an attribute from UPDATE_OBJECT()" do
+        @q.query("UPDATE(V(lft), demand, PRODUCT(V(UPDATE_OBJECT();demand),3))")
+        @q.query("V(lft; demand)").should == 300.0
+      end
+
+      it "should UPDATE mulitple referencing an attribute from UPDATE_OBJECT()" do
+        @q.query("UPDATE(V(lft, rgt), demand, PRODUCT(V(UPDATE_OBJECT();demand),3))")
+        @q.query("V(lft; demand)").should == 300.0
+        @q.query("V(rgt; demand)").should == 360.0
+      end
     end
+
+    describe "UPDATE_COLLECTION" do
+      it "should UPDATE mulitple referencing an attribute from UPDATE_COLLECTION()" do
+        # updates the demand with how many objects there are to be update
+        @q.query("UPDATE(V(lft, rgt), demand, COUNT(UPDATE_COLLECTION()))")
+        @q.query("V(lft; demand)").should == 2
+        @q.query("V(rgt; demand)").should == 2
+      end
+    end
+
+    describe "EACH" do
+      it "should UPDATE mulitple commands defined in EACH" do
+        @q.query("EACH( UPDATE(V(lft),demand,1), UPDATE(V(rgt),demand,2) )")
+        @q.query("V(lft; demand)").should == 1.0
+        @q.query("V(rgt; demand)").should == 2.0
+      end
+    end
+
 
 
   end
+
 end
 
 end
