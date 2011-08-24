5233b440173bf2e54f3207a47cab7a2923c622e8
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-24 14:48:12 +0700

    migrating and disabling v2 for decrease_rate

--- db/migrate/20110824043703_migrate_decrease_rate.rb
+++ db/migrate/20110824043703_migrate_decrease_rate.rb
@@ -9,9 +9,14 @@ class MigrateDecreaseRate < ActiveRecord::Migration
       input.save!
     end
 
+
     Input.where(:attr_name => 'decrease_total').each do |input|
       keys = input.keys.split('_AND_').map(&:strip).compact.uniq.join(',')
-      query = "UPDATE(V(#{keys}), preset_demand, NEG(USER_INPUT()))"
+      query = if input.factor != 100
+        "UPDATE(V(#{keys}), preset_demand, NEG(DIVIDE(USER_INPUT(),V(#{input.factor/100}))))"
+      else
+        "UPDATE(V(#{keys}), preset_demand, NEG(USER_INPUT()))"
+      end
 
       input.v1_legacy_unit = '%'
       input.query = query
