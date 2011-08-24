fbda12c059313de0f58dacd979ec2523dfd3c6fe
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-24 11:36:24 +0700

    Migrating growth_rate inputs

---
+++ db/migrate/20110824033613_migrate_growth_rate_inputs.rb
@@ -0,0 +1,19 @@
+class MigrateGrowthRateInputs < ActiveRecord::Migration
+  def self.up
+    Input.where(:attr_name => 'growth_rate').each do |input|
+      keys = input.keys.split('_AND_').map(&:strip).compact.uniq.join(',')
+      query = "UPDATE(V(#{keys}), preset_demand, USER_INPUT())"
+
+      input.v1_legacy_unit = '%y'
+      input.query = query
+      input.save!
+    end
+  end
+
+  def self.down
+    Input.where(:attr_name => 'growth_rate').each do |input|
+      input.v1_legacy_unit = nil
+      input.update_attribute :query, nil
+    end
+  end
+end
