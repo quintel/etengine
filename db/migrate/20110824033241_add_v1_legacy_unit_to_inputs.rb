79699ecdd302456d921fffbbbbe06f04226ca4f4
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-25 13:38:37 +0700

    trying to fix v1_legacy_unit, Input.reset_column_information

--- db/migrate/20110824033241_add_v1_legacy_unit_to_inputs.rb
+++ db/migrate/20110824033241_add_v1_legacy_unit_to_inputs.rb
@@ -1,6 +1,7 @@
 class AddV1LegacyUnitToInputs < ActiveRecord::Migration
   def self.up
     add_column :inputs, :v1_legacy_unit, :string
+    Input.reset_column_information
   end
 
   def self.down
