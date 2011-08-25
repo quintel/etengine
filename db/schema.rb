2ab96931472d853d1531824de8bf0ce34fe86cc4
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-08-25 16:02:10 +0700

    migrate _conversion_(conversion|growthrate) inputs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110824074331) do
+ActiveRecord::Schema.define(:version => 20110825084427) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
