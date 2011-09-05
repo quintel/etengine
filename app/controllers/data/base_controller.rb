e55f9914ae25f6b5b6825497c1ff628a6d2f6930
Paolo Zaccagnini <paozac@gmail.com>
2011-09-05 13:55:10 +0200

    minor fixes, removed useless test

--- app/controllers/data/base_controller.rb
+++ app/controllers/data/base_controller.rb
@@ -7,18 +7,19 @@ class Data::BaseController < ApplicationController
     Rails.cache.clear
     system("touch tmp/restart.txt")
 
-    redirect_to data_converters_url(
+    redirect_to data_root_path(
       :blueprint_id => params[:blueprint_id],
       :region_code  => params[:region_code])
   end
-  
+
   def redirect
     redirect_to data_root_path(
       :blueprint_id => params[:blueprint_id],
-      :region_code  => params[:region_code])    
+      :region_code  => params[:region_code])
   end
 
   protected
+
     def find_graph
       blueprint_id = params[:blueprint_id] ||= 'latest'
       region_code  = params[:region_code] ||= 'nl'
@@ -28,13 +29,13 @@ class Data::BaseController < ApplicationController
         Current.scenario = @api_scenario
         @graph = Current.graph
 
-      elsif region_code and blueprint_id
+      else
         @graph = Graph.latest_from_country(region_code)
         # We have to assign the gql to manually Current
         # DEBT: this is probablye not needed anymore. instead assign Current.graph = @graph
         Current.gql    = @graph.gql
       end
-      @blueprint     = @graph.blueprint
-      @dataset       = @graph.dataset
+      @blueprint = @graph.blueprint
+      @dataset   = @graph.dataset
     end
 end
