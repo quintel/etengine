80841ffeba4aa2b31a177ffa2f67df123cece004
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-09-27 15:02:28 +0200

    Revert f1c54200afa7c9be0a1a08fe6d413fd19b8467bf^..HEAD

--- app/models/energy_balance_group.rb
+++ app/models/energy_balance_group.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: energy_balance_groups
-#
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class EnergyBalanceGroup < ActiveRecord::Base
   has_many :converters
   
