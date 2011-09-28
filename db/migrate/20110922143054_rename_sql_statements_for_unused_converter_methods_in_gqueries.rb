93273a5369321e92011fc306fcbd05954bef8988
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-09-28 16:16:46 +0200

    fix rogue migration

--- db/migrate/20110922143054_rename_sql_statements_for_unused_converter_methods_in_gqueries.rb
+++ db/migrate/20110922143054_rename_sql_statements_for_unused_converter_methods_in_gqueries.rb
@@ -1,20 +1,25 @@
 class RenameSqlStatementsForUnusedConverterMethodsInGqueries < ActiveRecord::Migration
   def self.up
     Gquery.all.each do |gquery|
-      gquery.query.gsub!(/typical_capacity_gross_in_mj_s/,        "network_capacity_available_in_mw")
-      gquery.query.gsub!(/typical_capacity_effective_in_mj_s/,    "network_capacity_used_in_mw")
-      gquery.query.gsub!(/overnight_investment_ex_co2_per_mj_s/,  "network_expansion_costs_in_euro_per_mw")
-      gquery.query.gsub!(/cost_om_fixed_per_mj/,                  "costs_per_mj")
+      q = gquery.query
+      q = q.gsub(/typical_capacity_gross_in_mj_s/,        "network_capacity_available_in_mw")
+      q = q.gsub(/typical_capacity_effective_in_mj_s/,    "network_capacity_used_in_mw")
+      q = q.gsub(/overnight_investment_ex_co2_per_mj_s/,  "network_expansion_costs_in_euro_per_mw")
+      q = q.gsub(/cost_om_fixed_per_mj/,                  "costs_per_mj")
+      gquery.query = q
+
       gquery.save
-    end
+    end; nil
   end
 
   def self.down
     Gquery.all.each do |gquery|
-      gquery.query.gsub!(/network_capacity_available_in_mw/,        "typical_capacity_gross_in_mj_s")
-      gquery.query.gsub!(/network_capacity_used_in_mw/,             "typical_capacity_effective_in_mj_s")
-      gquery.query.gsub!(/network_expansion_costs_in_euro_per_mw/,  "overnight_investment_ex_co2_per_mj_s")
-      gquery.query.gsub!(/costs_per_mj/,                            "cost_om_fixed_per_mj")
+      q = gquery.query
+      q = q.gsub!(/network_capacity_available_in_mw/,        "typical_capacity_gross_in_mj_s")
+      q = q.gsub!(/network_capacity_used_in_mw/,             "typical_capacity_effective_in_mj_s")
+      q = q.gsub!(/network_expansion_costs_in_euro_per_mw/,  "overnight_investment_ex_co2_per_mj_s")
+      q = q.gsub!(/costs_per_mj/,                            "cost_om_fixed_per_mj")
+      gquery.query = q
       gquery.save
     end
   end
