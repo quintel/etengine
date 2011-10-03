07cc24f69263b5780a4856e32e8e3cb67a7471c3
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-10-03 14:36:19 +0200

    etsource. import, export

--- app/models/etsource.rb
+++ app/models/etsource.rb
@@ -1,7 +1,41 @@
 module Etsource
   ETSOURCE_DIR = 'etsource'
+
+  class Commit
+    attr_reader :commit
+
+    def initialize(commit)
+      @git = Git.open('etsource')
+      @commit = @git.gcommit(commit)
+      @git.checkout(commit)
+    end
+
+    def import!
+      Gquery.transaction do 
+        GqlTestCases.new.import!
+        Gqueries.new.import!
+      end
+    end
+  end
   
+  class GqlTestCases
+    def import!
+      GqlTestCase.delete_all
+      import
+    end
+    
+    def import
+      base_dir = "#{ETSOURCE_DIR}/test_suite"
+      Dir.glob("#{base_dir}/**/*.js").each do |f|
+        key = f.split('/').last.split('.').first
+        GqlTestCase.create(:name => key, :instruction => File.read(f))
+      end
+    end
+  end
+
   class Gqueries
+    VARIABLE_PREFIX = '-'
+    
     def import!
       Gquery.transaction do
         Gquery.delete_all
@@ -19,7 +53,7 @@ module Etsource
         path = [base_dir, group].compact.join('/')
 
         FileUtils.mkdir_p(path)
-        File.open("#{path}/#{gquery.key}.yml", 'w') do |f|
+        File.open("#{path}/#{gquery.key}.txt", 'w') do |f|
           f << to_file(gquery)
         end
       end
@@ -30,7 +64,7 @@ module Etsource
       groups = GqueryGroup.all.inject({}) {|hsh,g| hsh.merge group_key(g) => g}
       
       gqueries = []
-      Dir.glob("#{base_dir}/**/*.yml").each do |f|
+      Dir.glob("#{base_dir}/**/*.txt").each do |f|
         group_key, rest = f.gsub(base_dir+"/", '').split('/')
         gquery = from_file(f)
         groups[group_key] ||= GqueryGroup.create(:group_key => group_key)
@@ -40,14 +74,15 @@ module Etsource
       Gquery.import gqueries
     end
 
-  protected
+  #protected
     def to_file(gquery)
-      commented_description = "# #{gquery.description.to_s.strip.gsub("\n", "\n# ")}\n"
+      commented_description = "# #{gquery.description.to_s.strip.gsub("\n", "\n# ")}\n\n"
       unit = gquery.unit.to_s.downcase.strip.gsub(' ', '_')
       unit = 'boolean' if unit.include?('true')
       
       out = commented_description
-      out += "# unit: #{unit} \n\n"
+      out += "#{VARIABLE_PREFIX} deprecated_key = #{gquery.deprecated_key}\n" if gquery.deprecated_key.present?
+      out += "#{VARIABLE_PREFIX} unit = #{unit}\n\n"
       out += gquery.query
       out
     end
@@ -56,17 +91,31 @@ module Etsource
       key = f.split('/').last.split('.').first
       txt = File.read(f)
       
-      comment_lines = txt.lines.select{|l| l.match(/^#/)}
-      query_lines   = txt.lines.reject{|l| l.match(/^#/)}
+      comment_lines  = txt.lines.select{|l| l.match(/^#/)}
+      variable_lines = txt.lines.select{|l| l.match(/^#{VARIABLE_PREFIX}/)}
+      query_lines    = txt.lines.reject{|l| l.match(/^[#{VARIABLE_PREFIX}#]/)}
       
-      # the unit is defined inside the comment-block:
+      # the unit and deprecated_key (optional) is defined inside the comment-block:
+      #   # deprecated_key: foo
       #   # unit: kg
-      unit = comment_lines.last.match(/#\s*unit\:\s*(.+)\n/).captures.first.strip rescue 'undefined'
-      
-      description = comment_lines[0...-1].map{|l| l[1..-1].strip }.join("\n")
+      variables = variable_lines.inject({}) do |hsh, l|
+        begin
+          k,v = l.match(/-\s*([\w_]+)\s+=\s+(.+)\n/).captures
+          hsh.merge k.strip => v.strip
+        rescue => e
+          hsh
+        end
+      end
+      description = comment_lines.map{|l| l[1..-1].strip }.join("\n")
       query = query_lines.join("").strip
       
-      Gquery.new(:key => key, :description => description, :query => query, :unit => unit)
+      Gquery.new(
+        :key => key, 
+        :description => description, 
+        :query => query, 
+        :unit => variables['unit'], 
+        :deprecated_key => variables['deprecated_key']
+      )
     end
     
     def group_key(g)
