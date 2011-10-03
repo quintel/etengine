9ebd1649d8d31e595d5a10321e5ccd95c345b885
Paolo Zaccagnini <paozac@gmail.com>
2011-10-03 17:02:24 +0200

    gquery active_resource: pass deprecated_key, too

--- app/controllers/api/gqueries_controller.rb
+++ app/controllers/api/gqueries_controller.rb
@@ -2,7 +2,6 @@ class Api::GqueriesController < ApplicationController
   respond_to :xml
 
   def index
-    respond_with(@gqueries = Gquery.select("`id`, `key`").all)
+    respond_with(@gqueries = Gquery.select("`id`, `key`, `deprecated_key`").all)
   end
-
 end
