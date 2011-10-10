a8529487a062ccea79fb5bf8f73562e87737b499
Robbert Dol <robbert.dol@quintel.com>
2011-10-10 15:21:44 +0200

    updated config for rc deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,12 +43,12 @@ end
 
 
 task :release do
-  set :domain, "ec2-46-137-47-160.eu-west-1.compute.amazonaws.com"
+  set :domain, "ec2-46-137-63-176.eu-west-1.compute.amazonaws.com"
   set :branch, "release"
 
   set :application_key, "#{application}_rc"
-  set :deploy_to, "/home/ubuntu/apps/#{application}_staging"  ## this is a copy of staging, so serverconfig stays the same
-  set :config_files, "/home/ubuntu/config_files/#{application}_staging" ## this is a copy of staging, so serverconfig stays the same
+  set :deploy_to, "/home/ubuntu/apps/#{application}"  ## this is a copy of production, so serverconfig stays the same
+  set :config_files, "/home/ubuntu/config_files/#{application}" ## this is a copy of production, so serverconfig stays the same
 
   set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
   set :db_pass, "quintel"
