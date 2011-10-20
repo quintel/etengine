ecf67868b136ee7c7ad3ac45f77f4290990591bd
Paolo Zaccagnini <paozac@gmail.com>
2011-10-20 15:56:02 +0200

    areas autocomplete search box

--- app/javascripts/init/admin.interface.js
+++ app/javascripts/init/admin.interface.js
@@ -1,28 +1,4 @@
 $(document).ready(function() {
-  $("#panel").resizable({
-    handles: 'e'
-  });
-
-  $('input#search').keyup(function(ev) {
-    if(ev.keyCode == 27) { $(this).val(''); }
-    var search = $(this).val();
-    var cookie_key = $(this).attr('data-search_scope');
-
-    if (cookie_key != undefined) {
-      $.cookie(cookie_key, search);
-    }
-    
-    var condition = $.map( search.split(' '), function(el) {
-      if (el != "") { return "[data-search*="+el+"]"; }
-    }).join('');
-    
-    $('.tree li').hide();
-    $('.tree li'+condition).show();
-  });
-      
-  var stored_search = $.cookie($('input#search').attr('data-search_scope'));
-  $('input#search').val(stored_search).keyup();
-
   $('.gql_operator').mouseover(function(ev) {
     var statement = $(this).next().text();
     var query = $(this).text() + statement;
