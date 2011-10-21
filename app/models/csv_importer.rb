b57cc1e829e42cec59a146c62a77a428415980ef
Paolo Zaccagnini <paozac@gmail.com>
2011-10-21 10:27:25 +0200

    created csv_importer object

---
+++ app/models/csv_importer.rb
@@ -0,0 +1,25 @@
+# This class takes care of the entire zip import process
+# It uses many ActiveModel mixins for convenience
+
+class CsvImporter
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :version, :description, :zip_file
+
+  validates :version, :presence => true
+  validates :zip_file, :presence => true
+
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  # This object won't be stored in the database
+  def persisted?
+    false
+  end
+end
