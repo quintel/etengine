aba0b09b8b073a4d54f9b9660b7951adbb127771
Paolo Zaccagnini <paozac@gmail.com>
2011-10-24 17:28:58 +0200

    removed a couple deprecation warnings

--- config/environments/builded.rb
+++ config/environments/builded.rb
@@ -11,8 +11,7 @@ Etm::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.action_controller.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
+  config.consider_all_requests_local = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
