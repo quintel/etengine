66419eb54f8e91783d9dfdc09729f4ebe513f039
Paolo Zaccagnini <paozac@gmail.com>
2011-10-27 14:03:33 +0200

    TRASH: unused current.rb methods

--- spec/models/current_spec.rb
+++ spec/models/current_spec.rb
@@ -1,20 +1,6 @@
 require 'spec_helper'
 
 describe Current do
-
-  describe "already_shown" do
-    before {
-      Current.session['already_shown'] = nil
-    }
-    context "not shown" do
-      specify { Current.already_shown?('demand/intro').should be_false}
-    end
-    context "already shown" do
-      before { Current.already_shown?('demand/intro') }
-      specify { Current.already_shown?('demand/intro').should be_true}
-    end
-  end
-  
   describe "gql_calculated?" do
     context "without gql" do
       before { Current.gql = nil }
