94f6471591576a8d5f5e6dbdc4d9017d0bcedf29
Paolo Zaccagnini <paozac@gmail.com>
2011-10-28 12:00:44 +0200

    added goal#is_set? method

--- spec/models/gql/goal_spec.rb
+++ spec/models/gql/goal_spec.rb
@@ -8,5 +8,18 @@ module Gql
         g.key.should == :foo
       end
     end
+    
+    describe "#is_set?" do
+      it "should return false if the user value is nil" do
+        g = Goal.new(:foo)
+        g.is_set?.should be_false
+      end
+
+      it "should return true if the user value is not nil" do
+        g = Goal.new(:foo)
+        g.user_value = 123
+        g.is_set?.should be_true
+      end
+    end
   end
 end
