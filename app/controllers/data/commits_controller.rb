8ad49fdef613d7129d089af7d890935ce14d2fd5
Paolo Zaccagnini <paozac@gmail.com>
2011-11-01 11:34:42 +0100

    controller cleanup

--- app/controllers/data/commits_controller.rb
+++ app/controllers/data/commits_controller.rb
@@ -1,6 +1,5 @@
 class Data::CommitsController < Data::BaseController
   skip_before_filter :find_graph
-  # before_filter :init_git, :only => :index
 
   set_tab :commits
 
@@ -19,14 +18,4 @@ class Data::CommitsController < Data::BaseController
     @etsource.import!
     flash.now[:notice] = "It is now a good idea to refresh the gquery cache on all clients (ETM, Mixer, ...)"
   end
-
-  private
-
-    def init_git
-
-      @git = Git.open('etsource')
-      @output = @git.fetch
-      @git.checkout(@branch)
-      @git.pull
-    end
 end
