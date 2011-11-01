794c556484669af814bf6e8aecc3ec2f6ff68ccf
Paolo Zaccagnini <paozac@gmail.com>
2011-11-01 11:21:12 +0100

    switch branch and show relative history on commits#index

--- app/models/etsource/base.rb
+++ app/models/etsource/base.rb
@@ -1,4 +1,22 @@
 module Etsource
+  # Proxy to the git operations
+  #
   class Base
+    def initialize
+      @etsource_dir = APP_CONFIG[:etsource_dir] || 'etsource'
+      @git = Git.open @etsource_dir
+    end
+
+    def commits
+      @git.log
+    end
+
+    def branches
+      @git.branches.local.map(&:name)
+    end
+
+    def checkout(branch)
+      @git.checkout branch
+    end
   end
 end
