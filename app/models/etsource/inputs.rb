ee462f21c1e9910aabcae6719a8de421bc656344
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-11-04 18:03:39 +0700

    Modify ETsource import of inputs. create inputs when not exist. Force id that is defined in yml file

--- app/models/etsource/inputs.rb
+++ app/models/etsource/inputs.rb
@@ -28,11 +28,16 @@ module Etsource
 
       Dir.glob("#{base_dir}/**/*.yml").each do |f|
         attributes = YAML::load_file(f)
+        id = attributes.delete('id')
         begin
-          input = Input.find(attributes.delete('id'))
+          input = Input.find(id)
           input.update_attributes(attributes)
         rescue ActiveRecord::RecordNotFound
-          Rails.logger.debug "*** ETSource::Inputs#import: Input not found"
+          Rails.logger.debug "*** ETSource::Inputs#import: Created new Input"
+          input = Input.find_by_key(attributes['key'])
+          input.update_attributes(attributes) if input
+          input ||= Input.create!(attributes)
+          input.force_id(id)
         end
       end
     end
