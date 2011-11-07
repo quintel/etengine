37b95faa248bc108e67f333684510b083c8c7fe1
Paolo Zaccagnini <paozac@gmail.com>
2011-11-07 16:04:29 +0100

    preparing common users pages

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -4,6 +4,13 @@ class ConvertersController < ApplicationController
   end
   
   def show
+    @graph = Graph.latest_from_country('nl')
+    # We have to assign the gql to manually Current
+    # DEBT: this is probablye not needed anymore. instead assign Current.graph = @graph
+    Current.gql = @graph.gql
+    
+    Current.gql.prepare
     @converter = Converter.find params[:id]
+    @converter_present = @graph.gql.present_graph.graph.converter(@converter.id)
   end
 end
