c3911ad9e5bddb46636f3dd7ae2e0e5dee7cfa90
Paolo Zaccagnini <paozac@gmail.com>
2011-11-07 16:43:47 +0100

    show converter details on guest converter page

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -5,12 +5,9 @@ class ConvertersController < ApplicationController
   
   def show
     @graph = Graph.latest_from_country('nl')
-    # We have to assign the gql to manually Current
-    # DEBT: this is probablye not needed anymore. instead assign Current.graph = @graph
-    Current.gql = @graph.gql
-    
-    Current.gql.prepare
+    @graph.gql.prepare
     @converter = Converter.find params[:id]
     @converter_present = @graph.gql.present_graph.graph.converter(@converter.id)
+    @converter_future = @graph.gql.future_graph.graph.converter(@converter.id)
   end
 end
