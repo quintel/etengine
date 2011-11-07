4df7c674db1a8cd353b83387cd22a8d92f85ad07
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-11-07 16:15:23 +0700

    fix db migration

--- db/migrate/20111107082350_add_min_demand_and_max_demand_to_dataset_links.rb
+++ db/migrate/20111107082350_add_min_demand_and_max_demand_to_dataset_links.rb
@@ -1,6 +1,6 @@
 class AddMinDemandAndMaxDemandToDatasetLinks < ActiveRecord::Migration
   def self.up
-    add_column :dataset_link_data, :min_demand, :integer, :limit => 8
+    add_column :dataset_link_data, :max_demand, :integer, :limit => 8
   end
 
   def self.down
