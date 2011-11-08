7642862448dbdecf44a84163215b7b8c3339c309
Paolo Zaccagnini <paozac@gmail.com>
2011-11-08 14:57:45 +0100

    added temporary page to preview the effects of the hh heating structure fix

--- app/controllers/data/scenarios_controller.rb
+++ app/controllers/data/scenarios_controller.rb
@@ -1,11 +1,14 @@
 class Data::ScenariosController < Data::BaseController
-  before_filter :find_scenario, :only => :show
+  before_filter :find_scenario, :only => [:show, :fix]
   
   def index
     @scenarios = Scenario.page(params[:page]).per(35)
   end
 
   def show
+  end
+  
+  def fix
   end
   
   private
