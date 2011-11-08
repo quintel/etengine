6d6a5cfa4dcc530f041cd5c2341cb0565ac09516
Paolo Zaccagnini <paozac@gmail.com>
2011-11-08 14:57:45 +0100

    added temporary page to preview the effects of the hh heating structure fix

--- config/routes.rb
+++ config/routes.rb
@@ -89,7 +89,9 @@ Etm::Application.routes.draw do
       resources :blueprints, :except => [:edit, :update, :destroy]
       resources :inputs, :except => :show
       resources :blueprint_models, :only => :index
-      resources :scenarios, :only => [:index, :show]
+      resources :scenarios, :only => [:index, :show] do
+        put :fix, :on => :member
+      end
       resources :energy_balance_groups
 
       match '/gql' => "gql#index"
