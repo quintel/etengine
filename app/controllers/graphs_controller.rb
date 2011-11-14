8dfdd13535be444e28f380bb9f3a4e71eeadeeda
Paolo Zaccagnini <paozac@gmail.com>
2011-11-14 14:19:58 +0100

    show raphael graph

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,4 +1,7 @@
 class GraphsController < ApplicationController
   def show
+    params[:blueprint_id] ||= 'latest'
+    params[:region_code] ||= 'nl'
+    @blueprint_layout = BlueprintLayout.first
   end
 end
