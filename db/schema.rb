bbccfff3ef35d8390f60fed11af4733b51a4bd75
Paolo Zaccagnini <paozac@gmail.com>
2011-11-24 17:05:10 +0100

    migration to update the households heating sliders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111118105304) do
+ActiveRecord::Schema.define(:version => 20111124155441) do
 
   create_table "areas", :force => true do |t|
     t.string   "country"
