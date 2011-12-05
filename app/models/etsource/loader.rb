2ebaf7934c302f77e0e9511a3cb16d2837813b40
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-05 20:54:54 +0700

    load gal from etsource

--- app/models/etsource/loader.rb
+++ app/models/etsource/loader.rb
@@ -22,12 +22,26 @@ module Etsource
       @datasets ||= Etsource::Dataset.new(@etsource).import.with_indifferent_access
     end
 
+    # A Qernel::Graph from ETsource where the converters are ordered in a way that
+    #  is optimal for the calculation. 
+    #
+    def optimized_graph
+      unless @optimized_graph 
+        g = unoptimized_graph
+        g.dataset = dataset('nl')
+        g.optimize_calculation_order
+        g.reset_dataset!
+        @optimized_graph = g
+      end
+      @optimized_graph
+    end
+
     def unoptimized_graph
       @graph ||= Etsource::Graph.new(@etsource).import
     end
     
     def graph
-      unoptimized_graph
+      optimized_graph
     end  
   end
 end
