68ff38783292a8bbc955209612a1e105b687a67c
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-06 11:21:18 +0700

    TRASH: /converters and /graphs

--- app/controllers/graphs_controller.rb
+++
@@ -1,7 +0,0 @@
-class GraphsController < ApplicationController
-  def show
-    params[:blueprint_id] ||= 'latest'
-    params[:region_code] ||= 'nl'
-    @blueprint_layout = BlueprintLayout.first
-  end
-end
