9d4285180f472b1a5719f1374b9a4834c040732a
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-15 14:34:18 +0700

    with output_share

--- app/models/etsource/constants.rb
+++ app/models/etsource/constants.rb
@@ -1,7 +1,8 @@
+# This is to quickly prototype. has to find a better place. 
+# because it is both accessed by etsource dataset.yml files and frontend code
+
 module Etsource
-  class Constants
-    # This is to quickly prototype. has to find a better place
-    # because it is both accessed by etsource dataset.yml files and frontend code
+  module Constants
     HOUSEHOLDS_HOT_WATER = {
       :coal => %w[coal_boiler_households_energetic],
       :crude_oil => %w[oil_boiler_households_energetic],
@@ -9,6 +10,27 @@ module Etsource
       :biomass => %w[biomass_boiler_households_energetic],
       :electricity => %w[heatpump_boiler_households_energetic electric_boiler_households_energetic],
       :steam_hot_water => %w[heatpump_boiler_using_city_heat_households_energetic]
+    }
+    HOUSEHOLDS_HEATING = {
+      :coal => %w[heater_on_coal_households_energetic],
+      :crude_oil => %w[heater_on_oil_households_energetic],
+      :natural_gas => %w[
+          gas_fired_heater_households_energetic 
+          central_heating_households_energetic
+          gasheatpump_households_energetic
+          micro_CHP_production_households
+      ],
+      :electricity => %w[
+        electric_heater_households_energetic
+        heatpump_addon_households_energetic
+        heatpump_households_energetic
+        heat_pump_with_chs_households_energetic
+      ]
+    }
+
+    HOUSEHOLDS = {
+      :useable_heat => HOUSEHOLDS_HEATING,
+      :hot_water => HOUSEHOLDS_HOT_WATER
     }
   end
 end
