4830ec2a38bd2e08fae99a410a21ee35f53f9d61
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-15 13:06:58 +0700

    add steam_hot_water

--- app/models/etsource/constants.rb
+++ app/models/etsource/constants.rb
@@ -7,8 +7,8 @@ module Etsource
       :crude_oil => %w[oil_boiler_households_energetic],
       :natural_gas => %w[gas_boiler_households_energetic cv_hot_water_households_energetic fuel_cell_chp_households_energetic micro_chp_hot_water_households_energetic],
       :biomass => %w[biomass_boiler_households_energetic],
-      :electricity => %w[heatpump_boiler_households_energetic electric_boiler_households_energetic]#,
-      #:steam_hot_water => %w[heatpump_boiler_using_city_heat_households_energetic]
+      :electricity => %w[heatpump_boiler_households_energetic electric_boiler_households_energetic],
+      :steam_hot_water => %w[heatpump_boiler_using_city_heat_households_energetic]
     }
   end
 end
