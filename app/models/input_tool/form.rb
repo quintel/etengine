050e3b83a96effd2d7266d4362fde61f2d3e5d7e
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-20 21:16:59 +0700

    add some documentation

--- app/models/input_tool/form.rb
+++ app/models/input_tool/form.rb
@@ -1,6 +1,21 @@
 module InputTool
+  # Forms keep the input data of researchers temporarly in the database.
+  # They are assigned country and the code of a etsource/datasets/_forms/
+  # folder.
+  #
+  # By combining values of multiple forms we get a value box, which makes
+  # the form data accessible to the ETsource dataset form yml files.
+  #
+  #
   class Form < ActiveRecord::Base
     set_table_name 'input_tool_forms'
+
+
+    # we can use this to easily invalidate cache data, by adding a timestamp to
+    # the cache key.
+    def self.last_updated(code)
+      InputTool::Form.where(:area_code => code).maximum('updated_at')
+    end
 
     def value_box
       @value_box ||= ValueBox.new([self])
