4e811b81794aa8cc74f48d460caf83303df857e7
Sebi Burkhard <sebi.burkhard@gmail.com>
2011-12-22 12:09:49 +0700

    Refactor enormous /dataset.rb into 3 files responsible for import/export/rendering yml.erb templates.

---
+++ app/models/etsource/dataset/export.rb
@@ -0,0 +1,110 @@
+module Etsource
+  # ------ Examples -------------------------------------------------------------
+  #
+  #     et = Etsource::Dataset::Export.new('nl')
+  #     et.export # writes into :etsource_dir/datasets/:country/
+  #
+  class Dataset::Export
+    attr_reader :country
+    
+    def initialize(country)
+      # DEBT: @etsource is only used for the base_dir, can be solved better.
+      @etsource = Etsource::Base.new
+      @country  = country
+    end
+
+    def export
+      gql = Gql::Gql.new(Scenario.new(Scenario.default_attributes :country => country))
+      # Assign datasets w/o calculating. Use future graph (present is precalculated).
+      graph.dataset = gql.dataset_clone
+
+
+      # EDGE/Staging conflict/diverge:
+      # Code below this line is more uptodate on staging
+
+      FileUtils.mkdir_p base_dir+"/"+country+"/graph"
+      
+      graph = gql.future_graph   
+      # Assign datasets w/o calculating. Use future graph (present is precalculated).
+      graph.dataset = future_dataset
+
+      # ---- Export Time Curves -----------------------------------------------
+      
+      File.open(country_file(country, 'time_curves'), 'w') do |out|
+        out << YAML::dump(graph.dataset.time_curves)
+      end
+
+      # ---- Export Carriers --------------------------------------------------
+
+      File.open(country_file(country, 'carriers'), 'w') do |out|
+        hsh = graph.carriers.inject({}) do |hsh, c|
+          hsh.merge c.topology_key => c.object_dataset.merge(infinite: c.infinite)
+        end
+        out << YAML::dump(hsh).gsub("--- \n", '') 
+      end
+
+      # ---- Export Area ------------------------------------------------------
+
+      File.open(country_file(country, 'area'), 'w') do |out|
+        # Remove first --- line
+        out << YAML::dump({:area => graph.dataset.data[:area]}).gsub("--- \n", '') 
+      end
+
+      # ---- Export Graph Structure -------------------------------------------
+
+      File.open(country_file(country, 'graph/export'), 'w') do |out|
+        # No longer fake yml format
+        # out << '---' # Fake YAML format
+        graph.converters.each do |converter|
+          # Remove the "" from the keys, to make the file look prettier. 
+          #     "KEY": { values } => KEY: { values }
+          yml = YAML::dump({converter.topology_key => converter.object_dataset}) 
+          yml = yml.gsub(/^\"/,'').gsub('":',':').gsub('---','')
+          
+          out << yml
+
+          converter.outputs.each do |slot|
+            attrs = slot.object_dataset.map{|k,v| "#{k}: #{v.inspect}"}.join(', ')
+            out << "#{slot.topology_key}: {#{attrs}}\n"
+          end
+
+          converter.inputs.each do |slot|
+            attrs = slot.object_dataset.map{|k,v| "#{k}: #{v.inspect}"}.join(', ')
+            out << "#{slot.topology_key}: {#{attrs}}\n"
+            # only export links of inputs, so we don't export them twice.
+            slot.links.each do |link|
+              attrs = link.object_dataset.map{|k,v| "#{k}: #{v.inspect}"}.join(', ')
+              out << "#{link.topology_key}: {#{attrs}}\n"
+            end
+          end
+        end
+      end
+    end
+
+
+   protected
+
+    def all_countries
+      Area.all.map(&:region_code)
+    end
+
+    def base_dir
+      "#{@etsource.base_dir}/datasets"
+    end
+
+    # @param [String] country shortcut 'de', 'nl', etc
+    #
+    def country_dir(country)
+      "#{base_dir}/#{country}"
+    end
+ 
+    # @param [String] country shortcut 'de', 'nl', etc
+    #
+    def country_file(country, file_name)
+      f = "#{base_dir}/#{country}/#{file_name}"
+      f += ".yml" unless f.include?('.yml')
+      f
+    end
+ 
+  end
+end
---
+++ app/models/etsource/dataset/renderer.rb
@@ -0,0 +1,109 @@
+module Etsource
+  # Renderer for datasets/_forms/*/dataset.yml
+  # Makes sure yaml files have binding and can access ValueBox.
+  #
+  # ------ Examples -------------------------------------------------------------
+  # 
+  #     value_box = InputTool::ValueBox.area('nl')
+  #     renderer = Etsource::Dataset::Renderer.new(value_box, "etsource/datasets/_forms/households/dataset.yml")
+  #     renderer.result # => Hash 
+  #
+  #
+  # ------ Binding to ETsource template -----------------------------------------
+  #
+  # A ETsource dataset yml file has a binding to the Etsource::Dataset object. Meaning 
+  # that inside a yml file a method will have the Etsource::Dataset as scope.
+  #
+  #     ---
+  #     heating: <%= get( 'foo', 'bar' ) %>
+  #
+  # Above yaml will call the Etsource::Dataset#get with 'foo' and 'bar' as arguments.
+  # The binding happens in #load_yaml (if it has disappeared search for 'binding').
+  #
+  # ------ method_missing meta programming --------------------------------------
+  #
+  # @deprecated
+  #
+  # @updated I disabled this for now. Not critical to success and the research folks
+  #  should have enough mental capabilities to prepend every string with a ":".
+  #
+  #
+  # To make life easier for researchers I (sb) decided to let them omit the '' or
+  # symbol : for the get arguments.
+  #
+  #     ---
+  #     heating: <%= get('foo', 'bar') %>
+  #     heating: <%= get(foo, bar) %>
+  #
+  # I argue that the second example is rather prettier and less prone to typo-bugs.
+  # It works because the yaml file is bound to the value_box object, therefore foo
+  # will trigger the ValueBox#method_missing and return :foo back.
+  #
+  class Dataset::Renderer
+    attr_reader :country, :value_box
+    
+    def initialize(value_box, file_path)
+      @value_box = value_box
+      @file_path = file_path
+    end
+
+    def result
+      if has_research_data?
+        load_yaml || {}
+      else
+        {}
+      end      
+    end
+
+    # ----- Methods for within YAML -------------------------------------------
+
+    # Access values from the static dataset.
+    #
+    # @param key [String,Symbol] a key of a converter,link or slot "heating_demand-(hot_water)"
+    # @param attr_key [String,Symbol] the attribute name.
+    #
+    def val(key, attr_key)
+      @dataset.data[group_key(key)][Hashpipe.hash(key)][attr_key.to_sym]
+    end
+
+    # @see {InputTool::ValueBox#get}
+    #
+    def get(*args)
+      value_box.get(*args)
+    end
+
+    # @see {InputTool::ValueBox#set}
+    #
+    def shortcut(key, value)
+      value_box.set(key, value)
+    end
+    alias_method :set, :shortcut
+
+  protected
+    
+    # Returns true if we have research data for this file/form.
+    # Do so by checking if the form_code matches the path (they have to).
+    def has_research_data?
+      value_box.form_codes.any?{|c| file_path.include?("/#{c}/") }
+    end
+
+    def group_key(key)
+      key = key.to_s
+      if key.include?('-->')  then :link
+      elsif key.include?('(') then :slot
+      else                         :converter; end
+    end
+
+    def load_yaml
+      load_yaml_with_erb(File.read(file_path))
+    end
+
+    # ---- ERB MAGIC ----------------------------------------------------------
+    # 
+    # Run yaml contents through ERB renderer and attaches itself as a binding.
+    #
+    def load_yaml_with_erb(str)
+      YAML::load(ERB.new(str).result(  binding  ))
+    end
+  end
+end
