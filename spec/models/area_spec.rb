a508524b6149075043483b0effcc0271820f3649
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-01-06 17:46:32 +0700

    TRASH: spec/support/etm_helper.rb

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -2,21 +2,6 @@ require 'spec_helper'
 
 describe Area do
 
-  describe "#create" do
-    before(:all) do
-      load_carriers
-    end
-
-    before do
-      @area = Area.new(:country => 'xz')
-    end
-
-    it "should create a CarrierData for every Carrier" do
-   #   require 'ruby-debug'; debugger
-      @area.save
-      @area.carrier_datas.count.should == Carrier.count
-    end
-  end
 end
 
 
--- spec/support/etm_helper.rb
+++
@@ -1,6 +0,0 @@
-module EtmHelper  
-  # Stubs methods that are needed to just render the etm layout
-  def stub_etm_layout_methods!
-    Current.stub!(:graph)
-  end
-end
