669b47b896b847437f50fa522c64bd33a43ca2e4
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-01-25 09:54:17 +0700

    added exception when no attributes defined for a key

--- app/models/etsource/dataset/import.rb
+++ app/models/etsource/dataset/import.rb
@@ -108,6 +108,7 @@ module Etsource
       Dir.glob([base_dir, '_wizards', '*', "transformer.yml"].join('/')).each do |file|
         wizard   = ::Etsource::Wizard.new(file.split("/")[-2])
         renderer = ::Etsource::Dataset::Renderer.new(file, research_dataset, @dataset, wizard.config)
+        
         hsh = renderer.result
         renderer.save_compiled_yaml(file.gsub('datasets', "compiled/#{country}"))
         merge_hash_with_dataset!(hsh)
@@ -118,12 +119,13 @@ module Etsource
       # Dont make converters with keys :defaults and :globals 
       hsh.delete(:defaults) 
       hsh.delete(:globals)
-
+      
       hsh.each do |key,attributes|
         if key == :area
           # area is a special kid for now. dont hash keys or groups
           @dataset.merge(key, attributes)  
         else
+          raise "No attributes/hashing defined for key `#{key}` in following data bucket. Check the dataset. \n `#{hsh.inspect}`" if attributes.nil?
           attrs = {}; attributes.each{|k,v| attrs[k.to_sym] = v}
           @dataset.merge(group_key(key), Hashpipe.hash(key) => attrs)
         end
