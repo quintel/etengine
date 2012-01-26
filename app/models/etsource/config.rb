ca7876db9e596411b99b1cc5701ce5b36040385d
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-01-26 10:33:59 +0700

    disable loading of wizards until the wizards are updated with WvLs changes.

--- app/models/etsource/config.rb
+++ app/models/etsource/config.rb
@@ -3,7 +3,7 @@ module Etsource
     # Should ETsource::Wizards be included? 
     # true:  this makes the input_module work. 
     # false: turn off to make sure the ETengine is not affected by the input_module
-    LOAD_WIZARDS = true
+    LOAD_WIZARDS = false
 
     # If you work on the input module, this disables caching and will
     # always reload the Etsource from scratch.
