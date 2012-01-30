ab9da20c0a96cb9018343295f16707661bf6aeb8
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-01-30 16:38:17 +0700

    auto_number: use value.abs for figuring out formatting

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -5,17 +5,18 @@ module NumbersHelper
     return value unless value.is_a?(Numeric)
     return 'Infinity' if value.to_f.infinite?
 
-    if value > 10**8
+    abs_value = value.abs
+    if abs_value > 10**8
       number_with_precision(value.to_f / 10**9, :precision => 2, :delimiter => ",") + " B"
-    elsif value > 10**5
+    elsif abs_value > 10**5
       number_with_precision(value.to_f / 10**6, :precision => 2, :delimiter => ",") + " M"
-    elsif value > 10**2
+    elsif abs_value > 10**2
       number_with_precision(value.to_f / 10**3, :precision => 2, :delimiter => ",") + " K"
-    elsif value >= 1 && value < 100
+    elsif abs_value >= 1 && value < 100
       number_with_precision value, :precision => 2
-    elsif value > 0 && value < 1
+    elsif abs_value > 0 && value < 1
       number_with_precision value, :precision => 3
-    elsif value === 0
+    elsif abs_value === 0
       0
     else
       value
