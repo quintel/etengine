e5e95312e1f26e2f5ab0207ae466da019dedcf76
Paolo Zaccagnini <paozac@gmail.com>
2012-02-01 16:43:15 +0100

    converter detail page: temporary fix. see #259

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -1,12 +1,27 @@
 class ConvertersController < ApplicationController
+  before_filter :find_graph, :only => :show
+
   def show
-    @graph = Graph.latest_from_country('nl')
-    @graph.gql.prepare
-    converter_id = Converter.full_keys[params[:id].to_sym]
+    key = params[:id].to_sym
+    converter_id = Converter.full_keys[key]
     @converter = Converter.find converter_id
-    @converter_present = @graph.gql.present_graph.graph.converter(@converter.id)
-    @converter_future  = @graph.gql.future_graph.graph.converter(@converter.id)
-    
+    @converter_present = @gql.present_graph.graph.converter(key)
+    @converter_future  = @gql.future_graph.graph.converter(key)
     render :layout => false
+  end
+
+  private
+
+  def find_graph
+    # TODO: ETM should pass the right scenario id or, at least, the country
+    # DEBT: Data::BaseController has an identical method
+    api_scenario_id = params[:api_scenario_id] ||= 'latest'
+    if api_scenario_id == 'latest'
+      @api_scenario = ApiScenario.last
+    else
+      @api_scenario = ApiScenario.find(params[:api_scenario_id])
+    end
+    Current.scenario = @api_scenario
+    @gql = @api_scenario.gql(prepare: true)
   end
 end
