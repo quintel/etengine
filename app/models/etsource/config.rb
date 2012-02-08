b3904b6d8134b3efd5e1bb1cc4c12b9f76f91539
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-02-08 11:09:38 +0700

    make Etsource::Config configurable in config/config.yml

--- app/models/etsource/config.rb
+++ app/models/etsource/config.rb
@@ -3,10 +3,10 @@ module Etsource
     # Should ETsource::Wizards be included? 
     # true:  this makes the input_module work. 
     # false: turn off to make sure the ETengine is not affected by the input_module
-    LOAD_WIZARDS = false
+    LOAD_WIZARDS  = APP_CONFIG.fetch(:etsource_load_wizards, false)
 
     # If you work on the input module, this disables caching and will
     # always reload the Etsource from scratch.
-    FORCE_DATASET_RELOAD = true
+    CACHE_DATASET = APP_CONFIG.fetch(:etsource_cache_dataset, true)
   end
 end
