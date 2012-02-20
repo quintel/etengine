4a2adca5075fa155401c66b462b38903e7f6de2f
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-02-20 17:04:06 +0700

    moved Etsource::Config into Base

--- app/models/etsource/loader.rb
+++ app/models/etsource/loader.rb
@@ -27,7 +27,7 @@ module Etsource
     # @return [Qernel::Dataset] Dataset to be used for a country. Is in a uncalculated state.
     def dataset(country)
       ActiveSupport::Notifications.instrument("etsource.performance.dataset(#{country.inspect}") do
-        if Etsource::Config::CACHE_DATASET
+        if @etsource.cache_dataset?
           # DEBT Limitations of this cache:
           # if experimenting with input tool, you change a transformer.yml or config.yml will not
           # take effect, because cache only invalidates when a research dataset has been changed 
