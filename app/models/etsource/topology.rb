9152c41448dbfedfe58db29fb974b4a4a1f345d9
Paolo Zaccagnini <paozac@gmail.com>
2012-02-27 16:34:02 +0100

    etsource: load topology from export directory

--- app/models/etsource/topology.rb
+++ app/models/etsource/topology.rb
@@ -14,14 +14,14 @@ module Etsource
     def import
       converters = {}
 
-      each_file do |lines| 
+      each_file do |lines|
         # Initialize all converters first, before we map slots and links to them.
         hsh = lines.select{|l| l =~ /^\w+;/ }. # only match converter lines
                     map{|l| Qernel::Converter.import(l) }.
                     inject({}) {|h,k| h.merge k.code => k}
         converters.merge!(hsh)
       end
-      
+
       graph = Qernel::Graph.new(converters.values).tap{|g| g.connect_qernel }
 
       each_file do |lines|
@@ -38,11 +38,12 @@ module Etsource
         end
       end
 
-      
       graph.carriers.each {|c| c.graph = graph}
       graph
     end
-    
+
+    # writes to disk *in the working copy directory*
+    #
     def export
       FileUtils.mkdir_p(base_dir)
       File.open(topology_file, 'w') do |out|
@@ -53,30 +54,34 @@ module Etsource
         end
       end
     end
-    
+
     def carrier(obj)
       key = obj.respond_to?(:carrier_key) ? obj.carrier_key : obj
       @carriers ||= {}
       @carriers[key] ||= Qernel::Carrier.new(key: key)
     end
 
-  #########
   protected
-  #########
 
     def each_file(&block)
-      Dir.glob("#{base_dir}/*.graph").each do |f|
+      Dir.glob("#{export_dir}/*.graph").each do |f|
         lines = File.read(f).lines
         yield lines if block_given?
       end
     end
-    
+
     def topology_file
       "#{base_dir}/export.graph"
     end
 
+    # working copy
     def base_dir
       "#{@etsource.base_dir}/topology"
+    end
+
+    # export, read-only dir
+    def export_dir
+      "#{@etsource.export_dir}/topology"
     end
 
   end
