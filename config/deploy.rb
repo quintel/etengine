5fae3a72de53e9a3f315b74fbf97b26d9e175b94
Paolo Zaccagnini <paozac@gmail.com>
2012-02-29 13:53:03 +0100

    capistrano: updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,12 @@ set :application_key, "etengine"
 set :stage, :production
 set :server_type, 'production'
 
+set :deploy_to, "/u/apps/etengine"
+set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
+
 task :production do
   set :domain, "et-engine.com"
   set :branch, "production"
-  set :deploy_to, "/home/ubuntu/apps/#{application_key}"
-  set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
   set :db_pass, "Energy2.0"
   set :db_name, application_key
   set :db_user, application_key
@@ -22,8 +23,6 @@ end
 task :staging do
   set :domain, "beta.et-engine.com"
   set :branch, "staging"
-  set :deploy_to, "/u/apps/etengine"
-  set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
   set :db_pass, "r8ZPP7pQTDTBha"
   set :db_name, 'etengine_staging'
   set :db_user, 'etengine_staging'
@@ -35,8 +34,6 @@ task :edge do
   # set :domain, "edge.et-engine.com"
   set :domain, "ec2-176-34-77-157.eu-west-1.compute.amazonaws.com"
   set :branch, "edge"
-  set :deploy_to, "/u/apps/etengine"
-  set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
   set :db_pass, "lVtSsSv43KooQE"
   set :db_name, "etengine_edge"
   set :db_user, "etengine_edge"
@@ -45,13 +42,12 @@ task :edge do
 end
 
 set :user, 'ubuntu'
-
 set :scm, :git
-set :repository,  "git@github.com:dennisschoenmakers/etengine.git"
+set :repository, "git@github.com:dennisschoenmakers/etengine.git"
 set :deploy_via, :remote_cache
 set :chmod755, "app config db lib public vendor script script/* public/disp*"  	# Some files that will need proper permissions set :use_sudo, false
 ssh_options[:forward_agent] = true
-set :use_sudo,     false
+set :use_sudo, false
 set :local_db_name, 'etengine_dev'
 set :bundle_flags, '--deployment --quiet --binstubs --shebang ruby-local-exec'
 
