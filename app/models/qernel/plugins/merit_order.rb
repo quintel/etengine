6e6a036f25b2978af52af31fde296944e6413fab
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-03-01 16:33:40 +0700

    Create MeritOrder as a Qernel::Plugin

--- app/models/qernel/plugins/merit_order.rb
+++ app/models/qernel/plugins/merit_order.rb
@@ -5,121 +5,138 @@ module Qernel::Plugins
 
     included do |variable|
       set_callback :calculate, :after, :calculate_merit_order
-      # set_callback :calculate, :after, :calculate_full_load_hours
+      set_callback :calculate, :after, :calculate_full_load_hours
     end
 
     module ClassMethods
 
+      def merit_order_table
+        @merit_order_table ||= Etsource::Loader.instance.merit_order_table
+      end
+
+      def merit_order_converters
+        @merit_order_converters ||= Etsource::Loader.instance.globals('merit_order_converters')
+      end
+
     end
 
-    MERIT_ORDER_CONVERTERS = [
-      # the converter keys that correspond to the number array in MERIT_ORDER_DATA
-      :industrial_chp,
-      :agriculture_chp,
-      :space_heating_chp,
-      :wind_offshore,
-      :wind_coastal,
-      :wind_inland
-    ]
-
-    require 'csv'
-    MERIT_ORDER_DATA = ::CSV.read('../etsource/datasets/_globals/merit_order.csv', :converters => :numeric)
-    MERIT_ORDER_DATA.map! do |row|
-      [row.delete_at(0), row]
-    end
-    
     module InstanceMethods
 
+      def converters_for_merit_order
+        group_converters(:electricity_production).map(&:query)
+      end
+
+      # assign merit_order_start and merit_order_end
       def calculate_merit_order
-        # Converters to include in the sorting: G(electricity_production)
-        converters = group_converters(:electricity_production).map(&:query)
-        converters.sort_by! do |c| 
-          c.variable_costs_per_mwh_input * c.electricity_output_conversion
-        end
-        if converters.first
-          converters.first[:merit_order_end]   = 0.0
-          converters.first[:merit_order_start] = 0.0
-          converters[1..-1].each_with_index do |converter, i|
-            # i points now to the previous one, not the current index! (because we start from [1..-1])
-            converter[:merit_order_start] = converters[i][:merit_order_end]
-            
-            e  = converter[:merit_order_start]
-            e += (converter.installed_production_capacity_in_mw_electricity || 0.0) * converter.availability
-            converter[:merit_order_end] = e.round(3)
+        instrument("qernel.merit_order: calculate_merit_order") do
+          # Converters to include in the sorting: G(electricity_production)
+          converters = converters_for_merit_order
+          converters.sort_by! do |c| 
+            c.variable_costs_per_mwh_input * c.electricity_output_conversion
           end
-        end # if
-        dataset_set(:calculate_merit_order_finished, true)
-        calculate_full_load_hours
+
+          if first = converters.first
+            first[:merit_order_end]   = 0.0
+            first[:merit_order_start] = 0.0
+            converters[1..-1].each_with_index do |converter, i|
+              # i points now to the previous one, not the current index! (because we start from [1..-1])
+              converter[:merit_order_start] = converters[i][:merit_order_end]
+              
+              e  = converter[:merit_order_start]
+              e += (converter.installed_production_capacity_in_mw_electricity || 0.0) * converter.availability
+              converter[:merit_order_end] = e.round(3)
+            end
+          end # if
+          dataset_set(:calculate_merit_order_finished, true)
+        end
       end # calculate_merit_order
 
-      def normalized_residual_loads # Excel N
-
-        demands = [3946, 2973, 394, 20000,0,0,0]
-        residual_load_profiles = MERIT_ORDER_DATA.map do |load, wewp|
-          wewp_x_demands = wewp.zip(demands)
-          wewp_x_demands.map!{|wewp, demand| wewp * demand }
-          [0, load - wewp_x_demands.sum].max
+      
+      def merit_order_demands
+        self.class.merit_order_converters.map do |_ignore, converter_keys|
+          converter_keys.map{|key| converter(key).query.mw_input_capacity}.sum.round(1).tap{|s| puts "#{_ignore}: #{s}"}
         end
-        max = residual_load_profiles.max
+      end
 
-        # only interested in the normalized loads. so map! 
-        residual_load_profiles.map!{|l| l / max}
+      #
+      #
+      def normalized_residual_loads # Excel N
+        instrument("qernel.merit_order: normalized_residual_loads") do
+          demands = merit_order_demands
+          residual_load_profiles = self.class.merit_order_table.map do |load, wewp|
+            wewp_x_demands = wewp.zip(demands)
+            wewp_x_demands.map!{|wewp, demand| wewp * demand }
+            [0, load - wewp_x_demands.sum].max
+          end
+          max = residual_load_profiles.max
+
+          # only interested in the normalized loads. so map! 
+          residual_load_profiles.map!{|l| l / max}
+        end
       end
 
       def residual_ldc
-        loads      = []
-        nrl        = normalized_residual_loads
-        nrl_length = nrl.length.to_f
+        instrument("qernel.merit_order: residual_ldc") do
+          loads      = []
+          nrl        = normalized_residual_loads
+          nrl_length = nrl.length.to_f
 
-        precision = 10
+          precision = 10
 
-        (0..precision).to_a.each do |i| 
-          q = i / precision.to_f
-          y = nrl.count{|n| n >= q} / nrl_length
-          loads << [q, y.to_f]
+          (0..precision).to_a.each do |i| 
+            q = i / precision.to_f
+            y = nrl.count{|n| n >= q} / nrl_length
+            loads << [q, y.to_f]
+          end
+
+          loads
         end
-
-        loads
       end
 
       def calculate_full_load_hours
         if dataset_get(:calculate_merit_order_finished) != true
-          raise "Trying to calculate_full_load_hours before calculate_merit_order has started/finished!" 
+          calculate_merit_order
         end
+
         ldc_points = residual_ldc
 
-        converters = group_converters(:electricity_production).map(&:query)
+        converters = converters_for_merit_order
         converters.sort_by!{|c| c[:merit_order_end]}
         
         max_merit  = converters.last[:merit_order_end] || 0.0
-        
-        converters.each do |converter|
-          s = (converter[:merit_order_start]) / max_merit
-          e = (converter[:merit_order_end]  ) / max_merit
-          diff = [e - s, 0.001].max
 
-          # polygon_area expects the points passed in clock-wise order.
-          points  = [[s, 0]]
-          points << [s, interpolate_y(ldc_points, s)]     # top left (y interpolated)
-          ldc_points.each do |p|
-            x = p.first
-            points << p if x > s && x < e # in between
+        instrument("qernel.merit_order: calculate_full_load_hours") do        
+          converters.each do |converter|
+
+            lft  = (converter.merit_order_start) / max_merit
+            rgt  = (converter.merit_order_end  ) / max_merit
+            
+            # polygon_area expects the points passed in clock-wise order.
+            points  = [
+              [lft, 0],                                       # bottom left
+              [lft, interpolate_y(ldc_points, lft)],          # top left (y interpolated)
+              *ldc_points.select{|x,y| x > lft && x < rgt },  # points on residual_ldc curve
+              [rgt, interpolate_y(ldc_points, rgt)],          # top right (y interpolated)
+              [rgt, 0]                                        # bottom right
+            ]
+
+            area_size       = polygon_area(points.map(&:first), points.map(&:second))
+            diff            = [rgt - lft, 0.0].max 
+
+            capacity_factor = (area_size / diff).rescue_nan
+            full_load_hours = capacity_factor * 8760
+
+            converter.capacity_factor = capacity_factor.round(3)
+            converter.full_load_hours = full_load_hours.round(1)
           end
-          points << [e, interpolate_y(ldc_points, e)]     # top right (y interpolated)
-          points << [e, 0]                                # bottom right
-
-          area_size = polygon_area(points.map(&:first), points.map(&:second))
-
-          capacity_factor = area_size / diff
-          full_load_hours = capacity_factor * 8760
-
-          converter[:capacity_factor] = capacity_factor
-          converter[:full_load_hours] = full_load_hours.finite? ? full_load_hours.round(1) : 0.0
         end
 
         nil
       end
 
+      # this function is probably not so precise. 
+      # it's supposed to interpolate the y value for a given x.
+      # It uses the formulas i've learned at school and wikipedia
       def interpolate_y(points, x)
         return 1.0 if x == 0.0
         return 0.0 if x >= 1.0
@@ -135,7 +152,7 @@ module Qernel::Plugins
 
         y = m*x + n
 
-        y.finite? ? y : 0.0
+        y.rescue_nan
       end
 
       # Inspired from http://alienryderflex.com/polygon_area/
