15d750198841ba9662cca67132d23c4d1c47248c
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-03-19 15:28:34 +0700

    make new within_1_percent rspec matcher

--- spec/support/be_near_matcher.rb
+++ spec/support/be_near_matcher.rb
@@ -1,3 +1,25 @@
+RSpec::Matchers.define :be_within_a_percent do |expected|
+  match do |actual|
+    if actual.to_f == 0.0 && expected.to_f == 0.0
+      true
+    else
+      ((1.0 - actual.to_f / expected) * 100.0).abs < 1.0
+    end
+  end
+
+  failure_message_for_should do |actual|
+    "got: #{actual}. But should be near #{expected}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "got: #{actual}. But should not to be near #{expected}"
+  end
+
+  description do
+    "be near #{expected}"
+  end
+end
+
 RSpec::Matchers.define :be_near do |expected|
   match do |actual|
     (actual - expected).abs < 0.01
