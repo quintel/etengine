0b7845e325332a6171be28c801e1ea97ba1948f8
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-03-21 16:40:50 +0700

    add some more options

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -10,5 +10,5 @@ end
 # Put ETSOURCE_DIR in here, so that it is accessible when loading
 # with yaml box.
 ETSOURCE_DIR = APP_CONFIG.fetch(:etsource_working_copy, 'etsource')
-ETSOURCE_EXPORT_DIR = APP_CONFIG.fetch(:etsource_export, 'etsource')
+ETSOURCE_EXPORT_DIR = ENV['ETSOURCE_DIR'] || APP_CONFIG.fetch(:etsource_export, 'etsource')
 
