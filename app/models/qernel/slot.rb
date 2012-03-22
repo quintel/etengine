1743780cc9adb28b8a7219553c87a8aebbb387b1
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-03-22 10:05:27 +0700

    cherry-picked 436d4ed

--- app/models/qernel/slot.rb
+++ app/models/qernel/slot.rb
@@ -77,11 +77,12 @@ class Slot
     if input?
       active_links.select(&:constant?).each(&:calculate)
       active_links.select(&:share?).each(&:calculate)
-      flexible_links = active_links.select(&:flexible?)
+
       # Calculate flexible links with boundaries first. Because
       # without boundaries a link takes everything. 
-      flexible_links.select(&:max_boundaries?).each(&:calculate)
-      flexible_links.reject(&:max_boundaries?).each(&:calculate)
+      flexible_links = active_links.select(&:flexible?)
+      flexible_links.select(&:max_boundaries?).sort_by(&:priority).each(&:calculate)
+      flexible_links.reject(&:max_boundaries?).sort_by(&:priority).each(&:calculate)
     end
     if output?
       links.select(&:reversed?).each(&:calculate)
