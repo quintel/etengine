9cdd132a2e409582ffa7f35eef7d913b95b038d0
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-04-05 14:31:35 +0200

    keep gqueries in memory

---
+++ app/models/in_memory_record.rb
@@ -0,0 +1,38 @@
+module InMemoryRecord
+  extend ActiveSupport::Concern
+  
+  included do |variable|
+  end
+
+  module InstanceMethods
+    
+    def save(*args)
+      raise "InMemoryRecord: Object#save not allowed"
+    end
+    
+  end
+
+
+  module ClassMethods
+    def create(*args)
+      raise "InMemoryRecord: Object.create not allowed"
+    end
+
+    def all
+      records.values
+    end
+
+    def records
+      @records ||= load_records
+    end
+
+    def get(key)
+      records[key]
+    end
+
+    def add(obj)
+      records[obj.lookup_id] = obj
+      obj
+    end
+  end
+end
