333fe7117c587238246cd2fcf9909d07ad12519a
Paolo Zaccagnini <paozac@gmail.com>
2012-04-13 12:31:34 +0200

    api: waiting for the next jbuilder release

--- app/models/api/v3/converter.rb
+++ app/models/api/v3/converter.rb
@@ -7,8 +7,8 @@ module Api
         @code = code
         @scenario = scenario
         @gql = @scenario.gql(:prepare => true)
-        @present = @gql.present_graph.graph.converter(key) rescue nil
-        @future  = @gql.future_graph.graph.converter(key) rescue nil
+        @present = @gql.present_graph.graph.converter(@code) rescue nil
+        @future  = @gql.future_graph.graph.converter(@code) rescue nil
         if @present.nil? || @future.nil?
           raise "Converter not found! (#{@code})"
         end
@@ -17,6 +17,20 @@ module Api
       def to_json(options = {})
         Jbuilder.encode do |json|
           json.code @code
+          json.sector @present.sector_key
+          json.use @present.use_key
+          json.groups @present.groups
+          json.energy_balance_group @present.energy_balance_group
+          json.attributes Qernel::ConverterApi::ATTRIBUTE_GROUPS.keys do |json, group|
+            json.set! group do |json|
+              Qernel::ConverterApi::ATTRIBUTE_GROUPS[group].each do |attr|
+                json.set! attr do |json|
+                  json.present @present.send(attr) rescue nil
+                  json.future @future.send(attr) rescue nil
+                end
+              end
+            end
+          end
         end
       end
     end
