d9627b6afa55e7983e9657e06c0acfa20a66d5c1
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-04-18 14:43:32 +0700

    Improve Grammar::Core documentation

--- app/models/gql/grammar/functions/core.rb
+++ app/models/gql/grammar/functions/core.rb
@@ -4,22 +4,39 @@ module Gql::Grammar
 
       # Shortcut for the LOOKUP and ATTR function. Also see {#LOOKUP} and {#ATTR}. 
       #
-      # == Examples
-      #
-      #   V(foo)               # => LOOKUP(foo)
-      #   V(foo, demand)       # => ATTR(LOOKUP(foo), demand)
-      #   V(foo, bar)          # => LOOKUP(foo, bar)
-      #   V(foo, bar, demand)  # => ATTR(LOOKUP(foo, bar), demand)
       #
       # Instead of converter keys you can pass anything inside V(). This 
       # works because if LOOKUP does not find a converter for an argument
       # it returns that argument itself.
-      #   
-      #   V(CARRIER(electricity), cost_per_mj) # => Number
-      #   # => ATTR( LOOKUP(CARRIER(electricity)), cost_per_mj )
       #
-      # @return [Object] the result of LOOKUP If the last argument is a converter key.
-      # @return [Object] the result of {ATTR}(LOOKUP(first_key, second_key),last_key) if the last argument is *not* a converter key.
+      # @example Lookup a converter by key
+      #   V(foo)               == LOOKUP(foo)
+      #   # => [<foo>]
+      #
+      # @example Lookup multiple converters by their keys
+      #   V(foo, bar)          == LOOKUP(foo, bar)
+      #   # => [<foo>, <bar>]
+      #
+      # @example Lookup a converter attribute
+      #   V(foo, demand)       == ATTR(LOOKUP(foo), demand)
+      #   # => 100
+      #
+      # @example Lookup multiple converter attributes
+      #   V(foo, bar, demand)  == ATTR(LOOKUP(foo, bar), demand)
+      #   # => [100, 200]
+      #  
+      # @example Nesting of LOOKUPs
+      #   V(V(foo), V(bar), demand)  == ATTR(LOOKUP(foo, LOOKUP(bar)), demand)
+      #   # => [100, 200]
+      #
+      # @example Pass arbitrary objects to V()
+      #   V(CARRIER(electricity), cost_per_mj) == ATTR( LOOKUP(CARRIER(electricity)), cost_per_mj )
+      #   # => 23.3
+      #
+      # @see #LOOKUP
+      # @see #ATTR
+      # @return [Array] the result of {LOOKUP} if the last argument is a converter key.
+      # @return [Numeric,Array] the result of {ATTR}(LOOKUP(first_key, second_key),last_key) if the last argument is *not* a converter key.
       #
       def V(*args)
         last_key = LOOKUP(args.last)
@@ -39,36 +56,50 @@ module Gql::Grammar
 
       # QUERY() or Q() returns the result of another gquery with given key.
       #
-      # @param  key The key of the gquery.
-      # @return The result of that gquery. Can be a number, list of converters, etc.
+      # @example 
+      #    Q(total_costs)
+      #
+      # @param [Symbol] key The gquery key
+      #
+      # @return [Numeric,Array] The result of that gquery. Can be a number, list of converters, etc.
       # 
       def QUERY(key)
         scope.subquery(key.to_s)
       end
       alias Q QUERY
 
-      # Lookup objects by key(s). 
+      # Lookup objects by their corresponding key(s). 
       #
-      # keys - One or more keys.
       #
-      # Returns an array of objects (or the key if no object found).
+      # @example One or multiple converters
+      #   LOOKUP(foo)               # => [<Converter foo>]
+      #   LOOKUP(foo, bar)          # => [<Converter foo>, <Converter bar>]
       #
-      # Examples
+      # @example Elements that are not converter keys are simply returned
+      #   LOOKUP(foo, 3.0)          # => [<Converter foo>, 3.0]
+      #   LOOKUP(foo, CARRIER(gas)) # => [<Converter foo>,<Carrier Gas>]
       #
-      #   LOOKUP(foo)       => [Converter<foo>]
-      #   LOOKUP(foo, bar)  => [Converter<foo>, Converter<bar>]
-      #   LOOKUP(foo, not_available)  => [Converter<foo>, "not_available"]
+      # @example nil and duplicate elements are removed
+      #   LOOKUP(foo, nil)          # => [<Converter foo>]
+      #   LOOKUP(foo, LOOKUP(foo))  # => [<Converter foo>]
       #
-      # Change request:
+      # @example Nested arrays are flattened
+      #   LOOKUP(foo, LOOKUP(bar, SECTOR(households)))  
+      #   # => [<Converter foo>,<Converter bar>,...]
       #
-      # If LOOKUP finds no converter for a key it should return something else
-      # then the name of that key. This will cause problems down the road. 
+      # @param [Array] keys One or more of:
+      #   - Converter key
+      #   - {Qernel::Base} objects, e.g. LOOKUP(CARRIER(foo))
+      #   - Arrays thereof, e.g. LOOKUP(LOOKUP(),LOOKUP())
       #
-      #   LOOKUP(foo, not_available)  => [Converter<foo>, "not_available"]
+      # @return [Array] An array of the elements of the query. 
+      #                 Duplicates and nil values are removed.
       #
       def LOOKUP(*keys)
         keys.flatten!
         keys.map! do |key| 
+          # Given LOOKUP( key_1 ) key_1 will respond_to to_sym because
+          # it comes from Rubel::Base#method_missing, which returns Symbols.
           if key.respond_to?(:to_sym)
             # prevents lookup for strings from V(.., "demand*2") or Procs V(.., foo(1))
             @scope.converters(key)
@@ -82,32 +113,49 @@ module Gql::Grammar
       end
 
       # Access attributes of one or more objects.
-      # 
-      # Examples
       #
-      # With a single attribute:
+      # === Single and composed attributes
       #
-      #   ATTR(LOOKUP(foo), demand)      # => 3
-      #   ATTR(LOOKUP(foo, bar), demand) # => [3, 5]
+      # To query a single attribute simply pass it. You can 
+      # put it inside '', "" or not. 
       #
-      # Composed attributes add "" or ''
+      # To join multiple attributes and numbers surround it
+      # with "" or ''.
       #
-      #   ATTR(LOOKUP(foo), 'demand * (3.0 + co2_free)')
-      #   ATTR(LOOKUP(foo), "demand * (3.0 + co2_free)")
+      # === GQL functions as arguments
       #
-      # Attributes derived from GQL functions:
-      #
-      #   ATTR(LOOKUP(foo), primary_demand_of(CARRIER(electricity)))
-      #
+      # You can pass a GQL function as an argument to a *single* 
+      # method. This only works if you for a single method.
       # Attributes dervied from GQL functions cannot be inside "" or ''.
-      # 
-      # @param [] objects - an object or an array of objects. 
-      # @param [Symbol, String, Proc] attr_name The String to lookup. 
       #
-      def ATTR(objects, attr_name)
-        objects = [objects] unless objects.is_a?(::Array) 
+      # @example With a single attribute:
+      #   ATTR(L(foo), demand)      # => 100
+      #   ATTR(L(foo), "demand")    # => 100
+      #   ATTR(L(foo, bar), demand) # => [100, 200]
+      #
+      # @example Composed attributes add "" or ''
+      #   ATTR(L(foo), 'demand * (3.0 + co2_free)')
+      #   ATTR(L(foo), "demand * (3.0 + co2_free)")
+      #
+      # @example GQL functions as arguments
+      #   ATTR(L(foo), primary_demand_of(CARRIER(electricity)))
+      #   ATTR(L(foo), "primary_demand_of(CARRIER(electricity))")
+      #   ATTR(L(foo), demand * AREA(number_of_households))
+      #   ATTR(L(foo), "demand * AREA(number_of_households)"))))
+      #
+      # @example Access other Qernel objects
+      #   ATTR(L(foo), 'demand * area.number_of_households')
+      #
+      # @param [Array] elements The elements to query the attributes.
+      #                E.g. LOOKUP(...), SECTOR(...)
+      # @param [Symbol,String] attr_name Single or combined attribute
+      # @return [Array] An array of the results if multiple elements given
+      # @return [Numeric] The resulting number if only one element is given
+      #
+      def ATTR(elements, attr_name)
+        elements = [elements] unless elements.is_a?(::Array) 
         
-        objects.tap(&:flatten!).map! do |a| 
+        elements.tap(&:flatten!).map! do |a| 
           a = a.respond_to?(:query) ? a.query : a
 
           if attr_name.respond_to?(:call)
@@ -117,7 +165,7 @@ module Gql::Grammar
             a.instance_eval(attr_name.to_s)
           end
         end
-        objects.length <= 1 ? (objects.first || 0.0) : objects
+        elements.length <= 1 ? (elements.first || 0.0) : elements
       end
 
     end
