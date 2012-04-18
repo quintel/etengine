f59344ef175ebadedb99eb57c2bbc1f47c85b955
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-04-18 11:31:12 +0700

    TRASH: Marshal#load_with_rails_classloader

--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -1,34 +1,3 @@
-# I THINK THIS CAN BE REMOVED NOW:
-#
-#
-# Marshal.load is a C-method built into Ruby; because it's so low-level, it
-# bypasses the full classloading chain in Ruby, in particular the #const_missing
-# hook that Rails uses to auto-load classes as they're referenced. This monkey
-# patch catches the generated exceptions, parses the message to determine the
-# offending constant name, loads the constant, and tries again.
-#
-# This solution is adapted from here:
-# http://kballcodes.com/2009/09/05/rails-memcached-a-better-solution-to-the-undefined-classmodule-problem/
-#
-class <<Marshal
-  def load_with_rails_classloader(*args)
-    begin
-      load_without_rails_classloader(*args)
-    rescue ArgumentError, NameError => e
-      if e.message =~ %r(undefined class/module)
-        const = e.message.split(' ').last
-        const.constantize
-        retry
-      else
-        raise(e)
-      end
-    end
-  end
-
-  alias_method_chain :load, :rails_classloader
-end
-
-# I THINK THIS SHOULD BE REMOVED NOW:
 class Numeric
   # rescue from 0.0/0.0 or 0.0/1.0 calculation errors
   # (0.0 / 0.0).rescue_nan
@@ -45,7 +14,6 @@ end
 
 # Extend Hash with recursive merging abilities
 class Hash
-
   # Merges self with another hash, recursively.
   #
   # This code was lovingly stolen from some random gem:
@@ -64,7 +32,6 @@ class Hash
 
     target
   end
-
 
   # From: http://www.gemtacular.com/gemdocs/cerberus-0.2.2/doc/classes/Hash.html
   # File lib/cerberus/utils.rb, line 42
