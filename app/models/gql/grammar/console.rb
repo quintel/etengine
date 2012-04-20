a7d77decf3a309afdf8ab345b463d6adca99d5db
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-04-20 12:29:14 +0700

    Refactor GQL console a bit.

--- app/models/gql/grammar/console.rb
+++ app/models/gql/grammar/console.rb
@@ -14,6 +14,39 @@ module Gql::Grammar
     include ::Gql::Grammar::Functions::Helper
     include ::Gql::Grammar::Functions::Core
     
+    # A Pry prompt that logs what user enters to a log file
+    # so it can easily be copy pasted by users.
+    #
+    # DOES NOT WORK :( couldn't make it work 
+    class LoggingPrompt
+      include Readline
+
+      def readline(prompt = "GQL: ", add_hist = true)
+        @logger ||= Logger.new('gqlconsole/prompt.log', 'daily')
+        super(prompt, add_hist).tap do |line| 
+          @logger.info(line)
+        end
+      end
+    end
+
+    # Prints string directly
+    RESULT_PRINTER = proc do |output, value|
+      if value.is_a?(String)
+        output.puts value
+      else
+        Pry::DEFAULT_PRINT.call(output, value)
+      end
+    end
+
+    # Starts the Pry console
+    def console
+      enable_code_completion
+      puts "** Console Loaded"
+      Pry.start(self, 
+                # input: LoggingPrompt.new, 
+                prompt: proc { |_, nest_level| "GQL: " },
+                print:  RESULT_PRINTER)
+    end
 
     # code completion is for the gql console.
     # it adds converter and gquery keys as methods,
@@ -21,6 +54,9 @@ module Gql::Grammar
     #
     # The methods return the key as a symbol, which is
     # the same behaviour as with method_missing.
+    #
+    # There might be a better way of doing this by hooking
+    # them up to some Pry code_completion commands.
     #
     def enable_code_completion
       self.class.enable_code_completion(self)
