585a9aff4e0e8b82c7d7d54f44606f2de660ef13
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-04-20 13:21:11 +0700

    improve Aggreate documentation

--- app/models/gql/grammar/functions/aggregate.rb
+++ app/models/gql/grammar/functions/aggregate.rb
@@ -2,31 +2,46 @@ module Gql::Grammar
   module Functions
     module Aggregate
 
-      # Returns how many (non-nil) values
+      # Returns how many values. Removes nil values, but does 
+      # not remove duplicates.
+      # 
+      # @example Basic useage
+      #   COUNT(1)              # => 1
+      #   COUNT(1,2)            # => 1
       #
-      # Examples
+      # @example with converters
+      #   COUNT(L(foo,bar))     # => 2
       #
-      #   COUNT(1)       # => 1
-      #   COUNT(1,2)     # => 2
+      # @example multiple LOOKUPs (does not remove duplicates)
+      #   COUNT(L(foo,bar), L(foo))     # => 3
+      #   # However: (LOOKUP removes duplicates)
+      #   COUNT(L(foo,bar,foo), L(f))   # => 2
+      #
+      # @example nil values are removed (do not count)
       #   COUNT(1,nil,2) # => 2
       #
+      # @param [Numeric,Array] *values one or multiple values or arrays
+      # @return [Numeric] The element count.
+      #
       def COUNT(*values)
-        flatten_compact(values).length
+        values.flatten!
+        values.compact!
+
+        values.length
       end
 
       # Returns the *first* number as a negative
       #
-      # Examples
-      #
-      #   NEG(2)     # => -2
-      #   NEG(1,2,3) # => -1
+      # @example 
+      #   NEG(2)            # => -2
+      #   NEG(1,2,3)        # => -1
       #
       # Change Request:
       #
       # Following behaviour makes more sense
       #
-      #   NEG(2)     # => -2
-      #   NEG(1,2,3) # => [-1, -2, -3]
+      #   NEG(2)            # => -2
+      #   NEG(1,2,3)        # => [-1, -2, -3]
       #
       def NEG(*args)
         args.flatten!
@@ -38,11 +53,13 @@ module Gql::Grammar
 
       # Returns the average of all number (ignores nil values).
       #
-      # Examples
+      # @example
+      #   AVG(1,2)          # => 1.5
+      #   AVG(1,2,3)        # => 2
+      #   AVG(1,nil,nil,2)  # => 1.5
       #
-      #   AVG(1,2)   # => 1.5
-      #   AVG(1,2,3) # => 2
-      #   AVG(1,nil,nil,2)   # => 1.5
+      # @param [Numeric,Array] *values one or multiple values or arrays
+      # @return [Numeric] The average of all values
       #
       def AVG(*values)
         values.flatten!
@@ -52,12 +69,14 @@ module Gql::Grammar
 
       # Returns the sum of all numbers (ignores nil values).
       #
-      # Examples
+      # @example
+      #   SUM(1,2)          # => 3
+      #   SUM(1,2,3)        # => 6
+      #   SUM(1)            # => 1
+      #   SUM(1,nil)        # => 1
       #
-      #   SUM(1,2)   # => 3
-      #   SUM(1,2,3) # => 6
-      #   SUM(1)     # => 1
-      #   SUM(1,nil) # => 1
+      # @param [Numeric,Array] *values one or multiple values or arrays
+      # @return [Numeric] The average of all values
       #
       def SUM(*args)
         args.flatten!
@@ -67,12 +86,14 @@ module Gql::Grammar
       
       # Multiplies all numbers (ignores nil values).
       #
-      # Examples
+      # @example
+      #   PRODUCT(1,2)     # => 2 (1*2)
+      #   PRODUCT(1,2,3)   # => 6 (1*2*3)
+      #   PRODUCT(1)       # => 1
+      #   PRODUCT(1,nil)   # => 1
       #
-      #   PRODUCT(1,2)   # => 2 (1*2)
-      #   PRODUCT(1,2,3) # => 6 (1*2*3)
-      #   PRODUCT(1)     # => 1
-      #   PRODUCT(1,nil) # => 1
+      # @param [Numeric,Array] *values one or multiple values or arrays
+      # @return [Numeric] The average of all values
       #
       def PRODUCT(*args)    
         args.flatten!
@@ -83,21 +104,38 @@ module Gql::Grammar
       
       # Divides the first with the second.
       #
-      # Examples
+      # @example
+      #   DIVIDE(1,2)      # => 0.5
+      #   DIVIDE(1,2,3,4)  # => 0.5 # only takes the first two numbers
+      #   DIVIDE([1,2])    # => 0.5
+      #   DIVIDE([1],[2])  # => 0.5
+      #   DIVIDE(1,2)      # => 0.5
       #
-      #   DIVIDE(1,2) #=> 0.5
-      #   DIVIDE(1,2,3,4) #=> 0.5 # only takes the first two numbers
+      # @example compare with normal arithmetics
+      #   DIVIDE(1,2)      # => 0.5
+      #   # if you divide integers you get a different outcome
+      #   1 / 2            # => 0  
+      #   # One number has to be a float
+      #   1.0 / 2          # => 0.5
+      #
+      # @example Exceptions
+      #   DIVIDE(nil, 1)   # => 0.0
+      #   DIVIDE(0.0, 1)   # => 0.0 and not NaN
+      #   DIVIDE(0,   1)   # => 0.0 and not NaN
+      #   DIVIDE(1.0,0.0)  # => Infinity
+      #
+      # @param [Numeric,Array] *values one or multiple values or arrays. But only the first two are taken.
+      # @return [Numeric] The average of all values
       #
       def DIVIDE(*values)
         a,b = values.tap(&:flatten!)
 
-        if a == 0.0 || a.nil?
+        if a == 0.0 || a == 0 || a.nil?
           0.0
         else
-          a / b
+          a.to_f / b
         end
       end
-
     end
   end
 end
