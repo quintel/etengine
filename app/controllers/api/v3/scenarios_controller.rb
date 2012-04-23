3e4be56c24b25dc3e44012b89925352802b447aa
Paolo Zaccagnini <paozac@gmail.com>
2012-04-23 15:33:52 +0200

    api: preparing v3 scenarios actions

--- app/controllers/api/v3/scenarios_controller.rb
+++ app/controllers/api/v3/scenarios_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V3
     class ScenariosController < ApplicationController
-      before_filter :find_scenario, :only => :show
+      before_filter :find_scenario, :only => [:show, :update]
 
       # GET /api/v3/scenarios/:id
       #
@@ -21,6 +21,66 @@ module Api
       def templates
         @scenarios = Scenario.in_start_menu
         render :json => @scenarios
+      end
+
+      # POST /api/v3/scenarios
+      #
+      # Creates a new scenario
+      # TODO: not finished!
+      def create
+        @scenario = Scenario.new(params[:scenario])
+        if @scenario.save
+          render :json => @scenario
+        else
+          render :json => {:errors => @scenario.errors}, :status => 403
+        end
+      end
+
+      # PUT-PATCH /api/v3/scenarios/:id
+      #
+      # This is the main scenario interaction method
+      #
+      # Parameters:
+      #
+      # - gqueries: array of gquery keys
+      # - inputs: hash {input_key: input_value}
+      # - reset: boolean (default: false)
+      #
+      # Response:
+      # {
+      #   scenario: { ... },
+      #   gqueries: {
+      #     gquery_key: {
+      #       unit: 'foo',
+      #       present: 123,
+      #       future: 456
+      #     },
+      #     gquery_other: {
+      #       errors: [ 'bad gquery!' ]
+      #     }
+      #   }
+      # }
+      #
+      def update
+        gql = Gql::Gql.new(@scenario)
+        gquery_keys = params[:gqueries] || []
+        out = Jbuilder.encode do |json|
+          json.scenario @scenario
+          json.gqueries do |json|
+            gquery_keys.each do |k|
+              json.set! k do |json|
+                if gquery = Gquery.get(k)
+                  json.unit gquery.unit
+                  json.present(gql.send :query_present, gquery)
+                  json.future(gql.send :query_future, gquery)
+                else
+                  json.errors ["Missing gquery"]
+                end
+              end
+            end
+          end
+        end
+        render :json => out
       end
 
       private
