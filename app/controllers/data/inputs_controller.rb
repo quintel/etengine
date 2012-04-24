a4bac806362513dc7214b850c56a093d089ca88f
Paolo Zaccagnini <paozac@gmail.com>
2012-04-24 12:06:31 +0200

    inputs: fixed detail page. closes #313

--- app/controllers/data/inputs_controller.rb
+++ app/controllers/data/inputs_controller.rb
@@ -1,16 +1,16 @@
 class Data::InputsController < Data::BaseController
-  before_filter :find_input, :only => [:edit, :update, :destroy]
+  before_filter :find_input, :only => [:show]
   cache_sweeper Sweepers::Input
-  
+
   def index
     @inputs = Input.by_name(params[:q]).sort_by(&:key)
   end
 
-  def edit
+  def show
   end
 
   private
-  
+
     def find_input
       @input = Input.find params[:id]
     end
