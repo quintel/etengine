1491a58b1c9a074781b2bf6aef81ab4c706e4aca
Wouter van Lelyveld <wouter.vanlelyveld@quintel.com>
2012-05-07 11:38:43 +0200

    removed and renamed unused area attributes

---
+++ db/migrate/20120507115216_rename_and_remove_insulation_building_attributes.rb
@@ -0,0 +1,21 @@
+class Renameandremoveinsulationbuildingattributes < ActiveRecord::Migration
+  def self.up
+    remove_column :areas, :buildings_heating_share_offices
+    remove_column :areas, :buildings_heating_share_other
+    remove_column :areas, :buildings_heating_share_schools
+    remove_column :areas, :insulation_level_offices
+    rename_column :areas,
+      :insulation_level_schools,
+      :insulation_level_buildings
+  end
+
+  def self.down
+    add_column :areas, :buildings_heating_share_offices
+    add_column :areas, :buildings_heating_share_other
+    add_column :areas, :buildings_heating_share_schools
+    add_column :areas, :insulation_level_offices
+    rename_column :areas,
+      :insulation_level_buildings,
+      :insulation_level_schools
+  end
+end
