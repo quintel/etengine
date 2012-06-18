5ef14f06be961642c4c7b77e01a3df503de47dca
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-18 10:41:18 +0700

    INPUT_LINKS() accepts also slots. Improved Traversal documentation

--- app/models/gql/grammar/functions/traversal.rb
+++ app/models/gql/grammar/functions/traversal.rb
@@ -1,5 +1,16 @@
+
 module Gql::Grammar
   module Functions
+    # @example Example used in description
+    #
+    #                   +---------------+        +--> gas_1
+    #                   |               |        |
+    #   loss1 <- (loss)-|   foo         |- (gas)-+--> gas_2
+    #                   |               |
+    #   heat1 <- (heat)-|  <Converter>  |- (oil)-+
+    #                   |               |        +--> oil_1
+    #                   +---------------+        
+    #    
     module Traversal
 
       # ELEMENT_AT( SORT_BY(GROUP(electricity); demand), 0) => converter with smallest demand
@@ -51,16 +62,23 @@ module Gql::Grammar
         end
       end
 
+      
+      # All links on both sides of a converter
+      #
+      # @example 
+      #   LINKS(L(foo)) # => [foo->gas_1, foo->gas_2, loss1->foo, heat1->foo]
+      #
       def LINKS(*value_terms)
         value_terms.flatten.compact.map(&:links).flatten
       end
 
+      # Get the output (to the left) slots of converter(s). 
       #
+      # @example All input slots
+      #   OUTPUT_SLOTS(foo) #=> [(loss)-foo, (heat)-foo]
       #
-      # Examples
-      #
-      #   OUTPUT_SLOTS(converter_key, carrier)
-      #   OUTPUT_SLOTS(LOOKUP(converter_key), carrier)
+      # @example All input slots
+      #   OUTPUT_SLOTS(foo, loss) #=> [(loss)-foo]
       #
       def OUTPUT_SLOTS(*args)
         carrier = args.pop
@@ -68,8 +86,13 @@ module Gql::Grammar
         flatten_uniq converters.compact.map{|c| carrier ? c.output(carrier.to_sym) : c.outputs}
       end
 
-      # INPUT_SLOTS(converter_key; carrier)
-      # INPUT_SLOTS(V(converter_key); carrier)
+      # Get the input (to the right) slots of converter(s). 
+      #  
+      # @example All input slots
+      #   INPUT_SLOTS(foo) #=> [foo-(gas), foo-(oil)]
+      #
+      # @example All input slots
+      #   INPUT_SLOTS(foo, gas) #=> [foo-(gas)]
       #
       def INPUT_SLOTS(*args)
         carrier = args.pop
@@ -77,8 +100,30 @@ module Gql::Grammar
         flatten_uniq converters.compact.map{|c| carrier ? c.input(carrier.to_sym) : c.outputs}
       end
 
+      # @example All input links
+      #   INPUT_LINKS(L(foo))
+      #
+      # @example All input links with a constraint
+      #   INPUT_LINKS(L(foo), "share?")
+      #   INPUT_LINKS(L(foo), "flexible?")
+      #   INPUT_LINKS(L(foo), "flexible? && share >= 1.0")
+      #
+      # @example All input links of a given carrier/slot
+      #    INPUT_LINKS(INPUT_SLOTS(foo, oil)) # => [foo->oil_1]
+      #
+      # @example Input links of multiple converters
+      #   INPUT_LINKS(L(foo, bar))
+      #
       def INPUT_LINKS(value_terms, arguments = [])
-        links = flatten_uniq(value_terms.tap(&:flatten!).map(&:input_links))
+        links = flatten_uniq(value_terms.tap(&:flatten!)).map do |obj|
+          if obj.respond_to?(:input_links) # it's a converter
+            obj.input_links
+          elsif obj.respond_to?(:links) # it's a slot
+            obj.links
+          end
+        end
+        links.flatten!
+
         if arguments.present?
           inst_eval = arguments.is_a?(Array) ? arguments.first : arguments
           links.select!{|link| link.instance_eval(inst_eval.to_s) } 
@@ -86,8 +131,30 @@ module Gql::Grammar
         links
       end
 
+      # @example All input links
+      #   OUTPUT_LINKS(L(foo))
+      #
+      # @example All input links with a constraint
+      #   OUTPUT_LINKS(L(foo), "share?")
+      #   OUTPUT_LINKS(L(foo), "flexible?")
+      #   OUTPUT_LINKS(L(foo), "flexible? && share >= 1.0")
+      #
+      # @example All output links of a given carrier/slot
+      #    OUTPUT_LINKS(OUTPUT_SLOTS(foo, heat)) # => [heat->foo]
+      #
+      # @example Input links of multiple converters
+      #   OUTPUT_LINKS(L(foo, bar))
+      #
       def OUTPUT_LINKS(value_terms, arguments = [])
-        links = flatten_uniq(value_terms.tap(&:flatten!).map(&:output_links))
+        links = flatten_uniq(value_terms.tap(&:flatten!)).map do |obj|
+          if obj.respond_to?(:output_links) # it's a converter
+            obj.input_links
+          elsif obj.respond_to?(:links) # it's a slot
+            obj.links
+          end
+        end
+        links.flatten!
+        
         if arguments.present?
           inst_eval = arguments.is_a?(Array) ? arguments.first : arguments
           links.select!{|link| link.instance_eval(inst_eval.to_s) } 
