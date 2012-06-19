84b392e88658fd6cde4af6c23e6d1159419ce9cb
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-19 09:27:09 +0700

    Improved be_within matcher

--- spec/support/be_near_matcher.rb
+++ spec/support/be_near_matcher.rb
@@ -24,43 +24,57 @@ RSpec::Matchers.define :be_within_a_percent do |expected|
   end
 end
 
+RSpec::Matchers.define :increase do
+  match{ |actual| !actual.nil? && actual > 0.0 }
+
+  failure_message_for_should do |actual|
+    if actual == 0.0
+      "expected an increase, but it decreased by: #{actual.inspect}"
+    else
+      "expected an increase, but decreased by #{actual.inspect}"
+    end
+  end
+end
+
+RSpec::Matchers.define :decrease do
+  match { |actual| !actual.nil? && actual < 0.0 }
+
+  failure_message_for_should do |actual|
+    if actual == 0.0
+      "expected a decrease, but it stayed the same: #{actual.inspect}"
+    else
+      "expected a decrease, but increase by #{actual.inspect}"
+    end
+  end
+end
+
 RSpec::Matchers.define :be_within do |expected, percent|
   match do |actual|
-    if actual.respond_to?(:finite?) && !actual.finite?
-      expected.respond_to?(:finite?) && !expected.finite?
-    elsif !actual.respond_to?(:to_f)      
-      actual == expected
-    elsif expected.to_f == 0.0 && actual.to_f.abs < 0.001
-      true
+    if expected.nil? 
+      actual.nil?
     else
-      (((actual.to_f.round(3) / expected) - 1.0) * 100.0).abs < percent
+      factor   = percent / 100.0
+
+      if expected == 0.0 # allow for some rounding errors
+        min_max = [-0.0001, 0.0001]
+      else
+        min_max = [
+          expected * (1.0 - factor), 
+          expected * (1.0 + factor)
+        ].sort
+      end
+      
+      range = Range.new *min_max
+      range.include?(actual)
     end
   end
 
   failure_message_for_should do |actual|
-    "Currently: #{actual.andand.round(3).inspect}, but used to be #{expected}"
+    "Got: #{actual.andand.round(3).inspect}. But should be near #{expected}"
   end
 
   failure_message_for_should_not do |actual|
-    "Currently: #{actual.round(3)} But should not to be near #{expected}"
-  end
-
-  description do
-    "be near #{expected}"
-  end
-end
-
-RSpec::Matchers.define :be_near do |expected|
-  match do |actual|
-    (actual - expected).abs < 0.01
-  end
-
-  failure_message_for_should do |actual|
-    "expected #{actual} to be near #{expected}"
-  end
-
-  failure_message_for_should_not do |actual|
-    "expected #{actual} not to be near #{expected}"
+    "Got: #{actual.round(3)}. But should not to be near #{expected}"
   end
 
   description do
