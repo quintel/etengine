56f97f1c1a9515aaa76cab35b83f76c08b249334
Paolo Zaccagnini <paozac@gmail.com>
2012-06-20 11:01:38 +0200

    api: get area data from etsource

--- app/controllers/api/areas_controller.rb
+++ app/controllers/api/areas_controller.rb
@@ -1,21 +1,7 @@
 class Api::AreasController < Api::BaseController
   respond_to :xml
 
-  before_filter :find_area, :only => [:show]
-
-  def index
-    scope = Area.scoped
-    scope = scope.country(params[:country]) if params[:country]
-    respond_with(@areas = scope.all)
-  end
-
   def show
-    respond_with(@area)
+    respond_with(@area = Area.get(params[:id]))
   end
-
-  private
-  
-    def find_area
-      @area = Area.find(params[:id])
-    end
 end
