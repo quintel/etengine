5434a768ad6a7d1a5f2b7f21db516646c90540a4
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-20 17:02:09 +0700

    some code got lost

--- app/models/scenario/scenario_area.rb
+++ app/models/scenario/scenario_area.rb
@@ -1,11 +1,14 @@
 # Area related methods for scenario
 #
 class Scenario < ActiveRecord::Base
+  def area
+    Area.get(area_code)
+  end
+
   def area_input_values
-    area = Area.get(area_code)
     hash = area[:input_values]
     if hash.present?
-      YAML::load(hash)
+      YAML::load(hash).with_indifferent_access
     else
       {}
     end
