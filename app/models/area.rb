8b52e5bdf0e88631ac0f3f98e5789e48182d6847
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-20 17:09:15 +0700

    use hash instead of object. safer

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,7 +3,7 @@ class Area < ActiveRecord::Base
   
   def self.get(area_code)
     attributes = Etsource::Loader.instance.area_attributes(area_code)
-    new(attributes.with_indifferent_access)
+    attributes.with_indifferent_access
   end
   
 end
