af5143955eaa10a9b98870ce22361099714c8bf4
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-25 16:27:57 +0700

    create spec/requests/user_values_spec.rb fix etsource fixtures.

---
+++ spec/requests/user_values_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+
+# Uses the etsource defined in spec/fixtures/etsource
+#
+describe "api_scenario life cycle" do
+  before(:all) do 
+    NastyCache.instance.expire!
+    Etsource::Base.loader('spec/fixtures/etsource')
+  end
+
+  it "should return user_values" do
+    post 'api/v2/api_scenarios.json'
+
+    scenario     = JSON.parse(response.body)['scenario']
+    settings_url = "api/v2/api_scenarios/#{scenario['id']}/user_values.json"
+    api_url      = "api/v2/api_scenarios/#{scenario['id']}.json"
+
+    # ----- no updates --------------------------------------------------------
+
+    get settings_url
+    settings = JSON.parse(response.body)
+    settings["1"]["max_value"].should == 100
+    settings["1"]["min_value"].should == 0
+    settings["1"]["start_value"].should == 50
+    settings["2"]["start_value"].should == 100*100 # start_value_gql*factor
+    
+    # ----- updating 2 --------------------------------------------------------
+    
+    get api_url, :input => {'2' => '500'}
+    get settings_url
+    settings = JSON.parse(response.body)
+    settings["2"]["start_value"].should == 100*100
+    settings["2"]["user_value"].should == 500
+
+    # ----- updating again ----------------------------------------------------
+    
+    get api_url, :input => {'2' => '300', '1' => '50'}
+    get settings_url
+    settings = JSON.parse(response.body)
+    settings["2"]["user_value"].should == 300
+    settings["1"]["user_value"].should == 50
+    
+    # ----- updating going over min/max ----------------------------------------------------
+    # api simply accepts without further checking :(
+    #
+    get api_url, :input => {'1' => '-100'}
+    get settings_url
+    settings = JSON.parse(response.body)
+    settings["1"]["user_value"].should == -100
+
+
+  end
+
+
+end
