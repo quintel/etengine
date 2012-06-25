0f1788cf90b99f8fbe838c7169e21967f092fd4c
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-25 10:27:19 +0700

    TRASH: sweepers

--- app/models/sweepers/input.rb
+++
@@ -1,9 +0,0 @@
-module Sweepers
-  class Input < ActionController::Caching::Sweeper
-    observe ::Input
-    
-    def after_save(record)
-      expire_fragment 'inputs_autocomplete_map_cache'
-    end
-  end
-end
