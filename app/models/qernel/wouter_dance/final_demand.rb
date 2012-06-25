c034b6be2579e5968e48379e3961ab9179213d05
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-06-25 21:22:26 +0700

    added final_demand_of(:carrier1, :carrier2)

--- app/models/qernel/wouter_dance/final_demand.rb
+++ app/models/qernel/wouter_dance/final_demand.rb
@@ -7,18 +7,32 @@ module Qernel::WouterDance::FinalDemand
     end
   end
 
+  def final_demand_of(*carriers)
+    carriers.flatten.map do |c| 
+      key = c.respond_to?(:key) ? c.key : c
+      final_demand_of_carrier(key) 
+    end.compact.sum
+  end
+
   def final_demand_of_carrier(carrier_key)
     factor = wouter_dance(:final_demand_factor_of_carrier, nil, nil, carrier_key)
     (self.demand || 0.0) * factor
   end
 
-  def final_demand_factor_of_carrier(link, carrier_key, ruby18fix = nil)
+  def final_demand_factor_of_carrier(link, carrier_key)
+    link ||= output_links.first # in case we query a left-most converter
 
+    if link && final_demand_cbs?
+      link.carrier.key == carrier_key ? 1.0 : 0.0
+    else
+      nil
+    end
   end
   
   def final_demand_factor(link,ruby18fix = nil)
-    return 1.0 if final_demand_cbs?
-    return 0.0 if right_dead_end?
-    nil
+    if    final_demand_cbs?  then 1.0
+    elsif right_dead_end?    then 0.0
+    else                          nil
+    end
   end
 end
