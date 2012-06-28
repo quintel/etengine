aca5a9d5fc1e9a2ebfb43040bf518f46ce3e674b
Paolo Zaccagnini <paozac@gmail.com>
2012-06-28 14:28:00 +0200

    api: reintroduced areas#index
    The backcasting module was using it

--- app/controllers/api/areas_controller.rb
+++ app/controllers/api/areas_controller.rb
@@ -1,6 +1,10 @@
 class Api::AreasController < Api::BaseController
   respond_to :xml
 
+  def index
+    respond_with(Etsource::Dataset.region_codes.map{|c| Area.get c})
+  end
+
   def show
     respond_with(@area = Area.get(params[:id]))
   end
