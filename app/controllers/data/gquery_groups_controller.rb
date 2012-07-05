df2a0a513573d52b36472e5b3ae7892275c9f7a8
Paolo Zaccagnini <paozac@gmail.com>
2012-07-05 13:33:43 +0200

    admin: fixed gquery groups page. Closes #353

--- app/controllers/data/gquery_groups_controller.rb
+++ app/controllers/data/gquery_groups_controller.rb
@@ -2,9 +2,9 @@ class Data::GqueryGroupsController < Data::BaseController
   def index
     @gquery_groups = GqueryGroup.all
   end
-  
+
   def show
     @gquery_group = GqueryGroup.find_by_group_key(params[:id])
-    redirect_to data_gqueries_path(:group_ids => @gquery_group.id)
+    redirect_to data_gqueries_path(:group => @gquery_group.group_key)
   end
 end
