a09c6b53503948d97309903908afe4b25aead18a
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-07-17 20:07:10 +0700

    layout of debugger

--- app/helpers/debug_helper.rb
+++ app/helpers/debug_helper.rb
@@ -23,9 +23,6 @@ module DebugHelper
 
   def log_tree(logs = @logs)
     tree = Qernel::Logger.to_tree(logs)
-    # haml_tag :pre do
-    #   haml_concat tree.inspect
-    # end
     log_subtree(tree)
   end
 
@@ -37,40 +34,45 @@ module DebugHelper
           type      = parent[:type]
           attr_name = parent[:attr_name]
           value     = parent[:value]
-          
-          haml_tag "span.label.unfold_toggle", type.to_s, :class => LABELS[type]
-          
-          if type == :method
-            haml_tag 'a.attr_name', attr_name.to_s, 
-              :rel  => 'modal', 
-              :href => '#',
-              :data => {
-                :target        => "##{attr_name}", 
-                :"toggle" => :modal
-              }
 
-            @method_definitions << attr_name
-          else
-            haml_tag 'span.attr_name', attr_name.to_s
-          end
-          haml_tag 'span.pull-right' do
-            haml_tag 'a.fold_all', 'fold'
-            haml_tag 'a.unfold_all', 'unfold'
-            haml_tag 'a.unfold_1', '1'
-            haml_tag 'a.unfold_2', '2'
-          end
-          value = value.first if value.is_a?(Array) and value.length == 1
-          
-          if value.is_a?(Array) 
-            haml_tag 'strong.pull-right', "#{value.length} #"
-          else
-            haml_tag 'strong.pull-right', auto_number(value)
+
+          haml_tag :p, :class => type do
+            haml_tag "span.label.unfold_toggle", type.to_s, :class => LABELS[type]
+            haml_tag 'span' do
+              haml_tag 'a.fold_all', '-', :href => '#'
+              haml_tag 'a.unfold_all', '+', :href => '#'
+              # haml_tag 'a.unfold_1', '1', :href => '#'
+              # haml_tag 'a.unfold_2', '2', :href => '#'
+            end
+            
+            if type == :method
+              haml_tag 'a.attr_name', attr_name.to_s, 
+                :rel  => 'modal', 
+                :href => '#',
+                :data => {
+                  :target        => "##{attr_name}", 
+                  :"toggle" => :modal
+                }
+
+              @method_definitions << attr_name
+            else
+              haml_tag 'span.attr_name', attr_name.to_s
+            end
+            value = value.first if value.is_a?(Array) and value.length == 1
+            
+            if value.is_a?(Array) 
+              haml_tag 'strong.pull-right', "#{value.length} #"
+            else
+              haml_tag 'strong.pull-right', auto_number(value)
+            end
           end
 
           if parent[:key] == 'Q'
             gquery =  Gquery.get(parent[:attr_name].split(' ').last)
             haml_tag 'strong.pull-right', (gquery.unit || '-')+" "
-            haml_tag :pre, gquery.query
+            with_tabs(0) do
+              haml_tag :pre, gquery.query
+            end
           end
 
           log_subtree(children) unless children.nil?
