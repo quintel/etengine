74989f1dc4f0f4bd9f5456990a5fee2468f87568
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-07-25 15:58:59 +0700

    Remove MethodMetaData methods

--- app/models/qernel/method_meta_data.rb
+++ app/models/qernel/method_meta_data.rb
@@ -13,71 +13,11 @@ module MethodMetaData
     values.empty? ? nil : values.sum
   end
 
-  # Returns the attribute names of given method.
-  # if no method_name given return values of required by the caller_method
-  #
-  # @return [Array<Symbol>] required attribute_names of given method
-  #
-  def required_attributes_for_method(method_name = nil)
-    method_name ||= caller_method
-    self.class.required_attributes_for_method(method_name)
-  end
-
-  # Returns the values of given method.
-  # if no method_name given return values of required by the caller_method
-  #
-  # @return [Array<Float>] required values of given method
-  #
-  def values_for_method(method_name = nil)
-    # TODO remove caller_method as it is very slow
-    method_name ||= caller_method
-    required_attributes_for_method(method_name).
-      map{|attr_key| self.send(attr_key) }
-  end
-
-  # Is any of the required attributes nil.
-  #
-  # @return [true,false]
-  #
-  def required_attributes_contain_nil?(method_name = nil)
-    # TODO remove caller_method as it is very slow
-    method_name ||= caller_method
-    values_for_method(method_name).any?(&:nil?)
-  end
-
-
 
   module ClassMethods
-    def register_calculation_method(keys)
-      registered_calculation_methods = [registered_calculation_methods, keys].flatten
-    end
-
-      def registered_calculation_methods
-        @@registered_calculation_methods ||= []
-      end
-
-      # Hash of :method_name => required_attributes_for_method
-      #
-      def required_attributes
-        @@required_attributes ||= {}
-      end    
-
+    # used now in api/v3/converter.rb. Implement this when needed.
     def calculation_methods
-      required_attributes.keys + registered_calculation_methods
-    end
-
-    # @param method_name [String,Symbol]
-    # @return [Array<Symbol>] attributes/methods used by given method
-    #
-    def required_attributes_for_method(method_name)
-      required_attributes[method_name.to_sym] || []
-    end
-
-    # @param method_name [Symbol]
-    # @param attributes [Array<Symbol>]
-    #
-    def attributes_required_for(method_name, attributes)
-      required_attributes[method_name] = attributes
+      []
     end
   end
 
