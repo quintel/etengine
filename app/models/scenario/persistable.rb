417ad849a394e73ab3c30bb9e23e14b63b235407
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-07-31 16:30:33 +0700

    Extend spec/requests, and fix Scenario#reset!. Fixes #382

--- app/models/scenario/persistable.rb
+++ app/models/scenario/persistable.rb
@@ -10,8 +10,10 @@ module Scenario::Persistable
   #
   def reset!
     self.user_values = {}
-    @inputs_present = {}
-    @inputs_future = {}
+    # @inputs_present/future have to be nil, not an empty hash. otherwise
+    # the memoized def inputs_present will not pick up the changes. 
+    @inputs_present  = nil
+    @inputs_future   = nil
     self.use_fce = false
   end
 
