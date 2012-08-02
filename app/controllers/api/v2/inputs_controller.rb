75334fda879d165899bb045f7c87b0c65a28a234
Paolo Zaccagnini <paozac@gmail.com>
2012-08-02 12:01:27 +0200

    api v2: fixed inputs request

--- app/controllers/api/v2/inputs_controller.rb
+++ app/controllers/api/v2/inputs_controller.rb
@@ -3,8 +3,10 @@ module Api
     class InputsController < BaseController
       respond_to :xml
 
+      # This action is only used by the energymixer. It returns a minimal list
+      # of the available inputs
       def index
-        respond_with(@inputs = Input.all)
+        respond_with(@inputs = Input.all.map(&:basic_attributes))
       end
     end
   end
