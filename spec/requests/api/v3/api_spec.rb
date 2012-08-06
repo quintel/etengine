60a53833344cc14817adb4b96c3049bbbd1ec5d1
Paolo Zaccagnini <paozac@gmail.com>
2012-08-06 17:49:12 +0200

    some api v3 tests - INCOMPLETE

---
+++ spec/requests/api/v3/api_spec.rb
@@ -0,0 +1,71 @@
+require "spec_helper"
+
+# Uses the etsource defined in spec/fixtures/etsource
+#
+describe "API v3scenario life cycle" do
+  before(:all) do
+    NastyCache.instance.expire!
+    Etsource::Base.loader('spec/fixtures/etsource')
+  end
+
+  it "should create, update, persist" do
+    post 'api/v3/scenarios', :scenario => {:area_code => 'nl', :end_year => 2040}
+
+    scenario = JSON.parse(response.body)
+    id = scenario['id']
+    url = "api/v3/scenarios/#{id}"
+
+    scenario['id'].should_not be_blank
+    scenario['area_code'].should == 'nl'
+    scenario['end_year'].should == 2040
+
+    # ----- no updates --------------------------------------------------------
+
+    put url, :gqueries => ['bar_demand']
+
+    puts response.body
+    result = JSON.parse(response.body)['gqueries']
+    result['bar_demand']['present'].should == 60.0
+    result['bar_demand']['future'].should == 60.0
+
+    # ----- update ------------------------------------------------------------
+
+    put url,
+        :scenario => {:user_values => {'3' => '120'} },
+        :gqueries => %w[foo_demand bar_demand]
+
+    result = JSON.parse(response.body)['gqueries']
+    result['foo_demand']['future'].should == 120.0
+    result['bar_demand']['future'].should == 120.0*0.6
+
+    # ----- reset ------------------------------------------------------------
+
+    put url,
+        :scenario => {:user_values => {'3' => 'reset'} },
+        :gqueries => %w[foo_demand bar_demand]
+
+    result = JSON.parse(response.body)['gqueries']
+    result['bar_demand']['future'].should == 60.0
+    result['bar_demand']['future'].should == 60.0
+
+    # ----- updating another --------------------------------------------------
+
+    put url,
+        :scenario => {:user_values => {'2' => '20', '3' => '120'}},
+        :gqueries => %w[foo_demand bar_demand]
+
+    result = JSON.parse(response.body)['gqueries']
+    result['foo_demand']['future'].should == 120.0
+    result['bar_demand']['future'].should == 120.0*0.6 + 20
+
+    # ----- updating 3 again --------------------------------------------------
+
+    put url, :scenario => {:user_values => {'3' => '180'}},
+        :gqueries => %w[foo_demand bar_demand]
+
+    result = JSON.parse(response.body)['gqueries']
+    result['foo_demand']['present'].should == 100.0
+    result['foo_demand']['future'].should == 180.0
+    result['bar_demand']['future'].should == 180.0*0.6 + 20
+  end
+end
