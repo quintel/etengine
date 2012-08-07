704bd5c59c5dd85acccf4b6c90135b32989041bf
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-08-07 15:04:59 +0200

    rename max_demand_recursive to max_demand_recursive! (bang) to make it more obvious that it overwrites something

--- app/models/qernel/plugins/max_demand_recursive.rb
+++ app/models/qernel/plugins/max_demand_recursive.rb
@@ -10,17 +10,16 @@ module Qernel::Plugins
     extend ActiveSupport::Concern
 
     included do |variable|
-      set_callback :calculate, :after,  :calculate_max_demand_recursive
+      set_callback :calculate, :before, :calculate_max_demand_recursive
     end
 
     def calculate_max_demand_recursive
       instrument("qernel.calculate_max_demand_recursive") do
         converters.each do |c|
-          if c.query.max_demand == 'recursive'
-            c.query.max_demand = c.max_demand_recursive
-          end
+          c.max_demand_recursive! if c.query.max_demand == 'recursive'
         end
       end
     end
+    
   end
 end
