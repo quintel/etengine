86991aef38c14cc70f15fa0f6e9835a4ccb1c212
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-08-08 09:57:47 +0200

    Fix dropdowns in navbar. upgrade them to use api/v3. Fixes #408

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,22 @@
 //
 //= require admin.interface
 
+$(document).ready(function() {
+  $("#api_scenario_selector select").change(function(e){
+    e.preventDefault();
+    var url = location.pathname;
+    var tokens = url.split('/');
+    tokens[2] = $("#api_scenario_id").val();
 
+    var new_url = tokens.join('/');
+    location.href = new_url;
+  });
+
+  $("#area_code_selector select").change(function(e){
+    e.preventDefault();
+    var params = {scenario: {area_code: $("#area_code_selector select").val()}};
+    $.post('/api/v3/scenarios/', params, function(data, _ts, jqXHR) {
+      location.href = "/data/"+data.id;
+    })
+  });
+})
