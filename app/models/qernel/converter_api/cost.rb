d17452b5a1f7b8ded3bc33cb56d290727aaa698f
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-08-13 16:22:06 +0200

    a bit more extensive test data

--- app/models/qernel/converter_api/cost.rb
+++ app/models/qernel/converter_api/cost.rb
@@ -1,8 +1,8 @@
 class Qernel::ConverterApi
 
-  def one_more_time
-    function(:one_more_time) do
-      7
+  def average_cost_per_year
+    function(:average_cost_per_year) do
+      total_real_costs / lifetime
     end
   end
 
