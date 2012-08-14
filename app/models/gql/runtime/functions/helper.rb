aa0cc7bdda376495e544da40411e404de77350f3
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-08-14 16:42:23 +0200

    Add calculation audit. Closes #318

--- app/models/gql/runtime/functions/helper.rb
+++ app/models/gql/runtime/functions/helper.rb
@@ -2,12 +2,12 @@ module Gql::Runtime
   module Functions
     module Helper
 
-      def OBSERVE(*objects, arguments)
+      def OBSERVE_SET(*objects, arguments)
         keys = arguments # OBSERVE(..., :demand)
         if arguments.is_a?(Hash) # OBSERVE(..., keys: [:demand, :share], include: [:links])
           keys     = arguments[:keys]
           includes = [arguments[:include]].flatten
-          
+
           if includes.include?(:links)
             objects += flatten_uniq(objects).map{|o| [o.input_links, o.output_links]}
           end
@@ -15,7 +15,7 @@ module Gql::Runtime
         keys ||= [:demand, :value]
 
         flatten_uniq(objects).each do |obj|
-          obj.dataset_observe keys
+          obj.dataset_observe_set keys
         end
       end
 
@@ -24,7 +24,7 @@ module Gql::Runtime
         if arguments.is_a?(Hash) # OBSERVE(..., keys: [:demand, :share], include: [:links])
           keys     = arguments[:keys]
           includes = [arguments[:include]].flatten
-          
+
           if includes.include?(:links)
             objects += flatten_uniq(objects).map{|o| [o.input_links, o.output_links]}
           end
@@ -47,14 +47,14 @@ module Gql::Runtime
       # TXT_TABLE( converters ; attribute_1 ; attribute_2 ; ... )
       #
       # TXT_TABLE(
-      #   SORT_BY(V(G(electricity_production));merit_order_end); 
+      #   SORT_BY(V(G(electricity_production));merit_order_end);
       #   key; merit_order_start; merit_order_end; full_load_hours
       # )
       #
       def TXT_TABLE(objects, *arguments)
         rows = [arguments]
-        rows += flatten_uniq(objects).map do |obj| 
-          arguments.map{|a| obj.query.instance_eval(a.to_s) } 
+        rows += flatten_uniq(objects).map do |obj|
+          arguments.map{|a| obj.query.instance_eval(a.to_s) }
         end
         rows.to_table(:first_row_is_head => true).to_s
       end
