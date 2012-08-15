ffba5363c7fd55ddf06b3d7f2a1992c5d64c2b08
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-08-15 11:45:50 +0200

    adding spec on small numbers

--- app/models/qernel/converter_api/cost.rb
+++ app/models/qernel/converter_api/cost.rb
@@ -2,11 +2,7 @@ class Qernel::ConverterApi
 
   def average_cost_per_year
     function(:average_cost_per_year) do
-      if total_real_costs && lifetime && lifetime > 0
-        total_real_costs / lifetime
-      else
-        0
-      end
+      total_real_costs / lifetime rescue 0
     end
   end
 
