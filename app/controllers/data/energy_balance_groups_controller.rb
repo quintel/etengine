585801d32a5bb30becc9dda564a5ebecb5fd29e9
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-08-16 17:46:13 +0200

    data/latest/scenarios/current and better sorting for energy balance groups.

--- app/controllers/data/energy_balance_groups_controller.rb
+++ app/controllers/data/energy_balance_groups_controller.rb
@@ -2,11 +2,11 @@ class Data::EnergyBalanceGroupsController < Data::BaseController
   layout 'application'
 
   before_filter :find_item, :only => [:show, :edit, :update, :destroy]
-  
+
   def index
-    @energy_balance_groups = EnergyBalanceGroup.order('name').all
+    @energy_balance_groups = EnergyBalanceGroup.order('id').all
   end
-  
+
   def show
     @energy_balance_group = EnergyBalanceGroup.find params[:id]
   end
