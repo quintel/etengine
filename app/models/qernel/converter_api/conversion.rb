215b65690f08f8156f417cd68a089742fe00adc7
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-08-27 16:11:22 +0200

    not using class_eval, but just spell out the methods. Easier to do git grep on!

--- app/models/qernel/converter_api/conversion.rb
+++ app/models/qernel/converter_api/conversion.rb
@@ -7,21 +7,32 @@ class Qernel::ConverterApi
   #   total_costs_per(:converter)
   #   => 129721.8
   #
-  %w[
-    total_costs
-    fixed_costs
-    cost_of_capital
-    depreciation_costs
-    variable_costs
-    fuel_costs
-    co2_emissions_costs
-    variable_operations_and_maintenance_costs
-  ].each do |method_name|
-    class_eval <<-RUBY, __FILE__, __LINE__ + 1
-      def #{ method_name }_per(unit)
-        convert_to( #{ method_name }, unit )
-      end
-    RUBY
+  def total_costs_per(unit)
+    convert_to total_costs, unit
+  end
+
+  def fixed_costs_per(unit)
+    convert_to fixed_costs, unit
+  end
+
+  def depreciation_costs_per(unit)
+    convert_to depreciation_costs, unit
+  end
+
+  def variable_costs_per(unit)
+    convert_to variable_costs, unit
+  end
+
+  def fuel_costs_per(unit)
+    convert_to fuel_costs, unit
+  end
+
+  def co2_emissions_costs_per(unit)
+    convert_to co2_emissions_costs, unit
+  end
+
+  def variable_operations_and_maintenance_costs_per(unit)
+    convert_to variable_operations_and_maintenance_costs, unit
   end
 
   #######
@@ -42,28 +53,28 @@ class Qernel::ConverterApi
   #   => 12972.12
   #
   def convert_to(cost, unit)
-    case
+    case unit
 
     # Plant and Converter
-    when unit == :plant
+    when :plant
       cost
-    when unit == :converter
+    when :converter
       cost * real_number_of_units
 
     # MW
-    when unit == :mw_input
+    when :mw_input
       cost / effective_input_capacity
-    when unit == :mw_electricity
+    when :mw_electricity
       cost / output_capacity_electricity
-    when unit == :mw_heat
+    when :mw_heat
       cost / output_capacity_heat
 
     # MWh
-    when unit == :mwh_input
+    when :mwh_input
       cost / demand / SECS_PER_HOUR / real_number_of_units
-    when unit == :mwh_electricity
+    when :mwh_electricity
       cost / output_of_electricity / SECS_PER_HOUR / real_number_of_units
-    when unit == :mwh_heat
+    when :mwh_heat
       cost / output_of_heat_carriers / SECS_PER_HOUR / real_number_of_units
 
     # Some other unit that is unknown
