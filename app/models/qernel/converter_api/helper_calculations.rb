2841a261c7613f9717fc8cc2acf460102677be85
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-08-28 15:06:06 +0200

    adding helper_calculation from master fixing merit_order calc

--- app/models/qernel/converter_api/helper_calculations.rb
+++ app/models/qernel/converter_api/helper_calculations.rb
@@ -1,15 +1,24 @@
 class Qernel::ConverterApi
+  
+  # How many units are required to fill demand.
+  def number_of_units
+    return 0 if typical_input_capacity == 0 or typical_input_capacity.nil?
+    function(:number_of_units) do
+      mw_input_capacity / typical_input_capacity
+    end
+  end
 
-  # DEBT: Remove this method if no longer used
+  # RD: This method is used by the number_of_units_update in multicommand factory
+  # I dont think this belongs here!
   def number_of_units=(val)
     dataset_set(:number_of_units, val)
   end
 
-  def total_land_use
+  def total_land_use 
     return nil if [number_of_units, land_use_per_unit].any?(&:nil?)
     number_of_units * land_use_per_unit
   end
-
+  
   # Returns the number of households which are supplied by the energy created
   # in each unit. Used in DemandDriven converters for cases where a converter
   # may supply more than one building.
