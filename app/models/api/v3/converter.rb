3748da328a34b24312b49081ef5a6c9608bb71db
Paolo Zaccagnini <paozac@gmail.com>
2012-09-04 15:50:50 +0200

    API: fixed converter detail action

--- app/models/api/v3/converter.rb
+++ app/models/api/v3/converter.rb
@@ -24,13 +24,16 @@ module Api
           json.attributes do |json|
             Qernel::ConverterApi::ATTRIBUTE_GROUPS.each_pair do |group, attrs|
               json.set! group do |json|
-                attrs.each do |attr|
+                attrs.each do |attr, attr_data|
+                  desc, unit = attr_data
                   pres = format_value(@present, attr)
                   fut = format_value(@future, attr)
                   next unless (pres || fut)
                   json.set! attr do |json|
                     json.present pres
                     json.future fut
+                    json.unit unit
+                    json.desc desc
                   end
                 end
               end
@@ -44,6 +47,7 @@ module Api
               json.set!(attr) do |json|
                 json.present pres
                 json.future fut
+                json.unit Qernel::ConverterApi.unit_for_calculation(attr)
               end
             end
           end
