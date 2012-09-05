287ded2b4e062f2619b3dcc437f825ee9fce74b5
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-09-05 20:05:30 +0700

    more improvements and specs for merit_order

--- spec/models/qernel/plugins/merit_order_spec.rb
+++ spec/models/qernel/plugins/merit_order_spec.rb
@@ -1,32 +1,112 @@
 require 'spec_helper'
 
 module Qernel::Plugins::MeritOrder
-  # describe "MeritOrder" do
-  #   before :all do
-  #     NastyCache.instance.expire!
-  #     Etsource::Base.loader('spec/fixtures/etsource')
-  #   end
-  # end
+  describe "MeritOrder" do
+    before :all do
+      NastyCache.instance.expire!
+      Etsource::Base.loader('spec/fixtures/etsource')
+    end
+
+    describe "#calculate_merit_order" do
+      before do
+        @graph = Scenario.default.gql.future_graph
+
+        # Take some random converters and assign them required variables
+        @converters = @graph.converters[0...4]
+        [50, 0, nil, 200].each_with_index do |inst_cap, i|
+          @converters[i].query.stub!(:installed_production_capacity_in_mw_electricity).and_return inst_cap
+          @converters[i].query.stub!(:availability).and_return 1.0
+        end
+
+        @graph.stub!(:group_merit_order).and_return(@converters)
+        @graph.stub!(:converters_by_total_variable_cost).and_return(@converters.map(&:query))
+        @graph.calculate_merit_order
+      end
+
+      it "should correctly sum merit_order_start" do
+        @converters[0].query.merit_order_start.should ==  0
+        @converters[1].query.merit_order_start.should == 50
+        @converters[2].query.merit_order_start.should == 50
+        @converters[3].query.merit_order_start.should == 50
+      end
+
+      it "should correctly sum merit_order_end" do
+        @converters[0].query.merit_order_end.should ==  50
+        @converters[1].query.merit_order_end.should ==  50
+        @converters[2].query.merit_order_end.should ==  50
+        @converters[3].query.merit_order_end.should == 250
+      end
+
+      it "should correctly count positions" do
+        @converters[0].query.merit_order_position.should == 1
+        @converters[1].query.merit_order_position.should == 1000 # 0   => 1000
+        @converters[2].query.merit_order_position.should == 1000 # nil => 1000
+        @converters[3].query.merit_order_position.should == 2
+      end
+    end
+
+    describe "#capacity_factor_for" do
+      before do
+        @graph = Scenario.default.gql.future_graph
+        # The following is a realistic curve taken from etm
+        @ldc_polygon = CurveArea.new([[0.0, 1.0], [1402, 1.0], [7014, 0.5], [12000, 0.1], [14000, 0.0]])
+        @converter = @graph.converters.first.query
+        @converter.merit_order_start = 0.0
+      end
+
+      it "if it covers the whole area" do
+        @converter.merit_order_end = 14000
+        @ldc_polygon.area(0, 14000).should == 7206.8
+
+        {
+          1.0 => 0.5,
+          0.8 => 0.4,
+          0.5 => 0.3,
+          0.1 => 0.1
+        }.each do |availability, capacity_factor|
+          @converter.availability = availability
+          @graph.capacity_factor_for(@converter, @ldc_polygon).round(1).should == capacity_factor
+        end
+      end
+
+      it "if it covers only the first part (full capacity) availability becomes capacity_factor" do
+        @converter.merit_order_end = 1402
+        @ldc_polygon.area(0, @converter.merit_order_end).should == 1402
+
+        { # availability, expected capacity_factor
+          1.0 => 1.0,
+          0.8 => 0.8,
+          0.5 => 0.5,
+          0.1 => 0.1
+        }.each do |availability, capacity_factor|
+          @converter.availability = availability
+          @graph.capacity_factor_for(@converter, @ldc_polygon).round(1).should == capacity_factor
+        end
+      end
+    end
+  end
 
   describe LoadProfileTable do
     describe "#residual_load_profiles" do
       before do
         @tbl = LoadProfileTable.new(nil)
         @tbl.stub!(:graph_peak_demand).and_return(1000.0)
-        dmnds = [100, 200, 300, 400]
-        @tbl.stub!(:merit_order_demands).and_return(dmnds)
+        # Stubbing all this is unsexy, but it gives a good overview how the
+        # calculations fit together.
         @tbl.stub!(:merit_order_table).and_return([
           [0.6, [0.6, 0.00,1.00,3.0]], # 1000*0.6 - (0.6*100 + 0.0 + 1.0*300 + 3.0*400)
           [0.5, [0.7, 0.00,1.00,0.0]], # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
           [0.5, [0.7, 0.00,1.00,0.2]], # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
           [0.5, [0.7, 0.00,1.00,0.1]]  # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
         ])
+        # demands calculated by the graph for a set of converters.
+        dmnds = [100, 200, 300, 400]
+        @tbl.stub!(:merit_order_demands).and_return(dmnds)
       end
 
       specify { LoadProfileTable::PRECISION.should == 10 }
 
       it "#residual_load_profiles" do
-
         @tbl.send(:residual_load_profiles).should == [
           0,
           130.0,
@@ -37,11 +117,16 @@ module Qernel::Plugins::MeritOrder
 
       it "#residual_ldc_coordinates" do
         points = @tbl.residual_ldc_coordinates
-        points.include?([  0.0, 1.0 ]).should be_true
-        points.include?([ 13.0, 0.75]).should be_true
-        points.include?([ 52.0, 0.5 ]).should be_true
-        points.include?([ 91.0, 0.25]).should be_true
-        points.include?([130.0, 0.25]).should be_true
+        [ # expected coordinates
+          [  0.0, 1.0 ],
+          [ 13.0, 0.75],
+          [ 52.0, 0.5 ],
+          [ 91.0, 0.25],
+          [130.0, 0.25]
+        ].each do |coordinate|
+          points.include?(coordinate).should be_true
+        end
+
       end
     end
   end
