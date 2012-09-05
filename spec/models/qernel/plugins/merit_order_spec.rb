376d277daed19549cd801c9194e85b1c7534e5da
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-09-05 11:43:54 +0700

    renaming some vars and add LoadProfileTable.

--- spec/models/qernel/plugins/merit_order_spec.rb
+++ spec/models/qernel/plugins/merit_order_spec.rb
@@ -8,6 +8,44 @@ module Qernel::Plugins::MeritOrder
   #   end
   # end
 
+  describe LoadProfileTable do
+    describe "#residual_load_profiles" do
+      before do
+        @tbl = LoadProfileTable.new(nil)
+        @tbl.stub!(:graph_peak_demand).and_return(1000.0)
+        dmnds = [100, 200, 300, 400]
+        @tbl.stub!(:merit_order_demands).and_return(dmnds)
+        @tbl.stub!(:merit_order_table).and_return([
+          [0.6, [0.6, 0.00,1.00,3.0]], # 1000*0.6 - (0.6*100 + 0.0 + 1.0*300 + 3.0*400)
+          [0.5, [0.7, 0.00,1.00,0.0]], # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
+          [0.5, [0.7, 0.00,1.00,0.2]], # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
+          [0.5, [0.7, 0.00,1.00,0.1]]  # 1000*0.5 - (0.7*100 + 0.0 + 1.0*300 + 0.1*400)
+        ])
+      end
+
+      specify { LoadProfileTable::PRECISION.should == 10 }
+
+      it "#residual_load_profiles" do
+
+        @tbl.send(:residual_load_profiles).should == [
+          0,
+          130.0,
+          50.0,
+          90.0
+        ]
+      end
+
+      it "#residual_ldc_coordinates" do
+        points = @tbl.residual_ldc_coordinates
+        points.include?([  0.0, 1.0 ]).should be_true
+        points.include?([ 13.0, 0.75]).should be_true
+        points.include?([ 52.0, 0.5 ]).should be_true
+        points.include?([ 91.0, 0.25]).should be_true
+        points.include?([130.0, 0.25]).should be_true
+      end
+    end
+  end
+
   describe CurveArea do
 
     # 10 *
@@ -21,7 +59,7 @@ module Qernel::Plugins::MeritOrder
     #       total area: 50
     #
     context "curve with a simple slope" do
-      before { @curve = CurveArea.new([[0.0, 10.0], [10.0, 0.0]]) }
+      before  { @curve = CurveArea.new([[0.0, 10.0], [10.0, 0.0]]) }
 
       specify { @curve.area(0,   10.0).should == 50.0 } # total area
       specify { @curve.area(5.0, 10.0).should == 12.5 } # 5 * 5 / 2
@@ -47,7 +85,7 @@ module Qernel::Plugins::MeritOrder
     #       35   +   10 => 45 total area
     #
     context "curve with a simple slope" do
-      before { @curve = CurveArea.new([[0.0, 10.0], [5.0, 4.0], [10.0, 0.0]]) }
+      before  { @curve = CurveArea.new([[0.0, 10.0], [5.0, 4.0], [10.0, 0.0]]) }
 
       specify { @curve.area(0.0,  5.0).should == 35 }
       specify { @curve.area(5.0, 10.0).should == 10 }
