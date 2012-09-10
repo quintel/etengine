7ea694670bb3c32516981fa3ac3ab57d308a9135
Paolo Zaccagnini <paozac@gmail.com>
2012-09-10 09:57:01 +0200

    TRASH: spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,54 +11,40 @@ if ENV["COVERAGE"]
 end
 
 require 'rubygems'
-require 'spork'
 
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun' # Required for Rcov to run.
 
+require 'webrat'
+require 'authlogic/test_case'
+require 'factory_girl'
 
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun' # Required for Rcov to run.
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  require 'webrat'
-  require 'authlogic/test_case'
-  require 'factory_girl'
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    config.mock_with :rspec
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = true
-
-    config.include(Webrat::Matchers)
-    config.include(Authlogic::TestCase)
-    config.include(EtmAuthHelper)
-    config.include(MechanicalTurkHelper)
-  end
-
-end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  FactoryGirl.reload
+  config.include(Webrat::Matchers)
+  config.include(Authlogic::TestCase)
+  config.include(EtmAuthHelper)
+  config.include(MechanicalTurkHelper)
 end
