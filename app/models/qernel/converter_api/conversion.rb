5d74a43b2e8f53d24de1568345b414be5f47925a
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-09-18 09:37:50 +0200

    Fixed broken tests for cost conversion. Fixes #455.

--- app/models/qernel/converter_api/conversion.rb
+++ app/models/qernel/converter_api/conversion.rb
@@ -13,7 +13,7 @@ class Qernel::ConverterApi
   # :full_load_hour
   #
   # Costs can be calculated in different units...
-  # * plant:           per typical size of a plant. 
+  # * plant:           per typical size of a plant.
   # * mw_input:        Costs per MW capacity of input fuel (MWinput)
   # * mw_electricity:  Costs per MW electrical capacity (MWe)
   #                    Used by costs scatter plot
@@ -43,32 +43,32 @@ class Qernel::ConverterApi
   # @return [Float] the Cost per plant converted to Cost per unit as
   # specified in the parameter
   #
-  
+
   # CONVERTER ATTRIBUTES TO CONVERT
   def initial_investment_per(unit)
     convert_to initial_investment, unit
   end
-  
+
   def ccs_investment_per(unit)
     convert_to ccs_investment, unit
   end
-  
+
   def cost_of_installing_per(unit)
     convert_to cost_of_installing, unit
   end
-  
+
   def fixed_operation_and_maintenance_costs_per(unit)
     convert_to fixed_operation_and_maintenance_costs_per_year, unit
   end
-  
+
   def decommissioning_costs_per(unit)
     convert_to decommissioning_costs, unit
   end
-  
+
   def residual_value_per(unit)
     convert_to residual_value, unit
   end
-  
+
   # CONVERTER METHODS TO CONVERT
   def total_costs_per(unit)
     convert_to total_costs, unit
@@ -144,23 +144,23 @@ class Qernel::ConverterApi
 
     # MW capacity
     when :mw_input
-      cost / effective_input_capacity
+      cost / effective_input_capacity.to_f
     when :mw_electricity
-      cost / electricity_output_capacity
+      cost / electricity_output_capacity.to_f
     when :mw_heat
-      cost / heat_output_capacity
+      cost / heat_output_capacity.to_f
 
     # MWh production
     when :mwh_input
-      cost / typical_fuel_input * SECS_PER_HOUR
+      cost / typical_fuel_input.to_f * SECS_PER_HOUR
     when :mwh_electricity
-      cost / typical_electricity_output * SECS_PER_HOUR
+      cost / typical_electricity_output.to_f * SECS_PER_HOUR
     when :mwh_heat
-      cost / typical_heat_output * SECS_PER_HOUR
+      cost / typical_heat_output.to_f * SECS_PER_HOUR
 
     # full load hours
     when :full_load_hour
-      cost / full_load_hours
+      cost / full_load_hours.to_f
 
     # Some other unit that is unknown
     else
