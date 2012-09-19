0b4a0e1c09b0afbe063070c94bcd3b6afdc8bb05
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-09-19 19:29:51 +0700

    Document Hashpipe

--- app/models/hashpipe.rb
+++ app/models/hashpipe.rb
@@ -1,4 +1,17 @@
-# TODO @hasclass can you add some documentation?
+# Hashpipe creates a 32 bit Fixnum hash of a string. Used for qernel object
+# keys so that we can properly lookup data in the dataset. Using fixnums
+# instead of string keys decreases the total dataset size and unmarshalling
+# time, because our qernel keys are big.
+#
+# It's using the FNV algorithm from the fnv-gem. This could just as well be
+# replaced with the ruby hash method.
+#
+# @example
+#   Hashpipe.hash("some-long-key") => 121234
+#   # Removes whitespace!
+#   Hashpipe.hash("some  -long -   key  ") => 121234
+#
+#
 class Hashpipe
   include Singleton
 
