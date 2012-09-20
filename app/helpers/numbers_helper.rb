14b1c018a40ecc3e1bd377452e4defbc44f134a2
Paolo Zaccagnini <paozac@gmail.com>
2012-09-20 15:44:48 +0200

    data number formatting. See #423

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -5,16 +5,18 @@ module NumbersHelper
     return 'Infinity' if value.to_f.infinite?
 
     abs_value = value.abs
-    if abs_value > 10**8
-      number_with_precision(value.to_f / 10**9, :precision => 2, :delimiter => ",") + " B"
-    elsif abs_value > 10**5
-      number_with_precision(value.to_f / 10**6, :precision => 2, :delimiter => ",") + " M"
-    elsif abs_value > 10**2
-      number_with_precision(value.to_f / 10**3, :precision => 2, :delimiter => ",") + " K"
+    if abs_value > 1_000_000_000
+      number_with_precision(value.to_f / 10**9, :precision => 2, :separator => ',', :delimiter => ".") + " B"
+    elsif abs_value > 10_000_000
+      number_with_precision(value.to_f / 10**6, :precision => 2, :separator => ',', :delimiter => ".") + " M"
+    elsif abs_value > 100_000
+      number_with_precision(value.to_f / 10**3, :precision => 0, :separator => ',', :delimiter => ".") + " K"
+    elsif abs_value > 100
+      number_with_precision(value.to_f, :precision => 0, :separator => ',', :delimiter => ".")
     elsif abs_value >= 1 && value < 100
-      number_with_precision value, :precision => 2
+      number_with_precision value, :precision => 2, :separator => ',', :delimiter => "."
     elsif abs_value > 0 && value < 1
-      number_with_precision value, :precision => 3
+      number_with_precision value, :precision => 3, :separator => ',', :delimiter => "."
     elsif abs_value === 0
       0
     else
@@ -22,7 +24,7 @@ module NumbersHelper
     end
   end
 
-  # Formats number nicely, ignoring the unit
+  # Formats number nicely, ignoring the unit and without transformations
   def nice_number(x)
     return 0 if x === 0
     abs_value = x.abs
@@ -33,6 +35,6 @@ module NumbersHelper
     end
 
     x = number_with_precision x, :precision => precision
-    number_with_delimiter(x, :delimiter => ",", :separator => '.')
+    number_with_delimiter(x, :delimiter => ".", :separator => ',')
   end
 end
