132288853eb773412ce9d9e477d0d619372b966a
wmeyers <wouter.meyers@quintel.com>
2012-09-21 14:49:07 +0200

    uncommented complete old cost calculations methods. These are no longer available

--- app/models/qernel/converter_api/co2.rb
+++ app/models/qernel/converter_api/co2.rb
@@ -12,12 +12,12 @@ class Qernel::ConverterApi
   #
   # DEPRECATED
   #
-  def cost_of_co2_emission_credits
-    function(:cost_of_co2_emission_credits) do
-      cost_of_co2_emission_credits_per_mwh_input * mwh_input
-    end
-  end
-  unit_for_calculation "cost_of_co2_emission_credits", 'euro'
+  #def cost_of_co2_emission_credits
+  #  function(:cost_of_co2_emission_credits) do
+  #    cost_of_co2_emission_credits_per_mwh_input * mwh_input
+  #  end
+  #end
+  #unit_for_calculation "cost_of_co2_emission_credits", 'euro'
 
   # This returns the costs for co2 emission credits per MWh input, as it
   # multiplies the CO2 emitted by the converter by the price of the 
@@ -28,11 +28,11 @@ class Qernel::ConverterApi
   #
   # DEPRECATED
   #
-  def cost_of_co2_emission_credits_per_mwh_input
-    function(:cost_of_co2_emission_credits_per_mwh_input) do
-      (1 - self.area.co2_percentage_free ) * self.area.co2_price * part_ets * ((1 - co2_free) * weighted_carrier_co2_per_mj) * SECS_PER_HOUR
-    end
-  end
-  unit_for_calculation "cost_of_co2_emission_credits_per_mwh_input", 'euro'
+  #def cost_of_co2_emission_credits_per_mwh_input
+  #  function(:cost_of_co2_emission_credits_per_mwh_input) do
+  #    (1 - self.area.co2_percentage_free ) * self.area.co2_price * part_ets * ((1 - co2_free) * weighted_carrier_co2_per_mj) * SECS_PER_HOUR
+  #  end
+  #end
+  #unit_for_calculation "cost_of_co2_emission_credits_per_mwh_input", 'euro'
 
 end
