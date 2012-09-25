c353838e69d8c1235ebd9e433695fe2d8414a6b2
Anthony Williams <hi@antw.me>
2012-09-25 15:01:44 +0100

    Slots may now include optional hash data.
    Such slots may be defined like so:
    
        (carrier)-converter_key: { key: value, key2: value2 }
    
    The data hash is evaluated as Ruby. The colon and hash may be omitted
    for slots which do not have any data to define:
    
        (carrier)-converter_key
    
    Full specs for Etsource::SlotToken have been added.

---
+++ spec/models/etsource/topology/slot_token_spec.rb
@@ -0,0 +1,142 @@
+require 'spec_helper'
+
+describe 'Etsource::SlotToken' do
+  let(:parsed) { Etsource::SlotToken.find(line) }
+  let(:slot)   { parsed.first }
+
+  context 'with "(loss)-fusion"' do
+    let(:line) { '(loss)-fusion' }
+
+    it 'should create one SlotToken' do
+      expect(parsed).to have(1).member
+    end
+
+    it 'should set the carrier to be :loss' do
+      expect(slot.carrier_key).to eql(:loss)
+    end
+
+    it 'should set the key to be "(loss)-fusion"' do
+      expect(slot.key).to eql('(loss)-fusion')
+    end
+
+    it 'should set the converter key to be :fusion' do
+      expect(slot.converter_key).to eql(:fusion)
+    end
+
+    it 'should set the direction to be :output' do
+      expect(slot.direction).to eql(:output)
+    end
+
+    it 'should have no :type data' do
+      expect(slot.data(:type)).to be_nil
+    end
+  end
+
+  context 'with "fusion-(loss)"' do
+    let(:line) { 'fusion-(loss)' }
+
+    it 'should create one SlotToken' do
+      expect(parsed).to have(1).member
+    end
+
+    it 'should set the carrier to be :loss' do
+      expect(slot.carrier_key).to eql(:loss)
+    end
+
+    it 'should set the key to be "(loss)-fusion"' do
+      expect(slot.key).to eql('fusion-(loss)')
+    end
+
+    it 'should set the converter key to be :fusion' do
+      expect(slot.converter_key).to eql(:fusion)
+    end
+
+    it 'should set the direction to be :input' do
+      expect(slot.direction).to eql(:input)
+    end
+
+    it 'should have no :type data' do
+      expect(slot.data(:type)).to be_nil
+    end
+  end
+
+  context 'with "(loss)-fusion: {}"' do
+    let(:line) { '(loss)-fusion: {}' }
+
+    it 'should create one SlotToken' do
+      expect(parsed).to have(1).member
+    end
+
+    it 'should set the carrier to be :loss' do
+      expect(slot.carrier_key).to eql(:loss)
+    end
+
+    it 'should set the key to be "(loss)-fusion"' do
+      expect(slot.key).to eql('(loss)-fusion')
+    end
+
+    it 'should set the converter key to be :fusion' do
+      expect(slot.converter_key).to eql(:fusion)
+    end
+
+    it 'should set the direction to be :output' do
+      expect(slot.direction).to eql(:output)
+    end
+
+    it 'should have no :type data' do
+      expect(slot.data(:type)).to be_nil
+    end
+  end
+
+  context 'with "(loss)-fusion: {type: :elastic}"' do
+    let(:line) { '(loss)-fusion: {type: :elastic}' }
+
+    it 'should create one SlotToken' do
+      expect(parsed).to have(1).member
+    end
+
+    it 'should set the carrier to be :loss' do
+      expect(slot.carrier_key).to eql(:loss)
+    end
+
+    it 'should set the key to be "(loss)-fusion"' do
+      expect(slot.key).to eql('(loss)-fusion')
+    end
+
+    it 'should set the converter key to be :fusion' do
+      expect(slot.converter_key).to eql(:fusion)
+    end
+
+    it 'should set the direction to be :output' do
+      expect(slot.direction).to eql(:output)
+    end
+
+    it 'should set :type data to be :elastic' do
+      expect(slot.data(:type)).to eql(:elastic)
+    end
+  end
+
+  context 'with "(loss)-fusion (electricity)-fusion: {type: :elastic}"' do
+    let(:line) { '(loss)-fusion (electricity)-fusion: {type: :elastic}' }
+
+    it 'should create two SlotTokens' do
+      expect(parsed).to have(2).members
+    end
+
+    it 'should set the carriers to be :loss and :electricity' do
+      expect(parsed[0].carrier_key).to eql(:loss)
+      expect(parsed[1].carrier_key).to eql(:electricity)
+    end
+
+    it 'should set the converter keys to be :fusion' do
+      expect(parsed[0].converter_key).to eql(:fusion)
+      expect(parsed[1].converter_key).to eql(:fusion)
+    end
+
+    it 'should set :type data for the electricity slot' do
+      expect(parsed[0].data(:type)).to be_nil
+      expect(parsed[1].data(:type)).to eql(:elastic)
+    end
+  end
+
+end # Etsource::Topology
