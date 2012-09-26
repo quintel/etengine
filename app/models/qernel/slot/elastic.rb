b0cccd565d8ead48574deef107f97322874c883b
Anthony Williams <hi@antw.me>
2012-09-26 14:03:25 +0100

    Converters may not hav more than one elastic slot.

--- app/models/qernel/slot/elastic.rb
+++ app/models/qernel/slot/elastic.rb
@@ -9,6 +9,20 @@ module Qernel
   #
   class Slot::Elastic < Slot
 
+    # Creates a new Elastic slot.
+    #
+    # @raise [Qernel::Slot::Elastic::TooManyElasticSlots]
+    #   Raised if the converter already has an elastic slot with the same
+    #   direction.
+    #
+    def initialize(*)
+      super
+
+      if siblings.any? { |s| s.kind_of?(Slot::Elastic) }
+        raise TooManyElasticSlots.new(self)
+      end
+    end
+
     # Dynamically calculates +conversion+ so that all of the slots sum to 1.0.
     def conversion
       function(:conversion) do
@@ -17,6 +31,24 @@ module Qernel
         # Don't break the laws of thermodynamics; conversion may not be
         # negative.
         others > 1.0 ? 0.0 : 1.0 - others
+      end
+    end
+
+    # Raised when trying to add a second elastic slot to a converter.
+    class TooManyElasticSlots < RuntimeError
+      def initialize(slot)
+        @slot = slot
+      end
+
+      def message
+        other = @slot.siblings.detect do |sibling|
+          sibling.kind_of?(Slot::Elastic)
+        end
+
+        <<-MESSAGE.squish!
+          Converter #{ @slot.converter.inspect } already has an elastic slot
+          (#{ other.inspect }); you cannot add #{ @slot.inspect }.
+        MESSAGE
       end
     end
 
