63b4d27941a407a43eac75839ec2680861a47cf8
Paolo Zaccagnini <paozac@gmail.com>
2012-10-05 16:57:46 +0200

    API: converter details specs

--- spec/controllers/api/v3/converters_controller_spec.rb
+++ spec/controllers/api/v3/converters_controller_spec.rb
@@ -3,15 +3,6 @@ require 'spec_helper'
 describe Api::V3::ConvertersController do
   before do
     Gql::Gql.any_instance.stub(:prepare).and_return(true)
-    scenario = Scenario.new
-  end
-
-  pending "GET show.json" do
-    it "should return a converter info" do
-      get :show, :id => @converter.key, :format => :json
-      response.should be_success
-      assigns(:converter).should == @converter
-    end
   end
 
   describe "GET topology.json" do
