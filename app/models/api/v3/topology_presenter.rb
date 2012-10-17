b607b28948b0a2477fe4961608f5fe210d8c81fc
Paolo Zaccagnini <paozac@gmail.com>
2012-10-17 16:53:41 +0200

    API: topology action returns converter summary availability

--- app/models/api/v3/topology_presenter.rb
+++ app/models/api/v3/topology_presenter.rb
@@ -25,6 +25,13 @@ module Api
       private
 
       def converters
+        # converter groups that have the converter summary table
+        groups_with_extra_info = [
+          :cost_traditional_heat,
+          :cost_electricity_production,
+          :cost_heat_pumps,
+          :cost_chps
+        ]
         @converters.map do |c|
           # I'd rather use the converter key
           excel_id = c.excel_id.to_i
@@ -37,7 +44,8 @@ module Api
             stroke_color: position.stroke_color(c),
             sector: c.sector_key,
             use: c.use_key,
-            group: c.energy_balance_group
+            group: c.energy_balance_group,
+            summary_available: (c.groups & groups_with_extra_info).any?
           }
         end
       end
