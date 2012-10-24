ec59f93c1f2fa7a7fdd6c1cd9433a225150f614c
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-10-24 14:42:29 +0200

    replace #typical_effective_input_capacity with #effective_input_capacity

--- app/models/qernel/plugins/merit_order/merit_order_breakpoint.rb
+++ app/models/qernel/plugins/merit_order/merit_order_breakpoint.rb
@@ -141,7 +141,8 @@ module Qernel::Plugins
           converter[:full_load_seconds] = converter.merit_order_full_load_hours * 3600
           converter[:capacity_factor]   = converter.merit_order_capacity_factor
 
-          new_demand = converter.full_load_seconds * converter.typical_effective_input_capacity * converter.number_of_units # DEBT: check this better!
+          # DEBT: check this better!
+          new_demand = converter.full_load_seconds * converter.effective_input_capacity * converter.number_of_units
 
           Rails.logger.warn "**** demand #{converter.key}: #{converter.demand} -> #{new_demand}"
           # do not overwrite demand with nil
