34c96aa6fe4f523f750549ef387a3901c616b0c8
Sebi Burkhard <sebi.burkhard@gmail.com>
2012-10-25 14:30:17 +0200

    Fix typo

--- app/models/qernel/converter_api/merit_order.rb
+++ app/models/qernel/converter_api/merit_order.rb
@@ -1,5 +1,3 @@
-# This is what they don't tell you at Software Engineering course.
-#
 class Qernel::ConverterApi
   def merit_order_variable_costs
     fetch_and_rescue(:merit_order_variable_costs) do
@@ -25,13 +23,13 @@ class Qernel::ConverterApi
   unit_for_calculation "merit_order_co2_emissions_costs", 'euro / plant / year'
 
   def merit_order_weighted_carrier_co2_per_mj
-    inputs.map do |slot|
+    converter.inputs.map do |slot|
       slot.conversion * slot.carrier.merit_order_co2_per_mj rescue 0
     end.compact.sum
   end
 
   def merit_order_weighted_carrier_cost_per_mj
-    inputs.map do |slot|
+    converter.inputs.map do |slot|
       slot.conversion * slot.carrier.merit_order_cost_per_mj rescue 0
     end.compact.sum
   end
