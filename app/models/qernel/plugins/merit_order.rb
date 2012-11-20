7ccf761239de0341290f0d8c7eb386a03cd4f696
Paolo Zaccagnini <paozac@gmail.com>
2012-11-20 11:29:58 +0100

    load dispatchable converters from the new etsource file

--- app/models/qernel/plugins/merit_order.rb
+++ app/models/qernel/plugins/merit_order.rb
@@ -6,7 +6,9 @@ module Qernel::Plugins
 
     included do |klass|
       set_callback :calculate, :before, :add_merit_order_breakpoint
-      set_callback :calculate, :after,  :calculate_merit_order_if_no_breakpoint
+      # Disabled by PZ: is there any reason to run the merit order calculation
+      # when MO is off?
+      # set_callback :calculate, :after,  :calculate_merit_order_if_no_breakpoint
     end
 
     def add_merit_order_breakpoint
@@ -23,17 +25,28 @@ module Qernel::Plugins
     end
 
     def use_merit_order_demands?
-      if self[:use_merit_order_demands].nil?
-        false
-      else
-        self[:use_merit_order_demands].to_i == 1
-      end
+      self[:use_merit_order_demands].to_i == 1
     end
 
     # Select dispatchable merit order converters
+    #
     def dispatchable_merit_order_converters
-      group_converters(:merit_order_converters)
+      @dispatchable_converters ||= begin
+        merit_order_data['dispatchable'].keys.map do |k|
+          graph.converter(k.to_sym)
+        end.compact
+      end
     end
+
+    private
+
+    # memoizes the etsource-based merit order hash
+    #
+    def merit_order_data
+      @merit_order_data ||= Etsource::MeritOrder.new.import
+    end
+
+
 
   end # MeritOrder
 end
