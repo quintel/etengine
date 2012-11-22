ef7718cd0995b6fffc6f1ff1dad53162c121235d
Anthony Williams <hi@antw.me>
2012-11-22 20:12:36 +0000

    Fix number_of_units when full_load_seconds is zero.
    When full_load_seconds was zero, we'd get a divide-by-zero error and
    number_of_units returned NaN.
    
    This later caused a failure in the GQL MAX function which caused the
    HTTP request to respond 422.

--- app/models/qernel/converter_api/helper_calculations.rb
+++ app/models/qernel/converter_api/helper_calculations.rb
@@ -11,7 +11,8 @@ class Qernel::ConverterApi
     else
       fetch_and_rescue(:number_of_units) do
         # #to_i also checks if it is nil
-        if effective_input_capacity.nil? || effective_input_capacity.zero?
+        if effective_input_capacity.nil? || effective_input_capacity.zero? ||
+              full_load_seconds.nil? || full_load_seconds.zero?
           0
         else
           (demand || preset_demand) / (effective_input_capacity * full_load_seconds)
