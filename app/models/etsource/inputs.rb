79e23d8d5cd770b401bb546494c4a122125fc144
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-03-18 11:54:51 +0100

    making inputs work through ETSource::Input

--- app/models/etsource/inputs.rb
+++ app/models/etsource/inputs.rb
@@ -1,19 +1,19 @@
 module Etsource
+
   class Inputs
     def initialize(etsource = Etsource::Base.instance)
       @etsource = etsource
     end
 
     def import
-      base_dir = "#{@etsource.export_dir}/inputs"
-
-      Dir.glob("#{base_dir}/**/*.yml").map do |f|
-        attributes = YAML.load_file(f)
-        attributes[:key] = f.split('/').last.split('.').first.strip
-        attributes[:lookup_id] ||= attributes.delete('id')
+      ETSource::Input.all.map do |input|
+        attributes = input.to_hash
+        attributes[:lookup_id] ||= attributes.delete(:id)
+        attributes[:key] = input.key
         Input.new(attributes)
       end
     end
 
-  end
-end
+  end # class inputs
+
+end # module Etsource
