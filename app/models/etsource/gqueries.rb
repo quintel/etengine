ac5b1f770b5fcc7b723e5118a070abdb82a746fc
Chael Kruip <chael.kruip@quintel.com>
2013-05-14 14:54:27 +0200

    ETSource::ActiveDocument#file_path is now #path!

--- app/models/etsource/gqueries.rb
+++ app/models/etsource/gqueries.rb
@@ -10,12 +10,12 @@ module Etsource
     def import
       ETSource::Gquery.all.map do |gquery|
         Gquery.new(
-          :key => gquery.key.to_s,
-          :description => gquery.description,
-          :query => gquery.query,
-          :unit => gquery.unit,
+          :key =>            gquery.key.to_s,
+          :description =>    gquery.description,
+          :query =>          gquery.query,
+          :unit =>           gquery.unit,
           :deprecated_key => gquery.deprecated_key,
-          :file_path => gquery.file_path
+          :file_path =>      gquery.path
         )
       end
     end
