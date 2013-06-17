4153f3cce3731b919020646819f3ffaf32089b99
Frans van Camp <fvancamp@xs4all.nl>
2013-06-17 18:08:58 +0200

    Added has_energy_storage attribute to class Area
    In order to be able to exclude energy storage from sidebar for specific
    markets, a `:has_energy_storage` attribute has been added to the
    `ATTRIBUTES_USED` array of the `class Area` in `module Qernel`.

--- app/models/qernel/area.rb
+++ app/models/qernel/area.rb
@@ -32,6 +32,7 @@ module Qernel
       :has_climate,
       :has_coastline,
       :has_cold_network,
+      :has_energy_storage,
       :has_employment,
       :has_lignite,
       :has_merit_order,
