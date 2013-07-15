9996d8b06ef34088d7468c59a905300855ec1a8f
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-07-15 13:55:08 +0200

    Refactored FACTOR with blocks and added documentation

--- app/models/gql/runtime/functions/update.rb
+++ app/models/gql/runtime/functions/update.rb
@@ -104,15 +104,32 @@ module Gql::Runtime
       #   # => foo gets value 100. but order of inputs can be reversed in another scenario. (pulling input_2 first).
       #
       def UPDATE(*value_terms)
-        update_something_by(:generic_strategy, *value_terms)
+        update_something_by(*value_terms) do |original_value, input_value|
+          case update_strategy
+          when :absolute then input_value
+          when :relative_total
+            original_value + (original_value * input_value)
+          when :relative_per_year
+            original_value * ((1.0 + input_value) ** scope.scenario.years)
+          end.to_f
+        end
       end
 
+      # Same as UPDATE, but now the INPUT_VALUE() is expected to be factor,
+      # that the user supplies.
+      #
+      # @example when foo has a preset_demand of 100.0
+      #     UPDATE_WITH_FACTOR(V(foo), preset_demand, 1.1)
+      #     # => foo gets a demand of 110.0
+      #
       def UPDATE_WITH_FACTOR(*value_terms)
-        update_something_by(:factor, *value_terms)
+        update_something_by(*value_terms) do |original_value, input_value|
+          original_value * input_value
+        end
       end
 
       # @private
-      def update_something_by(type_of_update, *value_terms)
+      def update_something_by(*value_terms)
         input_value_proc = value_terms.pop
         attribute_name   = value_terms.pop
         objects          = value_terms.flatten.compact
@@ -127,13 +144,7 @@ module Gql::Runtime
             input_value    = input_value.first if input_value.is_a?(::Array)
             original_value = big_decimal(object[attribute_name].to_s)
 
-            if type_of_update == :factor
-              value = original_value * input_value
-            elsif type_of_update == :generic_strategy
-              value = update_value_with_strategy(input_value, original_value)
-            else
-              raise("update type: `#{ type }` unknown!")
-            end
+            value = yield original_value, input_value
 
             update_element_with(object, attribute_name, value)
           else
@@ -144,17 +155,6 @@ module Gql::Runtime
       ensure
         scope.update_collection = nil
         scope.update_object = nil
-      end
-
-      # @private
-      def update_value_with_strategy(input_value, original_value)
-        case update_strategy
-        when :absolute then input_value
-        when :relative_total
-          original_value + (original_value * input_value)
-        when :relative_per_year
-          original_value * ((1.0 + input_value) ** scope.scenario.years)
-        end.to_f
       end
 
       # @private
