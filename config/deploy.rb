fea7505c590d43f8836101c20b460e2413a2ceeb
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-08-19 16:23:24 +0200

    Fix #586.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,14 @@ set :stage, :production
 set :server_type, 'production'
 
 set :deploy_to, "/u/apps/etengine"
-set :db_host, "etm.cr6sxqj0itls.eu-west-1.rds.amazonaws.com"
 
 task :production do
   set :domain, "et-engine.com"
   set :branch, fetch(:branch, "production")
-  set :db_pass, "HaLjXwRWmu60DK"
-  set :db_name, application_key
-  set :db_user, application_key
+  set :db_pass, "1TObSSWD54mmRj5fJTj6"
+  set :db_host, "127.0.0.1"
+  set :db_name, "etengine"
+  set :db_user, "root"
   set :airbrake_key, "c7aceee5954aea78f93e7ca4b22439c7"
   server domain, :web, :app, :db, :primary => true
 end
@@ -19,20 +19,10 @@ end
 task :staging do
   set :domain, "beta.et-engine.com"
   set :branch, fetch(:branch, "staging")
-  set :db_pass, "r8ZPP7pQTDTBha"
+  set :db_pass, "Ce9pQnEDjMQZ139z_ldV"
+  set :db_host, "127.0.0.1"
   set :db_name, 'etengine_staging'
-  set :db_user, 'etengine_staging'
-  set :airbrake_key, "e483e275c8425821ec21580e0ffefe9d"
-  server domain, :web, :app, :db, :primary => true
-end
-
-task :edge do
-  # set :domain, "edge.et-engine.com"
-  set :domain, "ec2-176-34-77-157.eu-west-1.compute.amazonaws.com"
-  set :branch, "edge"
-  set :db_pass, "lVtSsSv43KooQE"
-  set :db_name, "etengine_edge"
-  set :db_user, "etengine_edge"
+  set :db_user, "root"
   set :airbrake_key, "e483e275c8425821ec21580e0ffefe9d"
   server domain, :web, :app, :db, :primary => true
 end
