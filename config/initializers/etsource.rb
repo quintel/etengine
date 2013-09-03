1922846cc15e09967ddfaff398cca7de9f0ad817
Anthony Williams <hi@antw.me>
2013-09-03 17:36:00 +0100

    Import time curves via Atlas
    YAML is no longer involved in loading the graph or dataset.

--- config/initializers/etsource.rb
+++ config/initializers/etsource.rb
@@ -7,8 +7,6 @@ if APP_CONFIG[:airbrake_api_key].present? && !APP_CONFIG[:standalone]
   end
 end
 
-# Put ETSOURCE_DIR in here, so that it is accessible when loading
-# with yaml box.
 ETSOURCE_DIR = APP_CONFIG.fetch(:etsource_working_copy, 'etsource')
 ETSOURCE_EXPORT_DIR = ENV['ETSOURCE_DIR'] || APP_CONFIG.fetch(:etsource_export, 'etsource')
 
