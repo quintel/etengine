91c058ac3e7408e8723a56011988adf594442fa2
Anthony Williams <hi@antw.me>
2013-09-11 11:54:15 +0100

    Disable Memcached in development environment
    With memcache, and the recent removal of the "do not cache dataset"
    option, it was necessary to remember to flush memcache each time you
    started the application, since an old dataset could still be in
    Memcache.
    
    Now we use the standard in-memory cache in development (which should be
    fine since you only ever have a single Rails process in dev) which is
    wiped out each time the application shuts down.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,12 @@
-require 'action_dispatch/middleware/session/dalli_store'
-Etm::Application.config.session_store :dalli_store, :key => "_etengine"
+# Cache store may be a symbol, or an array whose first value is a symbol.
+store_name, * = Etm::Application.config.cache_store
+
+if store_name == :dalli_store
+  # Dalli (Memcached) in production.
+  require 'action_dispatch/middleware/session/dalli_store'
+  session_store = :dalli_store
+else
+  session_store = :cookie_store
+end
+
+Etm::Application.config.session_store(session_store, key: '_etengine')
