e1a2a6381a565a5a5791f8090a1d66f9516f5124
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-12-11 10:09:20 +0100

    migrations should not depend on old AR model

--- db/migrate/20131105154914_drop_converter_position_table.rb
+++ db/migrate/20131105154914_drop_converter_position_table.rb
@@ -1,14 +1,5 @@
 class DropConverterPositionTable < ActiveRecord::Migration
   def up
-    hash = ConverterPosition.all.inject({}) do |mem, el|
-      mem[el.converter_key] = { x: el.x, y: el.y }
-      mem
-    end
-
-    File.open('config/converter_positions.yml', 'w') do |f|
-      f.write hash.to_yaml
-    end
-
     drop_table :converter_positions
   end
 
