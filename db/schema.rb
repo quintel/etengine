83f5fe2489213d90c0c097a7fff73a00f95e7fc8
Anthony Williams <hi@antw.io>
2016-08-09 17:46:35 +0100

    Update scenario inputs to reflect industry changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210114327) do
+ActiveRecord::Schema.define(version: 20160809084655) do
 
   create_table "fce_values", force: true do |t|
     t.string   "using_country"
