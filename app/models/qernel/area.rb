378183d53166cda347115157050ce80ef047817d
Peter Lohmann <peter.lohmann@quintel.com>
2017-01-12 13:52:37 +0100

    Qernel::Area also gets additional attributes of a DerivedDataset

--- app/models/qernel/area.rb
+++ app/models/qernel/area.rb
@@ -6,7 +6,10 @@ module Qernel
     # Attributes defined in ETSource should not be defined here, but in the
     # Atlas "Dataset" class.
     ATTRIBUTES_USED =
-      Atlas::Dataset.attribute_set.map(&:name) - [:id, :parent_id]
+      [Atlas::Dataset::FullDataset, Atlas::Dataset::DerivedDataset].
+        map { |klass| klass.attribute_set.map(&:name) }.
+        flatten -
+        [:id, :parent_id]
 
     dataset_accessors ATTRIBUTES_USED
     dataset_accessors :disabled_sectors
