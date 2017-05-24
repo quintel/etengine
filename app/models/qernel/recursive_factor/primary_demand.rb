551026b96bc875945592d6f82a23fc1e67f38430
Anthony Williams <hi@antw.io>
2017-05-24 14:47:58 +0100

    Code-style improvements for recursive factor files
    Cleaned up the code style somewhat and simplified some methods per
    Rubocop recommendations.

--- app/models/qernel/recursive_factor/primary_demand.rb
+++ app/models/qernel/recursive_factor/primary_demand.rb
@@ -1,59 +1,59 @@
 module Qernel::RecursiveFactor::PrimaryDemand
-
-
-  # Calculates the primary energy demand. It recursively iterates through all the child links.
-  #
-  # primary_demand: demand * SUM(links)[ link_share * 1/(1-share_of_loss) * primary_demand_link]
+  # Calculates the primary energy demand. It recursively iterates through all
+  # the child links.
   #
   # It uses primary_energy_demand? to determine if primary or not.
-  #
   def primary_demand
     fetch(:primary_demand) do
-      primary_demand_share = recursive_factor(:primary_demand_factor)
-      (self.demand || 0.0) * (primary_demand_share)
+      (demand || 0.0) * recursive_factor(:primary_demand_factor)
     end
   end
 
   def primary_demand_of(*carriers)
-    carriers.flatten.map do |c|
-      key = c.respond_to?(:key) ? c.key : c
-      primary_demand_of_carrier(key)
+    carriers.flatten.map do |carrier|
+      primary_demand_of_carrier(carrier.try(:key) || carrier)
     end.compact.sum
   end
 
   def primary_demand_of_carrier(carrier_key)
-     if demand && ! demand.zero?
-       demand * recursive_factor(
-         :primary_demand_factor_of_carrier, nil, nil, carrier_key)
-     else
-       0.0
-     end
+    if demand && !demand.zero?
+      demand * recursive_factor(
+        :primary_demand_factor_of_carrier, nil, nil, carrier_key
+      )
+    else
+      0.0
+    end
   end
 
   def primary_demand_with(factor_method, converter_share_method = nil)
-    if converter_share_method
-      w = recursive_factor("#{factor_method}_factor", "#{converter_share_method}_factor")
-    else
-      w = recursive_factor("#{factor_method}_factor")
-    end
-    d = (self.demand || 0.0)
-    w * d
+    factor =
+      if converter_share_method
+        recursive_factor(
+          "#{factor_method}_factor",
+          "#{converter_share_method}_factor"
+        )
+      else
+        recursive_factor("#{factor_method}_factor")
+      end
+
+    (demand || 0.0) * factor
   end
 
+  def primary_demand_factor(link)
+    return nil unless right_dead_end?
 
-  def primary_demand_factor(link,ruby18fix = nil)
-    return nil if !right_dead_end? # or !primary_energy_demand?
-    # We return nil when we want to continue traversing. So typically this is until
-    # we hit a dead end. Alternatively (for final_demand) we could stop when we hit
-    # a converter that is final_demand_group?
+    # We return nil when we want to continue traversing. So typically this is
+    # until we hit a dead end. Alternatively (for final_demand) we could stop
+    # when we hit a converter that is final_demand_group?
     factor_for_primary_demand(link)
   end
 
-  def primary_demand_factor_of_carrier(link, carrier_key, ruby18fix = nil)
-    return nil if !right_dead_end? or !primary_energy_demand?
+  def primary_demand_factor_of_carrier(link, carrier_key)
+    return nil if !right_dead_end? || !primary_energy_demand?
+
     link ||= output_links.first
 
-    if link and link.carrier.key == carrier_key
+    if link && link.carrier.key == carrier_key
       factor_for_primary_demand(link)
     else
       0.0
@@ -61,35 +61,35 @@ module Qernel::RecursiveFactor::PrimaryDemand
   end
 
   def factor_for_primary_demand(link)
-    # Example of a case when a link is not assigned (and therefore needs to be assigned
-    # in order to check if its imported_electricity):
-    # When you get the primary_demand of the group primary_energy_demand, you already
-    # start at the right dead end and don't jump throught links.
+    # Example of a case when a link is not assigned (and therefore needs to be
+    # assigned in order to check if its imported_electricity):
+    #
+    # When you get the primary_demand of the group primary_energy_demand, you
+    # already  start at the right dead end and don't jump throught links.
     link ||= output_links.first
 
     # If a converter has infinite ressources (such as wind, solar/sun), we
     # take the output of energy (1 - losses).
-    if infinite? and primary_energy_demand?
+    if infinite? && primary_energy_demand?
       (1 - loss_output_conversion)
 
     # Special case is imported electricity, if we import, somebody else has
     # to produce that electricity from primary energy. To take that into account
     # we add a higher factor for imported_electricity.
-    elsif primary_energy_demand? and link and link.carrier.key === :imported_electricity
+    elsif primary_energy_demand? &&
+        link && link.carrier.key == :imported_electricity
       # if export should be 1, if its import should be 1.82
-      if demand > 0.0 # if demand greater then 0.0 electricity is imported
+      if demand > 0.0
+        # if demand greater then 0.0 electricity is imported
         graph.area.import_electricity_primary_demand_factor
-      else # energy gets exported.
+      else
+        # energy gets exported.
         graph.area.export_electricity_primary_demand_factor
       end
-
     elsif primary_energy_demand? # Normal case.
       1.0
-    # ignore this converter if it is a dead end but not a primary_energy_demand.
-    # for example some environment converters.
     else
       0.0
     end
   end
-
 end
--- app/models/qernel/recursive_factor/sustainable.rb
+++ app/models/qernel/recursive_factor/sustainable.rb
@@ -1,29 +1,25 @@
-
 module Qernel::RecursiveFactor::Sustainable
-
-
-  # Primary demand of sustainable sources. Uses the Carrier#sustainable attribute
-  #
+  # Primary demand of sustainable sources. Uses the Carrier#sustainable
+  # attribute.
   def primary_demand_of_sustainable
     fetch(:primary_demand_of_sustainable) do
-      (self.demand || 0.0) * (recursive_factor(:sustainable_factor))
+      (demand || 0.0) * recursive_factor(:sustainable_factor)
     end
   end
 
-  # Primary demand of fossil sources. (primary_demand - primary_demand_of_sustainable)
+  # Primary demand of fossil sources.
   #
+  # i.e. (primary_demand - primary_demand_of_sustainable)
   def primary_demand_of_fossil
     fetch(:primary_demand_of_fossil) do
-      self.primary_demand - (recursive_factor(:sustainable_factor)) * (self.demand || 0.0)
+      primary_demand - recursive_factor(:sustainable_factor) * (demand || 0.0)
     end
   end
-
 
   # The share of sustainable energy. It is the (recursive) sum of the
   #  sustainable shares of its parents (nodes to the right).
   #
   # A.sustainability_share == 0.4*0.85 + 0.6 * 1.0
-  #
   def sustainability_share
     fetch(:sustainability_share, false) do
       recursive_factor_without_losses(:sustainability_share_factor)
@@ -31,41 +27,37 @@ module Qernel::RecursiveFactor::Sustainable
   end
 
   def sustainability_share_factor(link)
-    if right_dead_end? and link
-      # If the converter has a sustainability share which has been explicitly
-      # set (through research data or a graph plugin), use that in preference to
-      # the carrier sustainability.
-      custom_share = query.dataset_get(:sustainability_share)
-      custom_share || link.carrier.sustainable
-    end
+    return nil unless right_dead_end? && link
+
+    # If the converter has a sustainability share which has been explicitly
+    # set (through research data or a graph plugin), use that in preference to
+    # the carrier sustainability.
+    custom_share = query.dataset_get(:sustainability_share)
+    custom_share || link.carrier.sustainable
   end
 
+  def sustainable_factor(link)
+    return nil unless right_dead_end?
 
-  def sustainable_factor(link,ruby18fix = nil)
-    return nil if !right_dead_end?
     link ||= output_links.first
 
-    if infinite? and primary_energy_demand?
+    if infinite? && primary_energy_demand?
       (1 - loss_output_conversion)
-    elsif primary_energy_demand? and link and link.carrier.sustainable
+    elsif primary_energy_demand? && link && link.carrier.sustainable
       link.carrier.sustainable
     else
       0.0
     end
   end
 
-
   # Total amount of energy that are losses
   #
   # @return [Float]
-  #
   def total_losses
-    out = self.output(:loss)
-    out and out.external_value
+    output(:loss).try(:external_value)
   end
 
   def infinite?
-    carriers = slots.map {|slot| slot.carrier}.uniq
-    !carriers.empty? and carriers.any?(&:infinite)
+    slots.map(&:carrier).any?(&:infinite)
   end
 end
