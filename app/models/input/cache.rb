2ae143f29b5424344bf59a1571c12410c9486ade
Anthony Williams <hi@antw.io>
2017-06-14 14:59:02 +0100

    Code-style tweaks in Input::Cache
    Updated per RuboCop guidelines. Changed docs to TomDoc.

--- app/models/input/cache.rb
+++ app/models/input/cache.rb
@@ -1,47 +1,42 @@
 class Input
+  # Stores and retrieves input min, max, and start values.
   class Cache
-    # Retrieves the hash containing all of the input attributes.
+    # Public: Retrieves the hash containing all of the input attributes.
     #
     # If no values for the area and year are already cached, the entire input
     # collection values will be calculated and cached.
     #
-    # @param [Scenario] scenario
-    #   A scenario with an area code and end year.
-    # @param [Input] input
-    #   The input whose values are to be retrieved.
+    # scenario - A scenario with an area code and end year.
+    # input    - The input whose values are to be retrieved.
     #
+    # Returns a hash of the input min, max, etc.
     def read(scenario, input)
       cache_key = input_cache_key(scenario, input)
 
       Rails.cache.read(cache_key) ||
-        ( warm_values_for(scenario) && Rails.cache.read(cache_key) )
+        (warm_values_for(scenario) && Rails.cache.read(cache_key))
     end
 
-    #######
     private
-    #######
 
-    # Sets the hash containing all of the input attributes.
+    # Internal: Sets the hash containing all of the input attributes.
     #
-    # @param [Scenario] scenario
-    #   A scenario with an area code and end year.
-    # @param [Input] input
-    #   The input whose values are to be set.
-    # @param [Hash{Symbol=>Numeric}] values
-    #   Values for the input.
+    # scenario - A scenario with an area code and end year.
+    # input    - The input whose values are to be set.
+    # values   - Values for the input in the form
+    #            { min: Numeric, max: Numeric } etc
     #
+    # Returns the values written.
     def set(scenario, input, values)
       Rails.cache.write(input_cache_key(scenario, input), values)
     end
 
-    # Given a scenario, pre-calculates the values for each input using the
-    # scenario area and end year, and stores them in memcache for fast
-    # retrieval later.
+    # Internal: Pre-calculates values for each input.
     #
-    # @param [Scenario] scenario
-    #   A scenario with an area code and end year. All other attributes are
-    #   ignored.
+    # scenario - A scenario with an area code and end year. All other attributes
+    #            are ignored.
     #
+    # Returns nothing.
     def warm_values_for(scenario)
       attributes = scenario.attributes.slice('area_code', 'end_year')
       gql        = Scenario.new(attributes).gql
@@ -51,13 +46,12 @@ class Input
       end
     end
 
-    # Returns the values which should be cached for an input.
+    # Internal: The values which will be cached for an input.
     #
-    # @param [Input] input
-    #   The input whose values are to be cached.
-    # @param [Gql::Gql] gql
-    #   GQL instance for calculating values.
+    # input - The input whose values are to be cached.
+    # gql - GQL instance for calculating values.
     #
+    # Returns a hash of the input values.
     def values_for(input, gql)
       values = {
         min:      input.min_value_for(gql),
@@ -72,26 +66,26 @@ class Input
 
       required_numerics = values.slice(:min, :max, :default).values
 
-      if required_numerics.any? { |value| ! value.kind_of?(Numeric) }
+      if required_numerics.any? { |value| ! value.is_a?(Numeric) }
         { disabled: true, error: 'Non-numeric GQL value' }
       else
         values
       end
     end
 
+    # Internal: The scaler for the given GQL, or nil if the scenario is not
+    # scaled.
     def scaler_for(gql)
       if gql.scenario && Area.derived?(gql.scenario.area_code)
         ScenarioScaling.from_atlas_scaling(gql.scenario.area[:scaling])
       end
     end
 
-    # Given a scenario, returns the key used to store cached minimum, maximum,
-    # and start values.
+    # Internal: Given a scenario, returns the key used to store cached minimum,
+    # maximum, and start values.
     #
-    # @param [Scenario] scenario
-    #   The scenario containing an area code and end year.
-    # @param [Input] input
-    #   The input whose key you want.
+    # scenario - The scenario containing an area code and end year.
+    # input    - The input whose key you want.
     #
     def input_cache_key(scenario, input)
       area = scenario.area_code || :unknown
