f36926a6b0467a31a4b081fe66eeceef168cccb0
Gerard Westerhof <gerard@grdw.nl>
2017-08-09 17:32:25 +0200

    Change color of other
    + Adding a color for bunkers

--- lib/converter_positions.rb
+++ lib/converter_positions.rb
@@ -6,7 +6,7 @@ class ConverterPositions
     :agriculture  => 5300,
     :energy       => 3000,
     :other        => 8300,
-    :environment  => 100,
+    :environment  =>  100,
     :buildings    => 6100,
     :neighbor     =>  100
   }.with_indifferent_access
@@ -17,10 +17,11 @@ class ConverterPositions
     :transport    => '#FFD700',
     :agriculture  => '#B3CF7B',
     :energy       => '#ADD8E6',
-    :other        => '#FF6666',
+    :other        => '#7BABBA',
     :environment  => '#32CD32',
     :buildings    => '#FF6666',
-    :neighbor     => '#87CEEB'
+    :neighbor     => '#87CEEB',
+    :bunkers      => '#5CD0A5'
   }.with_indifferent_access
 
   def initialize(path)
