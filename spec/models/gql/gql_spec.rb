6be7b4ff7eb3124ddaf3a19a919a9e46cc8058b5
Gerard Westerhof <gerard@grdw.nl>
2017-09-13 14:13:56 +0200

    Implement new initializer methods
    As required by changes in Atlas (see Gemfile for the correct commit-sha)

--- spec/models/gql/gql_spec.rb
+++ spec/models/gql/gql_spec.rb
@@ -35,6 +35,36 @@ describe Gql::Gql do
         expect(gql.future.graph.link(:"baz-foo@electricity").share).to eq(0.5)
       end
     end
+
+    # Setting the reserved fraction of baz to 1.0
+    describe "reserved fraction" do
+      it 'updates the present graph with initializer inputs' do
+        expect(gql.present.graph.converter(:baz)
+          .converter_api.reserved_fraction).to eq(1.0)
+      end
+
+      it 'updates the future graph with initializer inputs' do
+        expect(gql.future.graph.converter(:baz)
+          .converter_api.reserved_fraction).to eq(1.0)
+      end
+    end
+
+    # Setting the slot conversion of gas of converter_fixture_for_slots
+    describe "reserved fraction" do
+      it 'updates the present graph with initializer inputs' do
+        slot = gql.present.graph.converter(:converter_fixture_for_slots)
+          .slots.detect{ |slot| slot.carrier.key == :gas }
+
+        expect(slot.conversion).to eq(0.5)
+      end
+
+      it 'updates the future graph with initializer inputs' do
+        slot = gql.future.graph.converter(:converter_fixture_for_slots)
+          .slots.detect{ |slot| slot.carrier.key == :gas }
+
+        expect(slot.conversion).to eq(0.5)
+      end
+    end
   end
 
   describe 'use_network_calculations' do
