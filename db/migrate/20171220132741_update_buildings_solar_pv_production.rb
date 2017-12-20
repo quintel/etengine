c4ce8a85f33bdafaad19f849ede574c79adec006
Anthony Williams <hi@antw.io>
2017-12-20 15:01:50 +0000

    Include analysis years in buildings solar PV migration
    Atlas data is not linked during the deploy until after migrations are
    run.

--- db/migrate/20171220132741_update_buildings_solar_pv_production.rb
+++ db/migrate/20171220132741_update_buildings_solar_pv_production.rb
@@ -6,6 +6,41 @@ class UpdateBuildingsSolarPvProduction < ActiveRecord::Migration
 
   SOLAR_KEY = :buildings_solar_pv_solar_radiation_market_penetration
 
+  # Cannot access area data inside migrations as Atlas/ETSource data hasn't been
+  # linked.
+  ANALYSIS_YEARS = {
+    almere: 2015,
+    baarland: 2015,
+    be: 2013,
+    br: 2013,
+    de: 2012,
+    de_biezen: 2013,
+    drenthe: 2013,
+    dronten: 2015,
+    es: 2012,
+    eu: 2011,
+    example: 2011,
+    flevoland: 2015,
+    fr: 2012,
+    friesland: 2015,
+    gemeente_groningen: 2015,
+    goes: 2015,
+    groningen: 2013,
+    lelystad: 2015,
+    nl: 2015,
+    nl2012: 2012,
+    nl2013: 2013,
+    noorderplantsoen: 2013,
+    noordoostpolder: 2015,
+    paddepoel_noord: 2013,
+    pl: 2012,
+    stedendriehoek: 2015,
+    uk: 2012,
+    urk: 2015,
+    zeeland: 2015,
+    zeewolde: 2015
+  }.freeze
+
   def up
     update_each_scenario do |scenario|
       scenario.user_values[SOLAR_KEY] /= growth_factor(scenario)
@@ -50,9 +85,9 @@ class UpdateBuildingsSolarPvProduction < ActiveRecord::Migration
 
   # See: https://github.com/quintel/etsource/issues/1330#issuecomment-352809844
   def growth_factor(scenario)
-    area = scenario.area
     buildings = scenario.user_values[:number_of_buildings]
 
-    (buildings / 100 + 1) ** (scenario.end_year - area[:analysis_year])
+    (buildings / 100 + 1) **
+      (scenario.end_year - ANALYSIS_YEARS[scenario.area_code.to_sym])
   end
 end
