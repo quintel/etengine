d8a6cbab641eaba9b7461ec81ff99aa2c4c6c22e
Gerard Westerhof <gerard@grdw.nl>
2018-01-04 16:51:33 +0100

    Add migration for CO2 emissions

---
+++ db/migrate/20180104152128_correct_co2_emission_sliders.rb
@@ -0,0 +1,42 @@
+class CorrectCo2EmissionSliders < ActiveRecord::Migration[5.1]
+  KG_PER_MJ_IN_G_PER_KWH = 3600.0
+
+  def up
+    count     = 0
+    gquery    = Gquery.get(:average_co2_emissions_of_produced_electricity)
+    scenarios = Scenario.where(
+      '(protected = ? OR created_at >= ?) AND source != ? AND title != ?',
+      true, 1.month.ago, 'Mechanical Turk', 'test'
+    ).order(:area_code)
+
+    puts "Need to migrate: #{ scenarios.count } scenarios"
+
+    scenarios.find_each do |scenario|
+      next unless scenario.valid?
+
+      if count % 50 == 0
+        puts "#{ Time.now } | #{ count } -> #{ scenario.id }"
+      end
+
+      begin
+        vals = scenario.gql.query(gquery)
+
+        scenario.update(user_values: scenario.user_values.merge(
+          co2_emissions_of_imported_electricity_present: KG_PER_MJ_IN_G_PER_KWH *
+            vals[0][1],
+          co2_emissions_of_imported_electricity_future: KG_PER_MJ_IN_G_PER_KWH *
+            vals[1][1]
+        ))
+
+      rescue StandardError => e
+        puts e
+      end
+
+      count += 1
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
