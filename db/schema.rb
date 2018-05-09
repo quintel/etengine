7bfc35e33b23a60bac21a3472067ce5536c5c84f
Anthony Williams <hi@antw.io>
2018-05-09 13:20:12 +0100

    Revert end years of some production scenarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508162152) do
+ActiveRecord::Schema.define(version: 20180509120035) do
 
   create_table "fce_values", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "using_country"
