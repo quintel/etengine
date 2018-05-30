0edaf22d2394c0da29e071ce78437f2454fc275f
Anthony Williams <hi@antw.io>
2018-05-30 14:33:23 +0100

    Support dynamic profiles for AlwaysOn participants

--- spec/models/qernel/plugins/merit/util_spec.rb
+++ spec/models/qernel/plugins/merit/util_spec.rb
@@ -86,4 +86,66 @@ describe Qernel::Plugins::Merit::Util do
       end
     end # with 26 curves alternating [1, 2, 1, 2] and [1, 2, 3, 4]
   end
+
+  describe '.amplify_curve' do
+    describe 'with a curve of [0.25, 0.25, 0.5, 1.0] (2flh)' do
+      let(:curve) { [0.25, 0.5, 0.75, 1.0].map { |v| v / 3600.0 / 2 } }
+
+      context 'amplifying to 3flh' do
+        let(:result) do
+          Qernel::Plugins::Merit::Util.amplify_curve(curve, 3.0)
+        end
+
+        it 'returns a Merit::Curve' do
+          expect(result).to be_a(Merit::Curve)
+        end
+
+        it 'returns a curve with four elements' do
+          expect(result.length).to eq(4)
+        end
+
+        it 'returns a curve with FLH of 3.0' do
+          max = result.max
+          flh = result.map { |val| val / max }.sum
+
+          expect(flh).to eq(3)
+        end
+      end
+
+      context 'amplifying to 20flh' do
+        let(:result) do
+          Qernel::Plugins::Merit::Util.amplify_curve(curve, 20.0)
+        end
+
+        it 'returns a Merit::Curve' do
+          expect(result).to be_a(Merit::Curve)
+        end
+
+        it 'returns a curve with four elements' do
+          expect(result.length).to eq(4)
+        end
+
+        it 'returns a curve with FLH of 4.0' do
+          max = result.max
+          flh = result.map { |val| val / max }.sum
+
+          expect(flh).to eq(4)
+        end
+
+        it 'maximises the curve' do
+          expect(result.all? { |val| val == result.max }).to be(true)
+        end
+      end
+
+      context 'amplifying to 1flh' do
+        let(:result) do
+          Qernel::Plugins::Merit::Util.amplify_curve(curve, 1.0)
+        end
+
+        it 'raises an error' do
+          expect { result }.to raise_error(/cannot amplify/i)
+        end
+      end
+    end # with a curve of [0.25, 0.25, 0.5, 1.0] (2flh)
+  end
 end
