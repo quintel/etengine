63aa85920dbf1be12fb334248cf4af5f8363b65c
Anthony Williams <hi@antw.io>
2018-06-04 16:25:44 +0100

    Remove clippy from sandbox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,4 @@
 module ApplicationHelper
-  def clippy(text, bgcolor='#FFFFFF')
-    html = <<-EOF
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
-              id="clippy" >
-      <param name="movie" value="/clippy.swf"/>
-      <param name="allowScriptAccess" value="always" />
-      <param name="quality" value="high" />
-      <param name="scale" value="noscale" />
-      <param NAME="FlashVars" value="text=#{h(text)}">
-      <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/clippy.swf"
-             width="110"
-             height="14"
-             name="clippy"
-             quality="high"
-             allowScriptAccess="always"
-             type="application/x-shockwave-flash"
-             pluginspage="http://www.macromedia.com/go/getflashplayer"
-             FlashVars="text=#{h(text)}"
-             bgcolor="#{bgcolor}"
-      />
-      </object>
-    EOF
-    html.html_safe
-  end
-
   def format_result(result, indent = 0)
     lead  = '  ' * indent
 
