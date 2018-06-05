fe5e9a2679f7fb18c0a9a29dc28cfe47fa13d375
Anthony Williams <hi@antw.io>
2018-06-05 16:31:16 +0100

    Configure electricity network through ETSource

--- app/models/etsource/config.rb
+++ app/models/etsource/config.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Etsource
   module Config
     module_function
@@ -8,6 +10,16 @@ module Etsource
     # Returns an array of strings.
     def unscaleable_units
       read(:unscaleable_units)
+    end
+
+    # Public: Defines the layers in the electricity network.
+    #
+    # Returns an array of hashes containing the name of the layer and the means
+    # by which the peak load is determined.
+    def electricity_network
+      read(:electricity_network).map do |layer|
+        { name: layer['name'].to_sym, peak: layer['peak'].classify }
+      end
     end
 
     private_class_method def read(name)
