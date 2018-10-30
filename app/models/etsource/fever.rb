daf08fb506f914b53e9f7ddc37648fc18c66d0df
Anthony Williams <hi@antw.io>
2018-10-30 14:54:23 +0000

    Remove hard-coded Fever group names in Plugin
    Fever groups are ordered according to the ETSource config, rather than
    being hard-coded into the Fever plugin.

--- app/models/etsource/fever.rb
+++ app/models/etsource/fever.rb
@@ -2,24 +2,52 @@ module Etsource
   module Fever
     module_function
 
+    FeverConfig = Struct.new(:name, :keys_by_type) do
+      # Public: Retrieves an array of node keys for the given Fever participant
+      # type.
+      #
+      # For example:
+      #   conf.keys(:producer)
+      #   # => [:node_one, :node_two, ...]
+      #
+      # Returns an array of symbols.
+      def keys(type)
+        keys_by_type[type] || []
+      end
+
+      # Public: Returns if there are any converters belonging to the given type.
+      #
+      # Returns true or false.
+      def any_of_type?(type)
+        keys_by_type[type]&.any?
+      end
+    end
+
     # Public: Reads the Fever information from the Atlas node list.
     #
     # Returns a Hash where each key is the Fever participant "group"
     # ("hot_water",  "space_heating", etc), and each value is a Hash in
     # the form {Node#key => Node#fever}.
-    def data
+    def groups
       Rails.cache.fetch('fever_data') do
-        grouped = Atlas::Node.all.select(&:fever).group_by do |node|
+        grouped_nodes = Atlas::Node.all.select(&:fever).group_by do |node|
           node.fever.group
         end
 
-        grouped.each_with_object({}) do |(group, nodes), data|
-          data[group] =
-            nodes.each_with_object({}) do |node, bt|
-              (bt[node.fever.type] ||= []).push(node.key)
-            end
+        Config.fever.map do |group_name|
+          FeverConfig.new(
+            group_name,
+            grouped_nodes[group_name]
+              .group_by { |node| node.fever.type }
+              .transform_values { |nodes| nodes.map(&:key) }
+          )
         end
       end
+    end
+
+    # Public: Retrieves the configuration for a single named Fever group.
+    def group(name)
+      groups.detect { |group| group.name == name }
     end
   end
 end
