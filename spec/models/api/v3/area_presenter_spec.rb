3b6972b163fc469c0362e2e7b1e4e79bda8d81ee
Anthony Williams <hi@antw.dev>
2019-06-03 14:52:58 +0100

    Add "detailed" param to AreasController#index
    Setting detailed to false allows ETEngine to omit much of the data
    clients don't care about.

---
+++ spec/models/api/v3/area_presenter_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe Api::V3::AreaPresenter do
+  let(:area) do
+    {
+      'area' => 'nl',
+      'enabled' => { 'etmodel' => true, 'etengine' => true },
+      'has_agriculture' => true,
+      'use_network_calculations' => false,
+      'co2_percentage_free' => 0.85
+    }
+  end
+
+  let(:json) { described_class.new(area, detailed: detailed).as_json }
+
+  context 'when detailed=true' do
+    let(:detailed) { true }
+
+    it 'includes the "area" attribute' do
+      expect(json).to include('area' => 'nl')
+    end
+
+    it 'includes the "useable" attribute' do
+      expect(json).to include('useable' => true)
+    end
+
+    it 'does not have the "enabled" attribute' do
+      expect(json).not_to have_key('enabled')
+    end
+
+    it 'has the co2_percentage_free attribute' do
+      expect(json).to include('co2_percentage_free' => 0.85)
+    end
+
+    it 'has the has_agriculture attribute' do
+      expect(json).to include('has_agriculture' => true)
+    end
+
+    it 'has the use_network_calculations attribute' do
+      expect(json).to include('use_network_calculations' => false)
+    end
+  end
+
+  context 'when detailed=true' do
+    let(:detailed) { false }
+
+    it 'includes the "area" attribute' do
+      expect(json).to include('area' => 'nl')
+    end
+
+    it 'includes the "useable" attribute' do
+      expect(json).to include('useable' => true)
+    end
+
+    it 'does not have the "enabled" attribute' do
+      expect(json).not_to have_key('enabled')
+    end
+
+    it 'does not have the co2_percentage_free attribute' do
+      expect(json).not_to include('co2_percentage_free' => 0.85)
+    end
+
+    it 'has the has_agriculture attribute' do
+      expect(json).to include('has_agriculture' => true)
+    end
+
+    it 'has the use_network_calculations attribute' do
+      expect(json).to include('use_network_calculations' => false)
+    end
+  end
+end
