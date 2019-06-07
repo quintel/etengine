5d69c27e13dc086348a04ec23ef9683f87fe16ba
Anthony Williams <hi@antw.dev>
2019-06-07 14:43:11 +0100

    Change scenarios.protected to be boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430162052) do
+ActiveRecord::Schema.define(version: 20190607131242) do
 
   create_table "fce_values", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "using_country"
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 20190430162052) do
     t.integer "preset_scenario_id"
     t.boolean "use_fce"
     t.datetime "present_updated_at"
-    t.integer "protected", limit: 1
+    t.boolean "protected"
     t.string "area_code"
     t.string "source"
     t.text "balanced_values", limit: 16777215
