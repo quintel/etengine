bf0cd62cd791b1b17736b000bb689f92b64f1d18
Anthony Williams <hi@antw.dev>
2019-07-08 15:02:30 +0100

    Set cost of imported electricity after merit order
    Closes quintel/etmodel#3086

--- app/models/qernel/plugins/merit/import_adapter.rb
+++ app/models/qernel/plugins/merit/import_adapter.rb
@@ -13,6 +13,15 @@ module Qernel::Plugins
           elec_link.dataset_set(:value, target_api.demand)
           elec_link.dataset_set(:calculated, true)
         end
+
+        # Recalculate the price per MJ.
+        total_price =
+          carrier.cost_curve.to_enum.with_index.sum do |price, index|
+            price * participant.load_at(index)
+          end
+
+        # Divide by production which is in MJ to set the cost per MJ.
+        carrier.dataset_set(:cost_per_mj, total_price / participant.production)
       end
 
       private
@@ -25,10 +34,7 @@ module Qernel::Plugins
         attrs = super
 
         attrs.delete(:marginal_costs)
-
-        attrs[:cost_curve] = ::Merit::Curve.new(
-          @graph.carrier(:imported_electricity).cost_curve
-        )
+        attrs[:cost_curve] = ::Merit::Curve.new(carrier.cost_curve)
 
         attrs
       end
@@ -43,6 +49,10 @@ module Qernel::Plugins
 
       def flh_capacity
         source_api.electricity_output_capacity
+      end
+
+      def carrier
+        @graph.carrier(:imported_electricity)
       end
     end # ImportAdapter
   end # Merit
