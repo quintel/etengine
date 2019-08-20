f9c18b5a1711657e30fa129151a9eb8fed3704aa
Anthony Williams <hi@antw.dev>
2019-08-20 13:26:04 +0100

    Prevent P2H electricity being used twice in MO
    P2H being classed as having electricity input meant that the heat added
    to P2H was being demanded as electricity in the next frame. This meant
    that P2H was using twice as much electricity as it should:
    
      * as excess electricity in frame 0
      * as mandatory electricity demand in frame 1
      * etc...
    
    This fixes P2H increasing blackout hours, when in fact this metric is
    expected to remain constant or fall when P2H is installed.
    
    Ref a296564

--- app/models/qernel/plugins/fever/storage_adapter.rb
+++ app/models/qernel/plugins/fever/storage_adapter.rb
@@ -27,6 +27,13 @@ module Qernel::Plugins
         end
       end
 
+      def input?(*)
+        # Storage adapters are a "dump" in which excess electricity is converted
+        # to heat. Their heat demand is _not_ to be accounted for in Merit
+        # otherwise their electricity consumption will be included twice.
+        false
+      end
+
       private
 
       def reserve
