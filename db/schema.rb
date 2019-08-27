351c5608c3bbaedd131dfceb5c4ce017dfb182e3
Michiel den Haan <michiel.denhaan@quintel.com>
2019-08-27 15:48:56 +0200

    added migration for biomass cost sliders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_165633) do
+ActiveRecord::Schema.define(version: 2019_08_05_103237) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
