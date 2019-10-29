dd89ecc1d09ea966ffe950e40cc230bd58c5244c
Anthony Williams <hi@antw.dev>
2019-10-29 16:44:11 +0000

    Rename power_to_power in FlexibilityOrder rows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_131249) do
+ActiveRecord::Schema.define(version: 2019_10_29_155720) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
