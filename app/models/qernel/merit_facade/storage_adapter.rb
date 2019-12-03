03c7b22f8df1c5c2d3ad50d28df85ae964d344f5
Anthony Williams <hi@antw.dev>
2019-12-03 14:33:36 +0000

    Dump unused stored energy in StorageAdapter
    When a merit flexibility option stored energy which is not used in the
    merit order calculation, the graph wrongly assumes that that energy
    should still flow to the next node.
    
    Now if the node has a share and an inversed_flexible links, it will
    dump the unused energy through the inversed flexible.

--- app/models/qernel/merit_facade/storage_adapter.rb
+++ app/models/qernel/merit_facade/storage_adapter.rb
@@ -10,6 +10,34 @@ module Qernel
         super && storage_volume_per_unit.positive?
       end
 
+      def inject!
+        super
+
+        # If the output slot has two links, one a share link and one inversed
+        # flexible, assume that unused energy is dumped through the flexible.
+        # Adjust the share edge so that only energy actually emitted by the
+        # storage flows.
+        output_slot = @converter.converter.output(@context.carrier)
+
+        return if !output_slot || output_slot.links.length != 2
+
+        share_link = output_slot.links.detect(&:share?)
+        if_link = output_slot.links.detect(&:inversed_flexible?)
+
+        return unless share_link && if_link
+
+        total = target_api.demand * output_slot.conversion / 3600
+
+        new_share =
+          if total.zero?
+            0.0
+          else
+            @participant.load_curve.sum { |v| v.positive? ? v : 0.0 } / total
+          end
+
+        share_link.dataset_set(:share, new_share)
+      end
+
       private
 
       def producer_attributes
