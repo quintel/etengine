593a08e6554bcfa4091bd89c896f341e0c710bab
Anthony Williams <hi@antw.dev>
2020-02-14 16:08:16 +0000

    Avoid loss of precision in curves with small values
    Adjusts CurveBehavior so that those curves containing a large number of
    small values are now rounded to four decimal places, instead of
    truncated to the nearest integer.
    
    This prevents charts from rendering "blocky" lines in regions where
    there are few residences, or in a chart where the values are all small.
    
    Rounding of small-value curves is done by multiplying by 1000,
    truncating, then dividing. This - surprisingly - is faster than using
    Float#round:
    
        Calculating -------------------------------------
                    truncate     2.758k (± 2.4%) i/s - 13.988k in 5.075602s
                       round   995.318  (± 2.4%) i/s -  4.998k in 5.024488s
        mult + truncate + div    1.192k (± 4.3%) i/s -  6.050k in 5.084865s
    
    Closes #1071

--- app/models/gquery/curve_behavior.rb
+++ app/models/gquery/curve_behavior.rb
@@ -3,6 +3,11 @@ class Gquery
   # the future, and truncate each value in the curve to reduce the amount of
   # data sent to the client.
   module CurveBehavior
+    # If a curve has `NO_TRUNCATE_THRESHOLD` values between 0 and 1 on the first
+    # day of each month, the values in the curve will not be truncated to
+    # integers.
+    NO_TRUNCATE_THRESHOLD = 12
+
     module_function
 
     def period_supported?(period)
@@ -10,11 +15,42 @@ class Gquery
     end
 
     def process_result(result)
-      result.map(&:truncate)
+      if truncate?(result, 300)
+        result.map(&:truncate)
+      else
+        # Ruby 2.6: mult + truncate + to_f + div is faster than Float#round(4).
+        result.map { |val| (val * 1000).truncate.to_f / 1000 }
+      end
     end
 
     def fallback_value
       []
+    end
+
+    # Internal: Determines if the values in a curve should be truncated to whole
+    # integers.
+    #
+    # Scan the first day of every second month (trusting that these days are
+    # representative of the rest of the month). If the total number of values
+    # greater than 0 -- but less than 300 - exceeds a threshold, the curve
+    # should not be truncated.
+    def truncate?(result, upper_value)
+      count = 0
+      per_window = result.length / 6
+      per_day = result.length / 365
+
+      return false if per_window.zero? || per_day < 1
+
+      6.times do |month|
+        start = month * per_window
+
+        result[start..(start + per_day)].each do |val|
+          val = val.abs
+          count += 1 if val.positive? && val.abs < upper_value
+        end
+      end
+
+      count < NO_TRUNCATE_THRESHOLD
     end
   end
 end
