7badcc682a965a6f42cd85bb3d9c90d08c23288b
Anthony Williams <hi@antw.dev>
2020-02-20 13:41:20 +0100

    MeritFacade: Prevent double-setting demand
    Prevent a "follower" adapter from setting the demand of a converter when
    another adapter has already done so.
    
    For example, a CHP which participates in the electricity and heat
    network merit orders should have the demand set by the electricity
    component and not by the heat component. This is because the heat
    component lags behind the electric by an hour, and therefore would
    install a slightly incorrect demand and FLH.
    
    Closes quintel/etmodel#3227

--- app/models/qernel/merit_facade/producer_adapter.rb
+++ app/models/qernel/merit_facade/producer_adapter.rb
@@ -19,24 +19,7 @@ module Qernel
       end
 
       def inject!
-        full_load_hours = full_load_hours_from_participant
-
-        full_load_seconds =
-          if !full_load_hours || full_load_hours.nan?
-            full_load_hours = 0.0
-          else
-            full_load_hours * 3600
-          end
-
-        target_api[:full_load_hours]   = full_load_hours
-        target_api[:full_load_seconds] = full_load_seconds
-        target_api[:number_of_units]   = number_of_units_from_participant
-
-        target_api.demand =
-          full_load_seconds *
-          flh_capacity *
-          target_api.number_of_units
-
+        install_demand!
         inject_curve!(:output) { @participant.load_curve }
       end
 
@@ -76,6 +59,34 @@ module Qernel
 
       def number_of_units_from_participant
         participant.number_of_units
+      end
+
+      def install_demand!
+        # Do not set demand or FLH when based on a "self" curve as these will
+        # be set by the other Causality component adapter. i.e. when a CHP
+        # participates as an electricity and heat producer, and the heat part is
+        # determined by the electricity load (self: electricity_output_curve),
+        # then the demand and FLH are set by the electricity adapter, and should
+        # be left alone by the heat adapter.
+        return if @config.group.to_s.start_with?('self:')
+
+        full_load_hours = full_load_hours_from_participant
+
+        full_load_seconds =
+          if !full_load_hours || full_load_hours.nan?
+            full_load_hours = 0.0
+          else
+            full_load_hours * 3600
+          end
+
+        target_api[:full_load_hours]   = full_load_hours
+        target_api[:full_load_seconds] = full_load_seconds
+        target_api[:number_of_units]   = number_of_units_from_participant
+
+        target_api.demand =
+          full_load_seconds *
+          flh_capacity *
+          target_api.number_of_units
       end
     end
   end
