aafde42ae8869c40b199f01db9cef681f516902e
Anthony Williams <hi@antw.dev>
2020-02-20 12:07:57 +0100

    Fix input of carriers for HHPAdapter
    Fixes the HHPAdapter so that the input carrier conversions are set in
    the graph consistent with the values calculated by Fever. This means
    that the shares of the `based_on` and `balanced_with` shares are set as
    before, but the share of the secondary participant carrier (typically
    network gas or hydrogen) is adjusted to reflect their output efficiency.
    
    The output conversion of the converter is no longer changed, as these
    changes would have knock-on effects on the flow of the primary carriers
    (e.g. electricity and ambient heat), resulting in the electricity curve
    and `input_of_electricity` no longer agreeing.
    
    Closes quintel/etmodel#3223

--- app/models/qernel/fever_facade/hhp_adapter.rb
+++ app/models/qernel/fever_facade/hhp_adapter.rb
@@ -3,34 +3,59 @@
 module Qernel
   module FeverFacade
     # An adapter which sets up a hybrid heat-pump to participate in Fever.
+    #
+    # Hybrid heat-pumps comprise two separate components: a primary and
+    # secondary component. The primary is a VariableEfficiencyProducer which
+    # balances two input carriers based on the efficiency of the producer. For
+    # example, a node with:
+    #
+    #   - fever.efficiency_based_on = electricity
+    #   - fever.efficiency_balanced_with = ambient_heat
+    #
+    # Configures the primary component to use electricity and ambient heat. With
+    # a fixed coefficient-of-performance of 3, the producer will use two units
+    # of ambient heat for every one unit of electricity, outputting three units
+    # of heat.
+    #
+    # The third (unnamed) input carrier to the node is used as the input carrier
+    # for the secondary component.
+    #
+    # The secondary component may also have an output efficiency defined the
+    # normal way in the node document:
+    #
+    #   - output.heat.hydrogen = 1.06
+    #
+    # For every one unit of hydrogen input to the component, 1.06 units of heat
+    # will be output. For this component there is no "balanced_with" carrier:
+    # the extra energy simply appears on the node after the calculation.
+    #
+    # Since the efficiencies and shares of carriers may change by the Fever
+    # calculation, it is not possible to set an output conversion on the node
+    # other than 1.0. If half of the input energy for the node were to come from
+    # the primary component, and half from the secondary, setting an output
+    # conversion of 1.03 would result in inaccurate input of the primary
+    # component carriers. To that end, the converter will be hard-coded to an
+    # output conversion of 1.0, and the input shares will be adjusted to ensure
+    # the flow of energ into the node is correct.
     class HHPAdapter < ProducerAdapter
+      MJ_IN_MWH = 3600
+
       def inject!
         if @number_of_units.zero? ||
             participant.producer.output_curve.all?(&:zero?)
           return
         end
 
-        orig_sec_share = secondary_share
-
-        set_output_conversions!
-
         # Sets the load-adjusted efficiency of the primary component carriers.
-        primary_adapter.inject!
+        # primary_adapter.inject!
+
+        @converter.converter.output(:useable_heat)[:conversion] = 1.0
 
         # Set the demands and other attributes based on the whole producer and
         # not the individual parts.
         super
 
-        # Set the conversion of the secondary component carrier.
-        sec_demand   = secondary_component.output_curve.sum
-        total_demand = primary_component.output_curve.sum + sec_demand
-        sec_share    = sec_demand / total_demand
-
-        @converter.converter.input(secondary_carrier)[:conversion] = sec_share
-
-        # The electric adapter has already set the electricity and ambient heat
-        # conversions, but failed to account for the new secondary share.
-        balance_primary_efficiency!(orig_sec_share - sec_share)
+        set_input_conversions!
       end
 
       def producer
@@ -72,7 +97,8 @@ module Qernel
               )
             else
               total_value { @config.capacity[secondary_carrier] }
-            end
+            end,
+            input_efficiency: output_efficiency_of_carrier(secondary_carrier)
           )
       end
 
@@ -111,39 +137,63 @@ module Qernel
         @secondary_carrier = carriers.first
       end
 
-      # Internal: Re-balances the efficiency of the (typically) electricity and
-      # ambient heat inputs to account for how much energy they actually
-      # provided relative to the gas component.
-      def balance_primary_efficiency!(secondary_delta)
-        slots = [
-          primary_adapter.based_on_slot,
-          primary_adapter.balanced_with_slot
-        ]
+      # Internal: Sets the input conversions after the calculation to match the
+      # use of energy by the Fever components.
+      def set_input_conversions!
+        return if @converter.demand.zero?
 
-        total_conv = slots.sum(&:conversion)
+        primary_demand = primary_component.input_curve.sum
+        based_on_input = source_energy_of_component(primary_component)
+        balanced_with_input = primary_demand - based_on_input
 
-        slots.each do |slot|
-          slot[:conversion] += secondary_delta * (slot.conversion / total_conv)
-        end
+        sec_eff = output_efficiency_of_carrier(secondary_carrier)
+        secondary_input = secondary_component.input_curve.sum / sec_eff
+
+        @converter.converter.input(:electricity)[:conversion] =
+          (based_on_input * MJ_IN_MWH) / @converter.demand
+
+        @converter.converter.input(:ambient_heat)[:conversion] =
+          (balanced_with_input * MJ_IN_MWH) / @converter.demand
+
+        @converter.converter.input(secondary_carrier)[:conversion] =
+          (secondary_input * MJ_IN_MWH) / @converter.demand
       end
 
-      # Internal: If the HHP has a variable output conversion (depends on the
-      # share of the inputs), adjust the conversion for what will be the new
-      # input shares.
-      def set_output_conversions!
+      # Internal: Calculates the total amount of "source" input to the given
+      # component.
+      def source_energy_of_component(component)
+        energy = 0.0
+
+        component.input_curve.length.times do |frame|
+          energy += component.source_at(frame)
+        end
+
+        energy
+      end
+
+      # Internal: Returns the output efficiency defined in the Node document, if
+      # present, otherwise returns 1.0.
+      #
+      # Some nodes will define different output efficiencies depending on which
+      # carrier is used to create the heat output:
+      #
+      #   - output.heat.electricity = 1.1
+      #   - output.heat.hydrogen = 1.06
+      #
+      # Given the carrier name, the appropriate efficiency is returned:
+      #
+      #   output_efficiency_of_carrier(:electricity) # => 1.1
+      #   output_efficiency_of_carrier(:hydrogen)    # => 1.06
+      #   output_efficiency_of_carrier(:network_gas) # => 1.0
+      #
+      # Returns a numeric.
+      def output_efficiency_of_carrier(carrier)
         output = Atlas::Node.find(@converter.key).output[:useable_heat]
+        output.is_a?(Hash) ? output[carrier] : 1.0
+      end
 
-        return unless output.is_a?(Hash)
-
-        total_demand = participant.producer.output_curve.sum
-
-        new_conversion =
-          output.sum do |(carrier, share)|
-            producer = producer_for_carrier(carrier.to_sym)
-            producer ? (producer.output_curve.sum / total_demand) * share : 0.0
-          end
-
-        @converter.converter.output(:useable_heat)[:conversion] = new_conversion
+      def output_efficiency
+        1.0
       end
     end
   end
