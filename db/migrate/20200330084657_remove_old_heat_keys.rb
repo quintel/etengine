92b0b2a74e00226f018fbd76506fff3911760244
noracato <ncschinkel@gmail.com>
2020-03-30 12:49:40 +0200

    Use ScenarioMigration module in old heat keys migration

--- db/migrate/20200330084657_remove_old_heat_keys.rb
+++ db/migrate/20200330084657_remove_old_heat_keys.rb
@@ -1,4 +1,7 @@
+require 'etengine/scenario_migration'
+
 class RemoveOldHeatKeys < ActiveRecord::Migration[5.2]
+  include ETEngine::ScenarioMigration
 
   OLD_HEAT_KEYS = %w[
     buildings_chp_engine_biogas_share
@@ -36,45 +39,10 @@ class RemoveOldHeatKeys < ActiveRecord::Migration[5.2]
 
 
   def up
-    update_scenarios do |scenario|
+    migrate_scenarios(since: Date.new(2020, 2, 1)) do |scenario|
       OLD_HEAT_KEYS.each do |key|
         scenario.user_values.delete(key)
       end
     end
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-
-  def update_scenarios
-    total = scenarios.count
-    changed = 0
-
-    say "Checking and migrating #{total} scenarios"
-
-    scenarios.find_each.with_index do |scenario, index|
-      if Atlas::Dataset.exists?(scenario.area_code)
-        yield(scenario)
-
-        if scenario.changed?
-          scenario.save(validate: false, touch: false)
-          changed += 1
-        end
-      end
-
-      if index.positive? && ((index + 1) % 1000).zero?
-        say "#{index + 1}/#{total} (#{changed} migrated)"
-      end
-    end
-
-    say "#{total}/#{total} (#{changed} migrated)"
-  end
-
-
-  # All protected scenarios, and any unprotected scenarios since March
-  # will be updated.
-  def scenarios
-    Scenario.migratable_since(Date.new(2020, 3, 1))
   end
 end
