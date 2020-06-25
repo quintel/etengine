77a7856cf3cacca14015bfa2d476db26e0a607b9
Anthony Williams <hi@antw.dev>
2020-06-25 10:49:11 +0100

    Make NodeApi files into separate modules

--- app/models/qernel/node_api/employment.rb
+++ app/models/qernel/node_api/employment.rb
@@ -1,15 +1,27 @@
-class Qernel::NodeApi
+# frozen_string_literal: true
 
-  dataset_accessors :hours_prep_nl,
-                    :hours_prep_abroad,
-                    :hours_prod_nl,
-                    :hours_prod_abroad,
-                    :hours_place_nl,
-                    :hours_place_abroad,
-                    :hours_maint_nl,
-                    :hours_maint_abroad,
-                    :hours_remov_nl,
-                    :hours_remov_abroad
+module Qernel
+  class NodeApi
+    # Attributes relating to employment.
+    module Employment
+      extend ActiveSupport::Concern
 
+      ATTRIBUTES = %i[
+        hours_prep_nl
+        hours_prep_abroad
+        hours_prod_nl
+        hours_prod_abroad
+        hours_place_nl
+        hours_place_abroad
+        hours_maint_nl
+        hours_maint_abroad
+        hours_remov_nl
+        hours_remov_abroad
+      ].freeze
 
+      included do
+        dataset_accessors(*ATTRIBUTES)
+      end
+    end
+  end
 end
--- app/models/qernel/node_api/recursive_methods.rb
+++ app/models/qernel/node_api/recursive_methods.rb
@@ -1,40 +1,35 @@
-class Qernel::NodeApi
+# frozen_string_literal: true
 
-  def max_demand_recursive
-    node.max_demand_recursive!
+module Qernel
+  class NodeApi
+    # Aliases of RecursiveFactor methods.
+    module RecursiveMethods
+      include CalculationUnits
+
+      def max_demand_recursive
+        node.max_demand_recursive!
+      end
+      unit_for_calculation 'max_demand_recursive', 'MJ'
+
+      delegate :primary_demand_of_fossil, to: :node
+      unit_for_calculation 'primary_demand_of_fossil', 'MJ'
+
+      delegate :primary_demand_of_sustainable, to: :node
+      unit_for_calculation 'primary_demand_of_sustainable', 'MJ'
+
+      delegate :sustainability_share, to: :node
+
+      # Inverse of sustainability_share https://github.com/dennisschoenmakers/etengine/issues/272
+      def non_renewable_share
+        1.0 - (sustainability_share || 0.0)
+      end
+      unit_for_calculation 'non_renewable_share', 'factor'
+
+      delegate :weighted_carrier_cost_per_mj, to: :node
+      unit_for_calculation 'weighted_carrier_cost_per_mj', 'euro'
+
+      delegate :weighted_carrier_co2_per_mj, to: :node
+      unit_for_calculation 'weighted_carrier_co2_per_mj', 'kg'
+    end
   end
-  unit_for_calculation "max_demand_recursive", 'MJ'
-
-  def primary_demand_of_fossil
-    node.primary_demand_of_fossil
-  end
-  unit_for_calculation "primary_demand_of_fossil", 'MJ'
-
-  def primary_demand_of_sustainable
-    node.primary_demand_of_sustainable
-  end
-  unit_for_calculation "primary_demand_of_sustainable", 'MJ'
-
-  def sustainability_share
-    node.sustainability_share
-  end
-  unit_for_calculation "primary_demand_of_sustainable", 'MJ'
-
-  # Inverse of sustainability_share
-  # https://github.com/dennisschoenmakers/etengine/issues/272
-  def non_renewable_share
-    1.0 - (sustainability_share || 0.0)
-  end
-  unit_for_calculation "non_renewable_share", 'factor'
-
-  def weighted_carrier_cost_per_mj
-    node.weighted_carrier_cost_per_mj
-  end
-  unit_for_calculation "weighted_carrier_cost_per_mj", 'euro'
-
-  def weighted_carrier_co2_per_mj
-    node.weighted_carrier_co2_per_mj
-  end
-  unit_for_calculation "weighted_carrier_co2_per_mj", 'kg'
-
 end
