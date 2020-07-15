fac0a085740fed81923371e18b4fa9169e348a59
Anthony Williams <hi@antw.dev>
2020-07-15 14:41:18 +0100

    Simplify QueryInterface::Lookup with GraphHelper

--- app/models/gql/query_interface/lookup.rb
+++ app/models/gql/query_interface/lookup.rb
@@ -1,121 +1,96 @@
-module Gql::QueryInterface::Lookup
+# frozen_string_literal: true
 
-  def update_object
-    @update_object
-  end
+module Gql
+  class QueryInterface
+    # Provides helpers for looking up values from the scenario and graph.
+    module Lookup
+      def update_object
+        @update_object
+      end
 
-  def update_object=(object)
-    @update_object = object
-  end
+      def update_object=(object)
+        @update_object = object
+      end
 
-  def update_collection
-    @update_collection
-  end
+      def update_collection
+        @update_collection
+      end
 
-  def update_collection=(col)
-    @update_collection = col
-  end
+      def update_collection=(col)
+        @update_collection = col
+      end
 
-  def big_decimal(n)
-    BigDecimal(n, exception: false) || n.to_f
-  end
+      def big_decimal(n)
+        BigDecimal(n, exception: false) || n.to_f
+      end
 
-  def scenario
-    @gql.scenario
-  end
+      def scenario
+        @gql.scenario
+      end
 
-  def present?
-    graph.present?
-  end
+      def present?
+        graph.present?
+      end
 
-  # @param [String] Graph API Method.
-  # @return [Float]
-  #
-  def graph_query(key)
-    key.nil? ? graph : graph.query(key)
-  end
+      # @param [String] Graph API Method.
+      # @return [Float]
+      #
+      def graph_query(key)
+        key.nil? ? graph : graph.query(key)
+      end
 
-  # @param [String] Area attribute. E.g. areable_land
-  # @return [Float]
-  #
-  def area(key)
-    graph.area.send(key)
-  end
+      # @param [String] Area attribute. E.g. areable_land
+      # @return [Float]
+      #
+      def area(key)
+        graph.area.send(key)
+      end
 
-  def all_nodes
-    graph.nodes
-  end
+      def all_nodes
+        graph.nodes
+      end
 
-  # @param [String] Carrier key
-  # @return [Carrier]
-  #
-  def carriers(keys)
-    if keys.is_a?(Array)
-      flatten_compact keys.map{|key| graph.carrier(key.to_sym) }
-    else
-      flatten_compact graph.carrier(keys.to_sym)
+      # @param [String] Carrier key
+      # @return [Carrier]
+      #
+      def carriers(keys)
+        GraphHelper.carriers(graph, keys)
+      end
+
+      # @param [String,Array] Use keys
+      # @return [Node]
+      #
+      def use_nodes(keys)
+        GraphHelper.use_nodes(graph, keys)
+      end
+
+      # @param [String,Array] Sector keys
+      # @return [Node]
+      #
+      def sector_nodes(keys)
+        GraphHelper.sector_nodes(graph, keys)
+      end
+
+      # @param [String,Array] Group keys
+      # @return [Node]
+      #
+      def group_nodes(keys)
+        GraphHelper.group_nodes(graph, keys)
+      end
+
+      # @param [String] Node keys
+      # @return [Node]
+      #
+      def nodes(keys)
+        GraphHelper.nodes(graph, keys)
+      end
+
+      # @param [String,Array] Group keys
+      # @return [Array<Edge>]
+      #
+      def group_edges(keys)
+        GraphHelper.group_edges(graph, keys)
+      end
     end
-  end
-
-  # @param [String,Array] Use keys
-  # @return [Node]
-  #
-  def use_nodes(keys)
-    if keys.is_a?(Array)
-      flatten_compact keys.flatten.map{|key| graph.use_nodes(key) }
-    else
-      flatten_compact graph.use_nodes(key)
-    end
-  end
-
-  # @param [String,Array] Sector keys
-  # @return [Node]
-  #
-  def sector_nodes(keys)
-    if keys.is_a?(Array)
-      flatten_compact keys.flatten.map{|key| graph.sector_nodes(key) }
-    else
-      flatten_compact graph.sector_nodes(keys)
-    end
-  end
-
-  # @param [String,Array] Group keys
-  # @return [Node]
-  #
-  def group_nodes(keys)
-    if keys.is_a?(Array)
-      flatten_compact keys.flatten.map{|key| graph.group_nodes(key) }
-    else
-      flatten_compact graph.group_nodes(keys)
-    end
-  end
-
-  # @param [String] Node keys
-  # @return [Node]
-  #
-  def nodes(keys)
-    if keys.is_a?(Array)
-      flatten_compact keys.flatten.map{|key| graph.node(key) }
-    else
-      flatten_compact [graph.node(keys)]
-    end
-  end
-
-  # @param [String,Array] Group keys
-  # @return [Array<Edge>]
-  #
-  def group_edges(keys)
-    # Array(keys).map(&graph.method(:group_edges))
-    if keys.is_a?(Array)
-      flatten_compact(keys.flatten.map{|key| graph.group_edges(key) })
-    else
-      flatten_compact graph.group_edge(keys)
-    end
-  end
-
-  def flatten_compact(val)
-    val.flatten!
-    val.compact!
-    val
   end
 end
