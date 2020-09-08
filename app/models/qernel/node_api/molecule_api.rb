da198f384a4e2b27b6bb39f7b614f9764fb1fb38
Anthony Williams <hi@antw.dev>
2020-09-08 15:25:03 +0100

    Allow molecule nodes to calculate number_of_units
    Number of units may be calculated in one of two ways:
    
    * With demand and input capacity
    * With demand, output of loss, and output capacity

--- app/models/qernel/node_api/molecule_api.rb
+++ app/models/qernel/node_api/molecule_api.rb
@@ -6,6 +6,21 @@ module Qernel
     class MoleculeApi < Base
       dataset_accessors :molecule_input_capacity, :molecule_output_capacity
 
+      # Public: Calculates the number of units, based on the demand and input or output capacity.
+      #
+      # Returns a numeric.
+      def number_of_units
+        return 0.0 if demand.zero?
+
+        if input_capacity&.positive?
+          demand / input_capacity
+        elsif molecule_output_capacity&.positive?
+          (demand - output_of_loss) / molecule_output_capacity
+        else
+          super
+        end
+      end
+
       private
 
       # Energy nodes use full_load_seconds in order to implicitly convert from MJ to MW, due to
---
+++ spec/models/qernel/node_api/molecule_api_spec.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Qernel::NodeApi::MoleculeApi do
+  let(:node) { FactoryBot.build(:node).with({}) }
+  let(:api) { described_class.new(node) }
+
+  let(:demand) { 0.0 }
+  let(:input_capacity) { 0.0 }
+  let(:output_capacity) { 0.0 }
+  let(:output_of_loss) { 0.0 }
+
+  before do
+    node.with(
+      demand: demand,
+      molecule_input_capacity: input_capacity,
+      molecule_output_capacity: output_capacity
+    )
+
+    allow(api).to receive(:output_of_loss).and_return(output_of_loss)
+  end
+
+  # number_of_units
+  # ---------------
+
+  describe 'calculating number_of_units with 100 demand, 20 loss, and output capacity 0.5' do
+    let(:demand) { 100.0 }
+    let(:output_capacity) { 0.5 }
+    let(:output_of_loss) { 20.0 }
+
+    it 'returns 160' do
+      expect(api.number_of_units).to eq(160)
+    end
+  end
+
+  describe 'calculating number_of_units with 0 demand, 0 loss, and output capacity 0.5' do
+    let(:demand) { 0.0 }
+    let(:output_capacity) { 0.5 }
+    let(:output_of_loss) { 0.0 }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+
+  describe 'calculating number_of_units with 10 demand, 0 loss, and output capacity 0.5' do
+    let(:demand) { 10.0 }
+    let(:output_capacity) { 0.5 }
+    let(:output_of_loss) { 0.0 }
+
+    it 'returns 20' do
+      expect(api.number_of_units).to eq(20)
+    end
+  end
+
+  describe 'calculating number_of_units with 10 demand, 0 loss, and output capacity 0' do
+    let(:demand) { 10.0 }
+    let(:output_capacity) { 0.0 }
+    let(:output_of_loss) { 0.0 }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+
+  describe 'calculating number_of_units with 0 demand, 0 loss, and output capacity 0' do
+    let(:demand) { 0.0 }
+    let(:output_capacity) { 0.0 }
+    let(:output_of_loss) { 0.0 }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+
+  describe 'calculating number_of_units with 10 demand, 0 loss, and output capacity nil' do
+    let(:demand) { 0.0 }
+    let(:output_capacity) { nil }
+    let(:output_of_loss) { 0.0 }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+
+  describe 'calculating number_of_units with 100 demand and input capacity 5' do
+    let(:demand) { 100.0 }
+    let(:input_capacity) { 5.0 }
+
+    it 'returns 20' do
+      expect(api.number_of_units).to eq(20)
+    end
+  end
+
+  describe 'calculating number_of_units with 100 demand and input capacity 0' do
+    let(:demand) { 100.0 }
+    let(:input_capacity) { 0.0 }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+
+  describe 'calculating number_of_units with 100 demand and input capacity nil' do
+    let(:demand) { 100.0 }
+    let(:input_capacity) { nil }
+
+    it 'returns 0' do
+      expect(api.number_of_units).to eq(0)
+    end
+  end
+end
