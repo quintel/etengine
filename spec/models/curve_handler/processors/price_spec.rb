51e3da7ccdf1c380af4de34d0c868cf18165538a
Anthony Williams <hi@antw.dev>
2020-12-17 15:02:53 +0000

    Move price curve "table" support to CurveHandler::Processors::Price

--- spec/models/curve_handler/processors/price_spec.rb
+++ spec/models/curve_handler/processors/price_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe CurveHandler::Processors::Price do
       expect(handler.sanitized_curve).to eq(curve.map { |v| v.round(2) })
     end
   end
+
+  describe '.from_string' do
+    let(:handler) { described_class.from_string(input) }
+
+    context 'when given an exported price curve' do
+      let(:input) do
+        "Time,Price (Euros)\n" + ("N/A,1.2\n" * 8760)
+      end
+
+      it 'is valid' do
+        expect(handler).to be_valid
+      end
+
+      it 'sanitizes the curve' do
+        expect(handler.sanitized_curve).to eq([1.2] * 8760)
+      end
+    end
+
+    context 'when given an exported price curve with malformed prices' do
+      let(:input) do
+        "Time,Price (Euros)\n" + ("N/A,1.2a\n" * 8760)
+      end
+
+      it 'is not valid' do
+        expect(handler).not_to be_valid
+      end
+    end
+  end
 end
--- spec/models/curve_handler/processors/shared_examples.rb
+++ spec/models/curve_handler/processors/shared_examples.rb
@@ -183,30 +183,6 @@ RSpec.shared_examples_for 'a non-normalizing CurveHandler processor' do
       end
     end
 
-    context 'when given an exported price curve' do
-      let(:input) do
-        "Time,Price (Euros)\n" + ("N/A,1.2\n" * 8760)
-      end
-
-      it 'is valid' do
-        expect(handler).to be_valid
-      end
-
-      it 'sanitizes the curve' do
-        expect(handler.sanitized_curve).to eq([1.2] * 8760)
-      end
-    end
-
-    context 'when given an exported price curve with malformed prices' do
-      let(:input) do
-        "Time,Price (Euros)\n" + ("N/A,1.2a\n" * 8760)
-      end
-
-      it 'is not valid' do
-        expect(handler).not_to be_valid
-      end
-    end
-
     context 'when given a string with mixed values' do
       let(:input) { "1.2\nnope\n" * 4380 }
 
