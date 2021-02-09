6eb4d215cf9d926e55178ab91b20f91279a7db92
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2021-02-09 10:22:28 +0100

    Migrate hard-coded freight transport modal split for etdataset issue #859

---
+++ db/migrate/20210209084243_fix_inland_shipping_efficiency.rb
@@ -0,0 +1,21 @@
+require 'etengine/scenario_migration'
+
+class FixInlandShippingEfficiency < ActiveRecord::Migration[5.2]
+  include ETEngine::ScenarioMigration
+
+  def up
+    migrate_scenarios do |scenario|
+      if ( scenario.area_code == "nl" &&
+         scenario.user_values["transport_trucks_share"].to_f > 83.2 && scenario.user_values["transport_trucks_share"].to_f < 83.6 &&
+         scenario.user_values["transport_freight_trains_share"].to_f > 5.0 && scenario.user_values["transport_freight_trains_share"].to_f < 5.4 && 
+         scenario.user_values["transport_ships_share"].to_f > 11.2 && scenario.user_values["transport_ships_share"].to_f < 11.6
+         )
+
+         scenario.user_values.delete("transport_trucks_share")
+         scenario.user_values.delete("transport_freight_trains_share")
+         scenario.user_values.delete("transport_ships_share")
+      end
+    end
+  end
+end
+
