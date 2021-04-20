806e596f038a6ed42c31d097dd6a790d119ab042
Nora Schinkel <ncschinkel@gmail.com>
2021-04-20 16:58:17 +0200

    Update Scenario area_codes to not contain `no_highways` or `no_steel` The datasets containing these suffixes will be removed form ETSource. The standard version dataset (that does contain steel or highways) will be used instead.

---
+++ db/migrate/20210420135121_remove_old_datasets_no_steel_no_highways.rb
@@ -0,0 +1,40 @@
+require 'etengine/scenario_migration'
+
+class RemoveOldDatasetsNoSteelNoHighways < ActiveRecord::Migration[5.2]
+  include ETEngine::ScenarioMigration
+
+  RENAME = {
+    hengstdal: :nl
+  }.freeze
+
+  REMOVE_SUFFIX = %w[_no_highways _no_steel]
+
+  def up
+    # Rename datasets
+    RENAME.each do |old_name, new_name|
+      say_with_time "#{old_name} -> #{new_name}" do
+        Scenario.where(area_code: old_name).update_all(area_code: new_name)
+      end
+    end
+
+    # Change all no_highways- and no_steel-dataset scenarios in standard-dataset scenarios
+    Scenario.all.each do |scenario|
+      remove_any_suffix_from_area_code(scenario)
+    end
+  end
+
+  private
+
+  def remove_any_suffix_from_area_code(scenario)
+    REMOVE_SUFFIX.each do |suffix|
+      return if remove_suffix(scenario, suffix)
+    end
+  end
+
+  def remove_suffix(scenario, suffix)
+    return false unless scenario.area_code.ends_with?(suffix)
+
+    scenario.update(area_code: scenario.area_code.delete_suffix(suffix))
+    true
+  end
+end
