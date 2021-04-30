ea4dbab15d84d27932f1a9b9c7b163026293f21e
redekok <roos.dekok@quintel.com>
2021-04-30 15:32:45 +0200

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_135121) do
+ActiveRecord::Schema.define(version: 2021_04_30_132228) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
