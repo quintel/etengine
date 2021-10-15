216ce42e55e8ce80d343656b6684f4ae3865f8e1
Anthony Williams <hi@antw.dev>
2021-10-15 12:05:11 +0100

    Add configuration for deploying with Docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Mail options for Devise.
-  config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_HOST'] }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
