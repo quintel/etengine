1f503de28ba6bf31cc9891c390f5cd6344c25723
Michiel den Haan <michiel.denhaan@quintel.com>
2021-11-30 18:53:59 +0100

    Cap cyclone furnace biomass input to 40%

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_094859) do
+ActiveRecord::Schema.define(version: 2021_11_29_094731) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
@@ -45,12 +45,6 @@ ActiveRecord::Schema.define(version: 2021_10_01_094859) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "carrier"
-  end
-
-  create_table "flexibility_orders", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "scenario_id"
-    t.text "order"
-    t.index ["scenario_id"], name: "index_flexibility_orders_on_scenario_id", unique: true
   end
 
   create_table "gquery_groups", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
