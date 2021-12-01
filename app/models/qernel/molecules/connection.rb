2ad2de427ed06c2a62337eaf125971410f343f45
Anthony Williams <hi@antw.dev>
2021-12-01 14:25:39 +0000

    Support CO2 factor in molecule connection
    Adds support for a factor attribute which returns the recursive factor
    of bio and fossil CO2 which passes through an edge. This may be used as
    part of the molecule graph connection:
    
        - from_energy.conversion.coal = edges: primary_co2_emission_of_bio_and_fossil_without_capture

--- app/models/qernel/molecules/connection.rb
+++ app/models/qernel/molecules/connection.rb
@@ -69,6 +69,13 @@ module Qernel
 
           raise "Invalid molecule conversion attribute for #{slot.carrier.key} carrier " \
                 "on #{slot.node.key} node: #{factor.inspect}"
+        elsif factor.to_s.start_with?('edges:')
+          attribute = factor[6..].strip
+          total = slot.edges.sum(&:demand)
+
+          return slot.edges.sum do |edge|
+            edge.query.public_send(attribute) * (edge.demand / total)
+          end
         end
 
         factor
