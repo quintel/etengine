d62009d660a97cb3d06164ce15828489ec711646
Anthony Williams <hi@antw.dev>
2022-03-02 21:20:26 +0000

    Fix failed title to metadata migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_01_101011) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_162546) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
