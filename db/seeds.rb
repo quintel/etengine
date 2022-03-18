91cb4943087f49fe55f3c08e42c90229715f6dbd
Anthony Williams <hi@antw.dev>
2022-03-18 14:25:25 +0000

    Add a better message when seeding the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,19 @@ Dir[Rails.root.join("db/seed", "*.{yml,csv}").to_s].each do |file|
   Fixtures.create_fixtures("db/seed", File.basename(file, '.*'))
 end
 
-password = SecureRandom.hex[0..8]
+password = SecureRandom.base58(8)
 
 User.create!(
   name:                  'Admin',
-  email:                 'admin@quintel.com',
+  email:                 "admin@example.org",
   password:              password,
   password_confirmation: password,
   role:                  Role.create(id: 1, name: 'admin')
 )
 
-puts "Created admin user admin@quintel.com with password: #{ password }"
+puts <<~MSG
+  +------------------------------------------------------------------------------+
+  |         Created admin user admin@example.org with password: #{password}         |
+  | Please change this password if you're deploying to a production environment! |
+  +------------------------------------------------------------------------------+
+MSG
