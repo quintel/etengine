49ed8ef3e8751de347df8784604b198ea3be371f
marliekeverweij <marlieke.verweij@quintel.com>
2022-05-30 11:23:16 +0200

    Fuel prod migration: change unit to PJ instead of TJ

--- db/migrate/20211029075343_set_fuel_procution_inputs.rb
+++ db/migrate/20211029075343_set_fuel_procution_inputs.rb
@@ -8,15 +8,15 @@ class SetFuelProcutionInputs < ActiveRecord::Migration[5.2]
   NEW_INPUTS = {
     nl: {
       2020 => {
-        'fuel_production_crude_oil' => 28400,
-        'fuel_production_natural_gas' => 552169
+        'fuel_production_crude_oil' => 28.4,
+        'fuel_production_natural_gas' => 552.169
       },
       2030 => {
-        'fuel_production_crude_oil' => 14200,
+        'fuel_production_crude_oil' => 14.2,
         'fuel_production_natural_gas' => 0
       },
       2040 => {
-        'fuel_production_crude_oil' => 7100,
+        'fuel_production_crude_oil' => 7.1,
         'fuel_production_natural_gas' => 0
       },
       2050 => {
@@ -26,50 +26,50 @@ class SetFuelProcutionInputs < ActiveRecord::Migration[5.2]
     },
     de: {
       2020 => {
-        'fuel_production_coal' => 166801.3751,
-        'fuel_production_lignite' => 1392089.981,
-        'fuel_production_natural_gas' => 414279.7162,
-        'fuel_production_crude_oil' => 159480.5452,
-        'fuel_production_uranium_oxide' => 354771.9584
+        'fuel_production_coal' => 166.8013751,
+        'fuel_production_lignite' => 1392.089981,
+        'fuel_production_natural_gas' => 414.2797162,
+        'fuel_production_crude_oil' => 159.4805452,
+        'fuel_production_uranium_oxide' => 354.7719584
       },
       2030 => {
-        'fuel_production_coal' => 128427.2344,
-        'fuel_production_lignite' => 1071827.293,
-        'fuel_production_natural_gas' => 259995.7325,
-        'fuel_production_crude_oil' => 94514.73514,
+        'fuel_production_coal' => 128.4272344,
+        'fuel_production_lignite' => 1071.827293,
+        'fuel_production_natural_gas' => 259.9957325,
+        'fuel_production_crude_oil' => 94.51473514,
         'fuel_production_uranium_oxide' => 0
       },
       2040 => {
-        'fuel_production_coal' => 111625.0786,
-        'fuel_production_lignite' => 931599.9552,
-        'fuel_production_natural_gas' => 115476.9832,
-        'fuel_production_crude_oil' => 56171.68825,
+        'fuel_production_coal' => 111.6250786,
+        'fuel_production_lignite' => 931.5999552,
+        'fuel_production_natural_gas' => 115.4769832,
+        'fuel_production_crude_oil' => 56.17168825,
         'fuel_production_uranium_oxide' => 0
       },
       2050 => {
-        'fuel_production_coal' => 125798.5848,
-        'fuel_production_lignite' => 1049889.124,
-        'fuel_production_natural_gas' => 98803.98629,
+        'fuel_production_coal' => 125.7985848,
+        'fuel_production_lignite' => 1049.889124,
+        'fuel_production_natural_gas' => 98.80398629,
         'fuel_production_crude_oil' => 0,
         'fuel_production_uranium_oxide' => 0
       }
     },
     dk: {
       2020 => {
-        'fuel_production_crude_oil' => 343378.3784,
-        'fuel_production_natural_gas' => 131488.6874
+        'fuel_production_crude_oil' => 343.3783784,
+        'fuel_production_natural_gas' => 131.4886874
       },
       2030 => {
-        'fuel_production_crude_oil' => 96051.24129,
-        'fuel_production_natural_gas' => 54236.30407
+        'fuel_production_crude_oil' => 96.05124129,
+        'fuel_production_natural_gas' => 54.23630407
       },
       2040 => {
-        'fuel_production_crude_oil' => 43016.63202,
-        'fuel_production_natural_gas' => 16508.26572
+        'fuel_production_crude_oil' => 43.01663202,
+        'fuel_production_natural_gas' => 16.50826572
       },
       2050 => {
-        'fuel_production_crude_oil' => 12829.52183,
-        'fuel_production_natural_gas' => 15209.59372
+        'fuel_production_crude_oil' => 12.82952183,
+        'fuel_production_natural_gas' => 15.20959372
       }
     }
   }
