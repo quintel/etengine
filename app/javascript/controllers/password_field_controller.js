3e1d9728ec7a2b2ce0e7ceec1953ceb3b2f2303a
Anthony Williams <hi@antw.dev>
2022-12-06 17:44:25 +0000

    Remove console statement from password controller

--- app/javascript/controllers/password_field_controller.js
+++ app/javascript/controllers/password_field_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = ["input", "showButton", "hideButton"];
 
   connect() {
-    console.log("hello");
     this.element.classList.remove("hidden");
   }
 
