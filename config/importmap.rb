82c16fc211f9e410937721b0746362b78adbf55f
Anthony Williams <hi@antw.dev>
2022-12-12 09:25:00 +0000

    Add "Copy" buttons to staff application template

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'identity', preload: true
+pin 'inspect', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
