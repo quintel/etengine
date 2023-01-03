5b11b7aa5af8e593306c018c1a2ff85bad868538
Anthony Williams <hi@antw.dev>
2023-01-03 15:02:08 +0000

    Ensure loacle is reset to :en in test

--- spec/system/locale_spec.rb
+++ spec/system/locale_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'Locales', type: :system do
     expect(page).to have_css('header button', text: 'Sign out')
 
     visit '/identity/profile?locale=nl'
-    expect(page).to have_css('header button', text: 'Afmelden')
+    expect(page).to have_css('header button', text: 'Uitloggen')
 
     visit '/identity/profile?locale=en'
     expect(page).to have_css('header button', text: 'Sign out')
 
     visit '/identity/profile?locale=de'
     expect(page).to have_css('header button', text: 'Sign out')
+  ensure
+    I18n.locale = :en # rubocop:disable Rails/I18nLocaleAssignment
   end
 end
