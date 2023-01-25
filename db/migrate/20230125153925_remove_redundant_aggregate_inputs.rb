e470fd21644c2719d9b7bb73c7d311328b83bc32
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2023-01-25 20:23:02 +0100

    Remove redundant aggregate inputs from existing scenarios

---
+++ db/migrate/20230125153925_remove_redundant_aggregate_inputs.rb
@@ -0,0 +1,20 @@
+require 'etengine/scenario_migration'
+
+class RemoveRedundantAggregateInputs < ActiveRecord::Migration[7.0]
+  include ETEngine::ScenarioMigration
+
+  REDUNDANT_AGGREGATE_KEYS = %w[
+    industry_useful_demand_for_chemical_aggregated_industry
+    industry_useful_demand_for_chemical_aggregated_industry_electricity_efficiency
+    industry_useful_demand_for_chemical_aggregated_industry_useable_heat_efficiency
+  ].freeze
+
+
+  def up
+    migrate_scenarios(since: Date.new(2021, 1, 1)) do |scenario|
+      REDUNDANT_AGGREGATE_KEYS.each do |key|
+        scenario.user_values.delete(key)
+      end
+    end
+  end
+end
