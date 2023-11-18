24087820b5155fb270b162b60a2eea141afe2976
Thomas <thomas.hassing@quintel.com>
2023-11-18 21:30:13 +0100

    Add unique constraints and improve indices for scenario_users table

--- db/migrate/20231103104416_create_scenario_users.rb
+++ db/migrate/20231103104416_create_scenario_users.rb
@@ -2,15 +2,16 @@ class CreateScenarioUsers < ActiveRecord::Migration[7.0]
   def up
     # Create new table + indices
     create_table :scenario_users do |t|
-      t.integer     :scenario_id, null: false
-      t.integer     :role_id, null: false
-      t.integer     :user_id, default: nil
-      t.string      :user_email, default: nil
+      t.integer :scenario_id, null: false
+      t.integer :role_id, null: false
+      t.integer :user_id, default: nil
+      t.string  :user_email, default: nil
     end
 
-    add_index :scenario_users, [:scenario_id, :user_id], name: 'scenario_users_scenario_id_user_id_idx'
+    add_index :scenario_users, [:scenario_id, :user_id], unique: true,    name: 'scenario_users_scenario_id_user_id_idx'
+    add_index :scenario_users, [:scenario_id, :user_email], unique: true, name: 'scenario_users_scenario_id_user_email_idx'
 
-    # Create ScenarioUser relations for currently existing scenarios
+    # Create ScenarioUser relations for currently existing scenarios, in batches of 1000
     Scenario.where.not(owner_id: nil).in_batches.each do |scenario_batch|
       scenario_users = scenario_batch.pluck(:id, :owner_id)
       scenario_users.map! do |su|
@@ -28,7 +29,7 @@ class CreateScenarioUsers < ActiveRecord::Migration[7.0]
 
   def down
     remove_index :scenario_users, name: 'scenario_users_scenario_id_user_id_idx'
-    remove_index :scenario_users, name: 'scenario_users_user_email_idx'
+    remove_index :scenario_users, name: 'scenario_users_scenario_id_user_email_idx'
     drop_table :scenario_users
   end
 end
