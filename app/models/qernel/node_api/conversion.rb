6ed17d7a6fb7cb9db77f1601e4107da4ece3c338
noracato <ncschinkel@gmail.com>
2024-02-26 17:23:13 +0100

    Per unit methods for captured biogenic emissions for nodes

--- app/models/qernel/node_api/conversion.rb
+++ app/models/qernel/node_api/conversion.rb
@@ -93,6 +93,10 @@ module Qernel
         convert_to(co2_emissions_costs, unit)
       end
 
+      def captured_biogenic_co2_emissions_costs_per(unit)
+        convert_to(captured_biogenic_co2_emissions_costs, unit)
+      end
+
       def variable_operation_and_maintenance_costs_per(unit)
         convert_to(variable_operation_and_maintenance_costs, unit)
       end
