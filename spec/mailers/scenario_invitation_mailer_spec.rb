7c2004acb94d42d080f1a26bdd27648e52619d5c
noracato <ncschinkel@gmail.com>
2024-03-28 19:27:19 +0100

    Update mailer test text after updating email

--- spec/mailers/scenario_invitation_mailer_spec.rb
+++ spec/mailers/scenario_invitation_mailer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ScenarioInvitationMailer, type: :mailer do
   let(:saved_scenario) { { id: 999, title: 'Some saved scenario' } }
 
   context 'when inviting an existing user' do
-    let(:mail) { described_class.invite_user('existing', email, name, saved_scenario) }
+    let(:mail) { described_class.invite_user(email, name, :scenario_collaborator, saved_scenario) }
 
     it 'renders the headers' do
       expect(mail.subject).to eq('Invitation: ETM scenario Some saved scenario')
@@ -15,25 +15,9 @@ RSpec.describe ScenarioInvitationMailer, type: :mailer do
     end
 
     it 'renders the body' do
-      expect(mail.body.raw_source).to include("test@quintel.com just invited you to collaborate on scenario")
+      expect(mail.body.raw_source).to include("Test user has just invited you")
       expect(mail.body.raw_source).to include("/saved_scenarios/999")
-      expect(mail.body.raw_source).to_not include("If you would like to do so, please create an account")
-    end
-  end
-
-  context 'when inviting a new user' do
-    let(:mail) { described_class.invite_user('new', email, name, saved_scenario) }
-
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Invitation: ETM scenario Some saved scenario')
-      expect(mail.to).to eq([email])
-      expect(mail.from).to eq(Mail::Field.parse("From: #{Settings.mailer.from}").addresses)
-    end
-
-    it 'renders the body' do
-      expect(mail.body.raw_source).to include("test@quintel.com just invited you to collaborate on scenario")
-      expect(mail.body.raw_source).to include("/saved_scenarios/999")
-      expect(mail.body.raw_source).to include("If you would like to do so, please create an account")
+      expect(mail.body.raw_source).to include("If you don't have an account yet")
     end
   end
 end
