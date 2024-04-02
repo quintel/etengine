6f0920eb86ddd1442cbd9b4977060a1ed3b47847
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2024-04-02 12:24:20 +0200

    Migrate captured biogenic CO2 price to be 0 in existing scenarios

---
+++ db/migrate/20240402111604_reset_biogenic_co2_price.rb
@@ -0,0 +1,19 @@
+require 'etengine/scenario_migration'
+
+class ResetBiogenicCo2Price < ActiveRecord::Migration[7.0]
+  include ETEngine::ScenarioMigration
+
+  BIOGENIC_CO2_PRICE = 'costs_captured_biogenic_co2'.freeze
+
+  # The default for the new captured biogenic CO2 price is equal to CO2 emissions
+  # price. However to retain the outcomes of older scenarios, the biogenic CO2 price 
+  # is set to 0.
+
+  def up
+    migrate_scenarios do |scenario|
+      next if scenario.user_values[BIOGENIC_CO2_PRICE]
+
+      scenario.user_values[BIOGENIC_CO2_PRICE] = 0.0
+    end
+  end
+end
