532483cd9ecf766a667759510acadb5b4dc1a05d
noracato <ncschinkel@gmail.com>
2024-04-23 11:05:52 +0200

    Prodution part of hydrogen migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_11_112150) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_073401) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
@@ -188,13 +188,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_11_112150) do
     t.index ["scenario_id"], name: "index_scenario_scalings_on_scenario_id", unique: true
   end
 
-  create_table "scenario_users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "scenario_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "scenario_id", null: false
     t.integer "role_id", null: false
     t.integer "user_id"
     t.string "user_email"
     t.index ["scenario_id", "user_email"], name: "scenario_users_scenario_id_user_email_idx", unique: true
     t.index ["scenario_id", "user_id"], name: "scenario_users_scenario_id_user_id_idx", unique: true
+  end
+
+  create_table "scenario_version_tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "scenario_id", null: false
+    t.integer "user_id", null: false
+    t.text "description"
+    t.index ["scenario_id", "user_id"], name: "index_scenario_version_tags_on_scenario_id_and_user_id", unique: true
   end
 
   create_table "scenarios", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
