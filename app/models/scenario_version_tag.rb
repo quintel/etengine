09fb47ca419306df559ac8e677d5c737e7c46e94
noracato <ncschinkel@gmail.com>
2024-05-08 16:10:48 +0200

    Validate scenario tag version description size

--- app/models/scenario_version_tag.rb
+++ app/models/scenario_version_tag.rb
@@ -3,13 +3,14 @@
 # Optional to add version tags to scenarios to display in saved scenario
 class ScenarioVersionTag < ApplicationRecord
   # scenario          the scenario for the tag, cannot be updated
-  # user              the user that created the tag
-  # text
+  # user              the user that created the tag, or last updated the scenario
+  # description
 
   belongs_to :scenario
   belongs_to :user
 
   validate :validate_user_in_scenario_users
+  validates :description, length: { maximum: 60_000 }
 
   def as_json(*)
     super(except: %i[id scenario_id])
