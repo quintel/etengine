248a7de293ffed63ebcd8a3ccc466b537c1555e2
louispt1 <louisparkestalbot1@gmail.com>
2024-07-30 14:22:50 +0200

    Cleaned up logic and implemented some fixes based on testing

--- db/migrate/20240729080631_electric_vehicle_profiles.rb
+++ db/migrate/20240729080631_electric_vehicle_profiles.rb
@@ -5,7 +5,7 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
   include Scenario::Attachments
   include Qernel::Causality::AggregateCurve
 
-  # NEW_PROFILE_MAPPING: A hash mapping new profile keys to their respective user value keys
+  # Mapping old profile keys to their new user value keys
   NEW_PROFILE_MAPPING = {
     electric_vehicle_profile_1_share: 'transport_car_using_electricity_public_charging_share',
     electric_vehicle_profile_2_share: 'transport_car_using_electricity_home_charging_share',
@@ -14,17 +14,8 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
     electric_vehicle_profile_5_share: 'transport_car_using_electricity_custom_profile_charging_share'
   }.freeze
 
-  # EV_PROFILE_SHARE_OLD: An array of old profile share keys
-  EV_PROFILE_SHARE_OLD = %w[
-    electric_vehicle_profile_1_share
-    electric_vehicle_profile_2_share
-    electric_vehicle_profile_3_share
-    electric_vehicle_profile_4_share
-    electric_vehicle_profile_5_share
-  ].freeze
-
-  # EV_PROFILE_OLD: An array of old profile keys
-  EV_PROFILE_OLD = %w[
+  # Old profile keys
+  OLD_PROFILE_KEYS = %w[
     electric_vehicle_profile_1
     electric_vehicle_profile_2
     electric_vehicle_profile_3
@@ -32,71 +23,60 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
     electric_vehicle_profile_5
   ].freeze
 
+  # Old profile share keys
+  OLD_PROFILE_SHARE_KEYS = %w[
+    electric_vehicle_profile_1_share
+    electric_vehicle_profile_2_share
+    electric_vehicle_profile_3_share
+    electric_vehicle_profile_4_share
+    electric_vehicle_profile_5_share
+  ].freeze
+
   def up
     migrate_scenarios do |scenario|
-      # Target the specific test scenario
-      next unless scenario.id == 2574213 # TODO REMOVE SCENRRIOS AFTER TESTING: 2574209, 2574210, 2574211, 2574212
-
-      custom_profile = 0
+      # Only target specific test scenarios (remove this check after testing)
+      next unless scenario.id == 2574220 # TODO: Remove this check after testing
 
       # Ensure the dataset for the scenario's area code exists
-      unless Atlas::Dataset.exists?(scenario.area_code)
-        next
-      end
+      next unless Atlas::Dataset.exists?(scenario.area_code)
 
       dataset = Atlas::Dataset.find(scenario.area_code)
 
       # Check if any profile has an attachment
-      has_attachment = EV_PROFILE_OLD.any? { |profile| scenario.attachment?(profile) }
-
-      # Check if the profile shares are in the default ratio
-      default_ratio = if scenario.user_values.empty?
-        true
-      else
-        EV_PROFILE_SHARE_OLD.each_with_index.all? do |profile_share, index|
-          scenario.user_values.fetch(profile_share, 0) == (index == 0 ? 100 : 0)
-        end
-      end
-
-      if default_ratio
-        if has_attachment
-          # Rename keys and use attached custom curves to make the custom profile
-          rename_keys(scenario, NEW_PROFILE_MAPPING)
-          make_custom_profile(scenario, EV_PROFILE_SHARE_OLD, fetch_attached_curves(scenario, dataset))
-        else
-          # Just rename the keys if there are no attachments
-          rename_keys(scenario, NEW_PROFILE_MAPPING)
-        end
-      else
-        # Rename keys and create a custom profile based on current profiles
+      if OLD_PROFILE_KEYS.any? { |profile| scenario.attachment?(profile) }
         rename_keys(scenario, NEW_PROFILE_MAPPING)
-        make_custom_profile(scenario, EV_PROFILE_SHARE_OLD, fetch_attached_curves(scenario, dataset))
+        make_custom_profile(scenario, fetch_attached_curves(scenario, dataset))
+      else
+        rename_keys(scenario, NEW_PROFILE_MAPPING)
       end
 
       # Save the scenario
       scenario.save
-      raise "Test scenario migration complete. Stopping further execution."
+      raise "Test scenario migration complete. Stopping further execution." # TODO: Remove after testing
     end
   end
 
   private
 
-  # Sets default share distribution and the custom_profile to be profile 5
+  # Sets default share distribution and the custom profile to be profile 5
   def setup_custom_profile(scenario, custom_profile)
-    # Set all values to zero except for transport_car_using_electricity_custom_profile_charging_share
+    # Set all profile shares to zero
     NEW_PROFILE_MAPPING.values.each do |key|
-      scenario.user_values[key] = (key == 'transport_car_using_electricity_custom_profile_charging_share') ? 1 : 0
+      scenario.user_values[key] = 0
     end
 
-    # Set the custom profile to electric vehicle profile 5
+    # Set the custom profile share to 100
+    scenario.user_values['transport_car_using_electricity_custom_profile_charging_share'] = 100
+
+    # Assign the custom profile to electric vehicle profile 5
     scenario.user_values['electric_vehicle_profile_5'] = custom_profile
   end
 
-  # Creates a custom profile based on old shares and current profiles
-  def make_custom_profile(scenario, old_share, current_profiles)
-    mix = old_share.each_with_index.with_object({}) do |(key_share, index), hash|
+  # Creates a custom profile based on current profiles
+  def make_custom_profile(scenario, current_profiles)
+    mix = NEW_PROFILE_MAPPING.values.each_with_index.with_object({}) do |(key_share, index), hash|
       share = scenario.user_values[key_share] || 0
-      profile = current_profiles[EV_PROFILE_OLD[index]]
+      profile = current_profiles[OLD_PROFILE_KEYS[index]]
       hash[profile] = share
     end
 
@@ -110,19 +90,18 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
   # Renames keys in the scenario's user values based on the new mapping
   def rename_keys(scenario, new_mapping)
     new_mapping.each do |old_key, new_key|
-      scenario.user_values[new_key] = scenario.user_values.delete(old_key)
+      scenario.user_values[new_key] = scenario.user_values.delete(old_key) || 0
     end
   end
 
   # Fetches attached curves for the profiles, or defaults if not attached
   def fetch_attached_curves(scenario, dataset)
-    EV_PROFILE_OLD.inject({}) do |attachments_hash, profile|
-      if scenario.attachment?(profile)
-        attachments_hash[profile] = scenario.attachment(profile).file.download
-      else
-        attachments_hash[profile] = dataset.load_profile(profile.to_sym)
-      end
-      attachments_hash
+    OLD_PROFILE_KEYS.each_with_object({}) do |profile, attachments_hash|
+      attachments_hash[profile] = if scenario.attachment?(profile)
+                                    scenario.attachment(profile).file.download
+                                  else
+                                    dataset.load_profile(profile.to_sym)
+                                  end
     end
   end
 end
