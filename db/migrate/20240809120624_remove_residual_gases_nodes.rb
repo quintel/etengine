0111100f025fcc99a306d1c295793af907a44b60
noracato <ncschinkel@gmail.com>
2024-08-12 12:23:32 +0200

    Make zeroes into float for residual gases migration

--- db/migrate/20240809120624_remove_residual_gases_nodes.rb
+++ db/migrate/20240809120624_remove_residual_gases_nodes.rb
@@ -42,7 +42,6 @@ class RemoveResidualGasesNodes < ActiveRecord::Migration[7.0]
     external_coupling_energy_chemical_other_transformation_external_coupling_node_methanol_output_share
   ].freeze
 
-
   def up
     migrate_scenarios do |scenario|
       # If the scenario was a coupled scenario, act on the coupling sliders.
@@ -50,45 +49,36 @@ class RemoveResidualGasesNodes < ActiveRecord::Migration[7.0]
 
       set_conversion_outputs(scenario)
       replace_coupled_shares(scenario)
-
     end
   end
-
 
   # This method replaces a set of "old" input shares in the scenario with a new combined share.
   # It iterates through a predefined set of new keys and corresponding old keys, aggregates the
   # values from the old keys, and assigns the sum to the new key in the scenario's user_values.
-
   def replace_coupled_shares(scenario)
     # Iterate over each pair of new_key and old_keys in the REPLACE_COUPLED_INPUTS hash.
     REPLACE_COUPLED_INPUTS.each do |new_key, old_keys|
-      was_set = false  # Flag to check if any of the old keys were set.
+      # Check if any of the old keys were set.
+      next unless old_keys.any? { |key| scenario.user_values.key?(key) }
 
       # Calculate the new share by summing up the values of the old keys.
-      new_share = old_keys.reduce(0.0) do |sum, old_key|
-        # Check if the old key has a value in the scenario.
-        was_set = true if scenario.user_values.key? old_key
-
+      scenario.user_values[new_key] = old_keys.reduce(0.0) do |sum, old_key|
         # Add the value of the old key to the sum, or 0 if the key does not exist.
-        sum += scenario.user_values.delete(old_key) || 0
+        sum += scenario.user_values.delete(old_key) || 0.0
       end
-
-      # If any of the old keys were set, assign the calculated sum to the new key.
-      scenario.user_values[new_key] = new_share if was_set
     end
   end
-
 
   def set_conversion_outputs(scenario)
     # Calculate the total demand
     total_demand = REPLACE_COUPLED_INPUTS['external_coupling_energy_chemical_other_transformation_external_coupling_node_total_demand'].sum do |old_key|
-      scenario.user_values[old_key] || 0
+      scenario.user_values[old_key] || 0.0
     end
 
     # Iterate over the output mapping and set the output shares
     CONVERSION_OUTPUT_MAPPING.each do |output, input|
-      input_value = scenario.user_values[input] || 0
-      scenario.user_values[output] = total_demand.zero? ? 0 : input_value / total_demand * 100.0
+      input_value = scenario.user_values[input] || 0.0
+      scenario.user_values[output] = total_demand.zero? ? 0.0 : input_value / total_demand * 100.0
     end
 
     # Finish if total_demand is zero
@@ -99,7 +89,5 @@ class RemoveResidualGasesNodes < ActiveRecord::Migration[7.0]
     SET_REMAINING_CONVERSION_SHARES.each do |key|
       scenario.user_values[key] = 0.0
     end
-
   end
-
 end
