82917dd5a6f791cab5daad4d0929bcf733c23e76
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2024-08-22 15:47:57 +0200

    Finish migration for improvement of external coupling of coal gas

--- db/migrate/20240822120921_add_coal_gas_distribution.rb
+++ db/migrate/20240822120921_add_coal_gas_distribution.rb
@@ -7,17 +7,14 @@ class AddCoalGasDistribution < ActiveRecord::Migration[7.0]
 
   def up
     migrate_scenarios do |scenario|
-      next unless scenario.user_values.key?(ENERGY_PRODUCTION_SHARE_KEY || CHEMICAL_FEEDSTOCK_SHARE_KEY)
+      next unless scenario.user_values.key?(ENERGY_PRODUCTION_SHARE_KEY && CHEMICAL_FEEDSTOCK_SHARE_KEY)
 
-      energy_production_share = scenario.user_values[ENERGY_PRODUCTION_SHARE_KEY] || 0.0
-      chemical_feedstock_share = scenario.user_values[CHEMICAL_FEEDSTOCK_SHARE_KEY] || 0.0
-      final_demand_share = 100.0 - chemical_feedstock_share - energy_production_share
+      energy_production_share = scenario.user_values[ENERGY_PRODUCTION_SHARE_KEY]
+      chemical_feedstock_share = scenario.user_values[CHEMICAL_FEEDSTOCK_SHARE_KEY]
 
-      if final_demand_share >= 100.0 || final_demand_share <= 0.01
-        raise "Invalid final demand share: #{final_demand_share}. It must be between 0 and 100."
-      else
-        scenario.user_values[FINAL_DEMAND_SHARE_KEY] = final_demand_share
-      end
+      next unless energy_production_share + chemical_feedstock_share < 100.0
+
+      scenario.user_values[FINAL_DEMAND_SHARE_KEY] = 100.0 - chemical_feedstock_share - energy_production_share
     end
   end
 end
