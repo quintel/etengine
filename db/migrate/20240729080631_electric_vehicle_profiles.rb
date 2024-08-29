f12da91c33b42a8e3bd84a0ce5dfc59a2acab6e6
noracato <ncschinkel@gmail.com>
2024-08-29 12:33:04 +0200

    Small fixes to EV migration

--- db/migrate/20240729080631_electric_vehicle_profiles.rb
+++ db/migrate/20240729080631_electric_vehicle_profiles.rb
@@ -2,18 +2,17 @@ require 'etengine/scenario_migration'
 
 class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
   include ETEngine::ScenarioMigration
-  include Scenario::Attachments
 
   # Mapping old electric vehicle charging share keys to new ones.
   NEW_SHARE_MAPPING = {
-    transport_car_using_electricity_hybrid_charging_share: 'transport_car_using_electricity_public_charging_share',
-    transport_car_using_electricity_home_charging_share: 'transport_car_using_electricity_home_charging_share',
-    transport_car_using_electricity_fast_charging_share: 'transport_car_using_electricity_fast_charging_share',
-    transport_car_using_electricity_smart_charging_share: 'transport_car_using_electricity_work_charging_share',
-    transport_car_using_electricity_regular_charging_share: 'transport_car_using_electricity_custom_profile_charging_share'
+    'transport_car_using_electricity_hybrid_charging_share' => 'transport_car_using_electricity_public_charging_share',
+    'transport_car_using_electricity_home_charging_share' => 'transport_car_using_electricity_home_charging_share',
+    'transport_car_using_electricity_fast_charging_share' => 'transport_car_using_electricity_fast_charging_share',
+    'transport_car_using_electricity_smart_charging_share' => 'transport_car_using_electricity_work_charging_share',
+    'transport_car_using_electricity_regular_charging_share' => 'transport_car_using_electricity_custom_profile_charging_share'
   }.freeze
 
-  # Old profile keys before migration.
+  # Old profile keys before migration. In order of correspondence to NEW_SHARE_MAPPING
   OLD_PROFILE_KEYS = %w[
     electric_vehicle_profile_1
     electric_vehicle_profile_2
@@ -21,18 +20,6 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
     electric_vehicle_profile_4
     electric_vehicle_profile_5
   ].freeze
-
-  # User profile keys that are attached to scenarios.
-  USER_PROFILE_KEYS = %w[
-    electric_vehicle_profile_1_curve
-    electric_vehicle_profile_2_curve
-    electric_vehicle_profile_3_curve
-    electric_vehicle_profile_4_curve
-    electric_vehicle_profile_5_curve
-  ].freeze
-
-  OLD_SHARE_KEYS = NEW_SHARE_MAPPING.keys.map(&:to_s)
-  NEW_SHARE_KEYS = NEW_SHARE_MAPPING.values
 
   # Executes the migration for all scenarios.
   def up
@@ -43,90 +30,78 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
       dataset = Atlas::Dataset.find(scenario.area_code)
 
       # Rename the user values based on the new key mapping.
-      rename_keys(scenario, NEW_SHARE_MAPPING)
+      rename_keys(scenario)
 
-      # Check if the scenario has any attached curves (user profiles).
-      if USER_PROFILE_KEYS.any? { |profile| scenario.attachment?(profile) }
-        # Fetch the current setup of attached curves or dataset profiles.
-        current_setup = fetch_attached_curves(scenario, dataset)
-        # Create and set up a custom profile based on the current setup.
-        make_custom_profile(scenario, current_setup)
+      # If the scenario has any attached curves (user profiles).
+      # Create and set up a custom profile based on the current setup.
+      if OLD_PROFILE_KEYS.any? { |profile| scenario.attachment?("#{profile}_curve") }
+        create_custom_profile(scenario, dataset)
       end
-
-      # Save the scenario and track the migration.
-      scenario.save
-      track_scenario_migration(scenario)
     end
   end
 
   private
 
-  # Sets up a custom profile with a default share distribution and assigns it to profile 5.
-  def setup_custom_profile(scenario, custom_profile)
-    # Reset all new profile shares to zero.
-    NEW_SHARE_KEYS.each { |key| scenario.user_values[key] = 0 }
+  # Renames keys in the scenario's user values based on the provided mapping.
+  def rename_keys(scenario)
+    NEW_SHARE_MAPPING.each do |old_key, new_key|
+      next unless scenario.user_values.key? (old_key)
 
-    # Detach any existing electric vehicle profile curve attachments.
-    USER_PROFILE_KEYS.each do |profile_key|
-      attachment = scenario.attachment(profile_key)
-      CurveHandler::DetachService.call(attachment) if attachment
+      scenario.user_values[new_key] = scenario.user_values.delete(old_key)
     end
-
-    # Set the custom profile share to 100% for 'electric_vehicle_profile_5_curve'.
-    scenario.user_values['transport_car_using_electricity_custom_profile_charging_share'] = 100.0
-    attach_custom_profile(scenario, custom_profile)
   end
 
   # Creates a custom profile based on the current profiles and their shares.
-  def make_custom_profile(scenario, current_setup)
-    mix = {}
-
+  def create_custom_profile(scenario, dataset)
     # Prepare a mix of profiles and their corresponding shares.
-    NEW_SHARE_KEYS.each_with_index do |key, index|
-      share = scenario.user_values[key] || 0.0
-      profile_key = USER_PROFILE_KEYS[index]
-      profile = current_setup[profile_key]
+    mix = NEW_SHARE_MAPPING.values.each_with_index.filter_map do |key, index|
+      next unless scenario.user_values[key]&.positive?
 
-      mix[profile] = share if profile && share.positive?
+      profile = curve_for(scenario, dataset, OLD_PROFILE_KEYS[index])
+
+      next unless profile
+
+      [profile, scenario.user_values[key]]
     end
 
     # Exit if no profiles were found in the mix.
     return if mix.empty?
 
-    # Aggregate the profiles and shares to create a custom profile.
-    custom_profile = curve_aggregator(mix)
-    return if custom_profile.nil?
-
-    # Set up the custom profile in the scenario.
-    setup_custom_profile(scenario, custom_profile)
+    # Use Merit to mix a new custom profile and set it on the scenario.
+    set_custom_profile(
+      scenario,
+      ::Merit::CurveTools.add_curves(
+        mix.map { |curve, share| Merit::Curve.new(curve) * share }
+      )
+    )
   end
 
-  # Renames keys in the scenario's user values based on the provided mapping.
-  def rename_keys(scenario, new_mapping)
-    new_mapping.each do |old_key, new_key|
-      # Transfer values from old keys to new keys, defaulting to 0 if the old key is not present.
-      scenario.user_values[new_key] = scenario.user_values.delete(old_key) || 0.0
+  # Fetches attached curve for the profile or defaults to dataset profile if not attached.
+  def curve_for(scenario, dataset, profile_key)
+    if scenario.attachment?("#{profile_key}_curve")
+      # If the user profile is attached, fetch the custom curve.
+      CustomCurveSerializer.new(scenario.attachment("#{profile_key}_curve")).send(:curve)
+    else
+      # Otherwise, load the default profile from the dataset.
+      dataset.load_profile(profile_key.to_sym)
     end
   end
 
-  # Fetches attached curves for the profiles or defaults to dataset profiles if not attached.
-  def fetch_attached_curves(scenario, dataset)
-    attachments_hash = {}
+  # Sets the custom profile with a 100% share and assigns it to profile 5.
+  def set_custom_profile(scenario, custom_profile)
+    # Set the custom profile share to 100% for the custom profile.
+    NEW_SHARE_MAPPING.values.each { |key| scenario.user_values[key] = 0.0 }
+    scenario.user_values['transport_car_using_electricity_custom_profile_charging_share'] = 100.0
 
-    USER_PROFILE_KEYS.each_with_index do |user_profile_key, index|
-      if scenario.attachment?(user_profile_key)
-        # If the user profile is attached, fetch the custom curve.
-        attachment = scenario.attachment(user_profile_key)
-        custom_curve = CustomCurveSerializer.new(attachment).send(:curve)
-        attachments_hash[user_profile_key] = custom_curve
-      else
-        # Otherwise, load the default profile from the dataset.
-        attachments_hash[user_profile_key] = dataset.load_profile(OLD_PROFILE_KEYS[index].to_sym)
-      end
+    # Detach any existing electric vehicle profile curve attachments.
+    OLD_PROFILE_KEYS.each do |profile_key|
+      attachment = scenario.attachment("#{profile_key}_curve")
+      CurveHandler::DetachService.call(attachment) if attachment
     end
 
-    attachments_hash
+    attach_custom_profile(scenario, custom_profile)
   end
+
 
   # Attaches the custom profile to the scenario as 'electric_vehicle_profile_5_curve'.
   def attach_custom_profile(scenario, custom_profile)
@@ -151,24 +126,5 @@ class ElectricVehicleProfiles < ActiveRecord::Migration[7.0]
     # Close and delete the temporary file.
     temp_file.close
     temp_file.unlink
-  end
-
-  # Aggregates the profiles and shares to create a custom profile.
-  def curve_aggregator(mix)
-    aggregated_curve = nil
-
-    # Iterate over each curve and its associated share in the mix.
-    mix.each do |curve, share|
-      curve_values = curve.to_a
-      scaled_curve = curve_values.map { |value| value * (share / 100.0) }
-
-      aggregated_curve = if aggregated_curve.nil?
-                           scaled_curve
-                         else
-                           aggregated_curve.each_with_index.map { |value, index| value + scaled_curve[index] }
-                         end
-    end
-
-    Merit::Curve.new(aggregated_curve)
   end
 end
