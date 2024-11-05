bf81b629e49450cb9d9ebcbf5d8682ec5b76c887
louispt1 <81332401+louispt1@users.noreply.github.com>
2024-11-05 11:20:30 +0100

    Release 2024.11

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 Sentry.init do |config|
   # Set release version
-  config.release = '2024.10'
+  config.release = '2024.11'
 
   config.dsn = ENV.fetch('SENTRY_DSN', nil)
   config.enabled_environments = %w[production staging]
