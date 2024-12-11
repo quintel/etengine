295ecfdb6416069842c3c2776e742f387b8fcf18
louispt1 <louisparkestalbot1@gmail.com>
2024-12-11 12:03:10 +0100

    Updates to spec

--- spec/controllers/inspect/areas_controller_spec.rb
+++ spec/controllers/inspect/areas_controller_spec.rb
@@ -5,12 +5,12 @@ describe Inspect::AreasController do
   let(:scenario) { FactoryBot.create :scenario }
 
   before do
-    # sign_in(admin)
+    sign_in(admin)
   end
 
   describe "GET show" do
     it "should be successful" do
-      get :show, params: { :api_scenario_id => scenario.id }
+      get :show, params: {:api_scenario_id => scenario.id }
       expect(response).to render_template(:show)
     end
   end
