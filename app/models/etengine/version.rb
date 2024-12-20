767d695e48533b7302f36eec17fdb33039db926f
louispt1 <louisparkestalbot1@gmail.com>
2024-12-20 10:45:48 +0100

    Implemented version dropdown as in ETModel

---
+++ app/models/etengine/version.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module ETEngine
+  module Version
+    # Release tag
+    TAG = 'stable.01'
+
+    # Year of release.
+    MAJOR = 2024
+
+    # Month of release.
+    MINOR = 11
+
+    # Day of release. This is not used in version numbers but is required to build the DATE_VERSION
+    # so that we know the exact date. This should be the day on which the minor version was released,
+    # not incremented for each small change.
+    DAY = 5
+
+    STRING = format('%<major>d.%<minor>02d', major: MAJOR, minor: MINOR)
+    DATE = Date.new(MAJOR, MINOR, DAY)
+
+    def self.prerelease?
+      DATE > Time.now.utc
+    end
+  end
+end
