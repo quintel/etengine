48b8767ff5cec31397950f0d9c94a903c999e63f
louispt1 <louisparkestalbot1@gmail.com>
2025-04-10 11:46:47 +0200

    Serialize active couplings with MessagePack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_10_074412) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_10_084424) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
@@ -123,8 +123,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_10_074412) do
     t.string "source"
     t.text "balanced_values", size: :medium
     t.text "metadata"
-    t.text "active_couplings", size: :medium
+    t.text "active_couplings_old", size: :medium
     t.binary "user_values", size: :long
+    t.binary "active_couplings", size: :long
     t.index ["created_at"], name: "index_scenarios_on_created_at"
   end
 
