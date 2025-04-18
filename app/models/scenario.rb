96ac3c52fd277bab3597efaae2d6bcf8e3b44347
louispt1 <louisparkestalbot1@gmail.com>
2025-04-18 10:28:23 +0200

    Copy and assign active couplings for safe copying

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -14,7 +14,7 @@ class Scenario < ApplicationRecord
   include Scenario::Couplings
 
   serialize  :user_values, type: Hash, coder: MessagePack
-  store      :balanced_values
+  serialize  :balanced_values, type: Hash, coder: MessagePack
   serialize  :active_couplings, type: Array, coder: MessagePack
   store      :metadata, coder: JSON
 
@@ -175,6 +175,7 @@ class Scenario < ApplicationRecord
   def scaled?
     scaler.present? || Area.derived?(area_code)
   end
+
 
   # Public: The year on which the analysis for the scenario's area is based.
   #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_10_122627) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_15_093227) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
@@ -114,17 +114,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_10_122627) do
   create_table "scenarios", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.text "user_values_old", size: :medium
     t.integer "end_year", default: 2040
     t.boolean "keep_compatible", default: false, null: false
     t.boolean "private", default: false, null: false
     t.integer "preset_scenario_id"
     t.string "area_code"
     t.string "source"
-    t.text "balanced_values", size: :medium
+    t.text "balanced_values_old", size: :medium
     t.text "metadata"
     t.text "active_couplings_old", size: :medium
     t.binary "user_values", size: :long
+    t.binary "balanced_values", size: :medium
     t.binary "active_couplings", size: :medium
     t.index ["created_at"], name: "index_scenarios_on_created_at"
   end
