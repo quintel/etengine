e4261b241fb1cc5fd95f86b7e19eb2fa6e953578
louispt1 <81332401+louispt1@users.noreply.github.com>
2025-04-30 11:46:50 +0200

    Use curve_for_storage when attaching curves
    * Fix flh display for capacity profiles in serializer
    
    ---------
    
    Co-authored-by: noracato <ncschinkel@gmail.com>

--- app/serializers/custom_profile_curve_serializer.rb
+++ app/serializers/custom_profile_curve_serializer.rb
@@ -6,11 +6,21 @@ class CustomProfileCurveSerializer < CustomCurveSerializer
   def stats
     attrs = super
 
-    if config.reducer
-      reduced = config.reducer.call(@curve.to_a)
-      attrs[:full_load_hours] = reduced
-    end
+    attrs[:full_load_hours] = reduced if config.reducer
 
     attrs
+  end
+
+  # Internal: If the config has a reducer, we know it's a capacity profile
+  # many of them have already been normalized at this point, so we
+  # have to check the flh input they set
+  #
+  # Returns the full load hours associated to the profile
+  def reduced
+    if config.sets_inputs? && @user_curve.scenario.user_values.key?(config.input_keys.first)
+      @user_curve.scenario.user_values[config.input_keys.first]
+    else
+      config.reducer.call(@curve.to_a)
+    end
   end
 end
