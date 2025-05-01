a29233725c8cae37bda8278d534b95918b7594fc
noracato <ncschinkel@gmail.com>
2025-05-01 11:29:08 +0200

    Adjust migratable scenarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_01_090204) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_01_090210) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
