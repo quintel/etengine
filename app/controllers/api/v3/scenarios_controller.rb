0a702ccc1784f5a561e69e83effd3de601aa9fb7
louispt1 <louisparkestalbot1@gmail.com>
2025-05-21 10:06:18 +0200

    Extracted logic to ScenarioPacker, added multi-spec, added comments, rubocop corrected, sanitized params

--- app/controllers/api/v3/scenarios_controller.rb
+++ app/controllers/api/v3/scenarios_controller.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 module Api
   module V3
     class ScenariosController < BaseController
-
       rescue_from Scenario::YearInterpolator::InterpolationError do |ex|
         render json: { errors: [ex.message] }, status: :bad_request
       end
 
       load_resource except: %i[show create destroy dump]
-      load_and_authorize_resource class: Scenario, only: %i[index show create destroy dump]
+      load_and_authorize_resource class: Scenario, only: %i[index show destroy dump]
 
       before_action only: %i[batch] do
         # Only check that the user can read. We restrict the search in the action.
@@ -121,10 +122,12 @@ module Api
         # Weird ActiveResource bug: the user values attribute is nested inside
         # another user_values hash. Used when generating a scenario with the
         # average values of other scenarios.
-        inputs = params[:scenario][:user_values]["user_values"] rescue nil
-        if inputs
-          params[:scenario][:user_values] = inputs
+        inputs = begin
+                   params[:scenario][:user_values]['user_values']
+        rescue StandardError
+                   nil
         end
+        params[:scenario][:user_values] = inputs if inputs
 
         attrs = Scenario.default_attributes.merge(scenario_params || {})
         parent = nil
@@ -319,7 +322,9 @@ module Api
         if coupling_parameters[:force]
           force_uncouple
         else
-          coupling_parameters[:groups].each { |coupling| @scenario.deactivate_coupling(coupling.to_s) }
+          coupling_parameters[:groups].each do |coupling|
+            @scenario.deactivate_coupling(coupling.to_s)
+          end
           if @scenario.save
             render json: ScenarioSerializer.new(self, @scenario)
           else
@@ -406,9 +411,7 @@ module Api
           attrs[:user_values] = user_vals
         end
 
-        if attrs[:scenario]&.key?(:metadata)
-          attrs[:metadata] = filtered_metadata(attrs[:scenario])
-        end
+        attrs[:metadata] = filtered_metadata(attrs[:scenario]) if attrs[:scenario]&.key?(:metadata)
 
         attrs[:descale] = true if attrs[:descale] == 'true'
 
@@ -499,8 +502,8 @@ module Api
       # scenario which caused the error.
       #
       # Returns the result of the block.
-      def wrap_with_sentry_context
-        ScenarioSentryContext.with_context(@scenario) { yield }
+      def wrap_with_sentry_context(&)
+        ScenarioSentryContext.with_context(@scenario, &)
       end
     end
   end
--- spec/models/scenario_packer/load_spec.rb
+++ spec/models/scenario_packer/load_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe ScenarioPacker::Load, type: :model do
+  subject(:loader) { described_class.new(scenario_data) }
+
   let(:scenario_data) do
     {
       area_code:        'nl',
@@ -24,21 +26,19 @@ RSpec.describe ScenarioPacker::Load, type: :model do
       }
     }.with_indifferent_access
   end
-
-  subject(:loader) { described_class.new(scenario_data) }
   let(:scenario_instance) { loader.instance_variable_get(:@scenario) }
 
   describe '#initialize' do
     it 'initializes a new Scenario with correct attributes' do
       expect(scenario_instance).to be_a(Scenario)
       expect(scenario_instance).to be_new_record
-      expect(scenario_instance.area_code).to eq 'nl'
-      expect(scenario_instance.end_year).to eq 2050
-      expect(scenario_instance.private).to be false
-      expect(scenario_instance.keep_compatible).to be true
+      expect(scenario_instance.area_code).to eq('nl')
+      expect(scenario_instance.end_year).to eq(2050)
+      expect(scenario_instance.private).to be(false)
+      expect(scenario_instance.keep_compatible).to be(true)
       expect(scenario_instance.user_values).to eq('foo' => 1.23)
       expect(scenario_instance.balanced_values).to eq('bar' => 4.56)
-      expect(scenario_instance.active_couplings).to eq []
+      expect(scenario_instance.active_couplings).to eq([])
       expect(scenario_instance.heat_network_orders).to be_empty
       expect { scenario_instance.user_curves }.not_to raise_error
     end
@@ -49,14 +49,14 @@ RSpec.describe ScenarioPacker::Load, type: :model do
 
     it 'builds one HeatNetworkOrder' do
       heat_orders = scenario_instance.heat_network_orders
-      expect(heat_orders.first.temperature).to eq 'ht'
-      expect(heat_orders.first.order).to eq HeatNetworkOrder.default_order
+      expect(heat_orders.first.temperature).to eq('ht')
+      expect(heat_orders.first.order).to eq(HeatNetworkOrder.default_order)
     end
 
     it 'builds one ForecastStorageOrder' do
       forecast_order = scenario_instance.forecast_storage_order
       expect(forecast_order).to be_a(ForecastStorageOrder)
-      expect(forecast_order.order).to eq ForecastStorageOrder.default_order
+      expect(forecast_order.order).to eq(ForecastStorageOrder.default_order)
     end
   end
 
@@ -65,10 +65,10 @@ RSpec.describe ScenarioPacker::Load, type: :model do
 
     it 'builds UserCurve records with correct keys and data' do
       curve_keys = scenario_instance.user_curves.map(&:key)
-      expect(curve_keys).to match_array %w[curve_one curve_two]
+      expect(curve_keys).to match_array(%w[curve_one curve_two])
 
       curve_one = scenario_instance.user_curves.find { |c| c.key == 'curve_one' }
-      expect(curve_one.curve).to eq [0, 1, 2, 3]
+      expect(curve_one.curve).to eq([0, 1, 2, 3])
     end
   end
 
@@ -80,8 +80,8 @@ RSpec.describe ScenarioPacker::Load, type: :model do
 
       expect(result).to be(scenario_instance)
       expect(scenario_instance).to have_received(:save!)
-      expect(scenario_instance.heat_network_orders.size).to eq 1
-      expect(scenario_instance.user_curves.size).to eq 2
+      expect(scenario_instance.heat_network_orders.size).to eq(1)
+      expect(scenario_instance.user_curves.size).to eq(2)
     end
   end
 end
