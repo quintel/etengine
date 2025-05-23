87e8581269e20b7b16060f0c40624bd2e329ead8
mabijkerk <67338510+mabijkerk@users.noreply.github.com>
2025-05-23 08:27:53 +0200

    Merge household appliances efficiency sliders into one (#1568)
    Co-authored-by: Kas Kranenburg <kas.kranenburg@quintel.com>
    Co-authored-by: noracato <ncschinkel@gmail.com>
    Co-authored-by: louispt1 <louisparkestalbot1@gmail.com>

---
+++ db/migrate/20250429122439_merge_household_appliances_efficiency.rb
@@ -0,0 +1,41 @@
+require 'etengine/scenario_migration'
+
+class MergeHouseholdAppliancesEfficiency < ActiveRecord::Migration[7.0]
+  include ETEngine::ScenarioMigration
+
+  NEW_INPUT = 'households_appliances_electricity_efficiency'.freeze
+
+  # Parent shares mapped to corresponding inputs
+  SHARE_MAP = {
+    households_appliances_clothes_dryer_electricity:  'households_appliances_clothes_dryer_electricity_efficiency',
+    households_appliances_computer_media_electricity: 'households_appliances_computer_media_electricity_efficiency',
+    households_appliances_dishwasher_electricity:     'households_appliances_dishwasher_electricity_efficiency',
+    households_appliances_fridge_freezer_electricity: 'households_appliances_fridge_freezer_electricity_efficiency',
+    households_appliances_other_electricity:          'households_appliances_other_electricity_efficiency',
+    households_appliances_television_electricity:     'households_appliances_television_electricity_efficiency',
+    households_appliances_vacuum_cleaner_electricity: 'households_appliances_vacuum_cleaner_electricity_efficiency',
+    households_appliances_washing_machine_electricity:'households_appliances_washing_machine_electricity_efficiency'
+  }.freeze
+
+  def up
+    migrate_scenarios do |scenario|
+      next unless Atlas::Dataset.exists?(scenario.area_code)
+      next unless SHARE_MAP.values.any? { |key| scenario.user_values.key?(key) }
+
+      dataset = Atlas::Dataset.find(scenario.area_code)
+      parent_shares = dataset.shares("energy/residences_final_demand_for_appliances_electricity_parent_share")
+
+      # Calculate weighted avg. Start value for the old sliders was 0.0. If input is not set, it's treated as 0.0.
+      weighted_avg = SHARE_MAP.sum(0.0) do |parent_share_key, input_key|
+        efficiency = scenario.user_values.delete(input_key) || 0.0
+        parent_shares.get(parent_share_key) * (1.0 - efficiency / 100.0)
+      end
+
+      # The weighted average is reconverted into a percentage
+      new_input = (1.0 - weighted_avg) * 100.0
+
+      # The slider values should be checked for their minimum and maximum values
+      scenario.user_values[NEW_INPUT] = [[new_input, 90.0].min, -90.0].max
+    end
+  end
+end
