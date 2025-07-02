477254d101aab7f3a7e0607b2c481f7f3fa36e66
louispt1 <louisparkestalbot1@gmail.com>
2025-07-02 16:27:34 +0200

    Remove unused methods and add a stimulus controller to hide the ids input box when unnecessary

--- app/components/inspect/scenarios/load_modal_component.rb
+++ app/components/inspect/scenarios/load_modal_component.rb
@@ -3,11 +3,6 @@
 module Inspect
   module Scenarios
     class LoadModalComponent < ApplicationComponent
-      option :params
-
-      def form_action
-        load_dump_inspect_scenarios_path
-      end
     end
   end
 end
