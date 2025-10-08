3dd4f3edfad7410da3c4cb94c7d5320bd450e034
louispt1 <louisparkestalbot1@gmail.com>
2025-10-08 15:10:18 +0200

    Update to separate rename and remove methods

--- db/migrate/20251008073517_chemicals_and_synthetic_products.rb
+++ db/migrate/20251008073517_chemicals_and_synthetic_products.rb
@@ -21,27 +21,26 @@ class ChemicalsAndSyntheticProducts < ActiveRecord::Migration[7.1]
   BION_KEY = 'external_coupling_industry_chemical_other_non_energetic_bionaphtha_share'
 
   RENAME = {
-    'capacity_of_energy_production_synthetic_kerosene_dispatchable' =>
-      'capacity_of_energy_production_fischer_tropsch_synthetic_dispatchable',
-    'investment_costs_co2_utilisation' => 'investment_costs_fischer_tropsch',
-    'om_costs_co2_utilisation' =>
-      'om_costs_methanol_synthesis',
-    'om_costs_co2_utilisation' => 'om_costs_fischer_tropsch',
-    'investment_costs_co2_utilisation' =>
-      'investment_costs_methanol_synthesis',
-    'transport_plane_using_kerosene_share' =>
-      'transport_plane_using_kerosene_mix_share',
-    'external_coupling_energy_chemical_other_transformation_external_coupling_node_bio_oil_output_share' =>
-      'external_coupling_energy_chemical_other_transformation_external_coupling_node_bionaphtha_output_share',
-    'external_coupling_energy_chemical_refineries_transformation_external_coupling_node_bio_oil_output_share' =>
-      'external_coupling_energy_chemical_refineries_transformation_external_coupling_node_bionaphtha_output_share',
-    'external_coupling_energy_chemical_fertilizers_transformation_external_coupling_node_bio_oil_output_share' =>
-      'external_coupling_energy_chemical_fertilizers_transformation_external_coupling_node_bionaphtha_output_share',
-    'output_of_energy_production_synthetic_methanol' =>
-      'output_of_energy_production_methanol_synthesis'
+    'capacity_of_energy_production_synthetic_kerosene_dispatchable' => ['capacity_of_energy_production_fischer_tropsch_synthetic_dispatchable'],
+    'investment_costs_co2_utilisation' => ['investment_costs_fischer_tropsch', 'investment_costs_methanol_synthesis'],
+    'om_costs_co2_utilisation' => ['om_costs_methanol_synthesis', 'om_costs_fischer_tropsch'],
+    'transport_plane_using_kerosene_share' => ['transport_plane_using_kerosene_mix_share'],
+    'external_coupling_energy_chemical_other_transformation_external_coupling_node_bio_oil_output_share' => ['external_coupling_energy_chemical_other_transformation_external_coupling_node_bionaphtha_output_share'],
+    'external_coupling_energy_chemical_refineries_transformation_external_coupling_node_bio_oil_output_share' => ['external_coupling_energy_chemical_refineries_transformation_external_coupling_node_bionaphtha_output_share'],
+    'external_coupling_energy_chemical_fertilizers_transformation_external_coupling_node_bio_oil_output_share' => ['external_coupling_energy_chemical_fertilizers_transformation_external_coupling_node_bionaphtha_output_share'],
+    'output_of_energy_production_synthetic_methanol' => ['output_of_energy_production_methanol_synthesis']
   }.freeze
 
-  REMOVE = %w[external_coupling_energy_production_synthetic_methanol_demand external_coupling_energy_production_synthetic_kerosene_demand]
+  REMOVE = %w[external_coupling_energy_production_synthetic_methanol_demand
+              external_coupling_energy_production_synthetic_kerosene_demand
+              capacity_of_energy_production_synthetic_kerosene_dispatchable
+              investment_costs_co2_utilisation
+              om_costs_co2_utilisation
+              transport_plane_using_kerosene_share
+              external_coupling_energy_chemical_other_transformation_external_coupling_node_bio_oil_output_share
+              external_coupling_energy_chemical_refineries_transformation_external_coupling_node_bio_oil_output_share
+              external_coupling_energy_chemical_fertilizers_transformation_external_coupling_node_bio_oil_output_share
+              output_of_energy_production_synthetic_methanol]
 
   def up
     @defaults = JSON.load(File.read(
@@ -52,7 +51,8 @@ class ChemicalsAndSyntheticProducts < ActiveRecord::Migration[7.1]
       migrate_fossil_refinery(scenario)
       migrate_synthetic_kerosene(scenario)
       migrate_ext_coupling_bionaphtha(scenario)
-      rename_and_remove_inputs(scenario)
+      rename_inputs(scenario)
+      remove_inputs(scenario)
     end
   end
 
@@ -79,12 +79,16 @@ class ChemicalsAndSyntheticProducts < ActiveRecord::Migration[7.1]
     scenario.user_values[WP_KEY] = 0.0
   end
 
-  def rename_and_remove_inputs(scenario)
-    RENAME.each do |old_key, new_key|
+  def rename_inputs(scenario)
+    RENAME.each do |old_key, new_keys|
       next unless scenario.user_values.key?(old_key)
-      scenario.user_values[new_key] = scenario.user_values.delete(old_key)
+      Array(new_keys).each do |new_key|
+        scenario.user_values[new_key] = scenario.user_values[old_key]
+      end
     end
+  end
 
+  def remove_inputs(scenario)
     REMOVE.each do |key|
       next unless scenario.user_values.key?(key)
       scenario.user_values.delete(key)
