99d8b4df27b88359ca8791c1c052dbc177119aa1
louispt1 <louisparkestalbot1@gmail.com>
2025-10-09 09:25:41 +0200

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_26_121929) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_08_073518) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", limit: 191, null: false
