27b8a9197a2061c125cb023cb1df332c10735413
Nora Schinkel <ncschinkel@gmail.com>
2025-10-31 10:34:46 +0100

    Create adapter for temperature based power to heat participants (#1647)
    * Create apapter for temperatur based power to heat participants
    
    * Inject calculated availability back in the graph
    
    * Check if availability curve from node is valid in temperature based adapter
    
    * minor syntax fix
    
    * Update comments in temperature based adapter
    
    Co-authored-by: louispt1 <81332401+louispt1@users.noreply.github.com>
    
    * Cutoff temperature above setpoint instead of below
    
    * Derotate the temp curve when calculting availability for p2h
    
    * P2H migration (#1651)
    
    * Migration for p2h technologies with temperature setpoint
    
    * P2H migration for temperature setpoint
    
    * Make syntax clearer for p2h migration
    
    * Update Atlas commit
    
    ---------
    
    Co-authored-by: louispt1 <louisparkestalbot1@gmail.com>
    Co-authored-by: louispt1 <81332401+louispt1@users.noreply.github.com>

---
+++ db/migrate/20251027143649_temperature_setpoint_p2h.rb
@@ -0,0 +1,14 @@
+require 'etengine/scenario_migration'
+
+class TemperatureSetpointP2h < ActiveRecord::Migration[7.1]
+  include ETEngine::ScenarioMigration
+
+  def up
+    migrate_scenarios do |scenario|
+      if scenario.user_values['capacity_of_energy_heat_flexibility_p2h_boiler_electricity']&.positive? ||
+         scenario.user_values['capacity_of_energy_heat_flexibility_p2h_heatpump_electricity']&.positive?
+        scenario.user_values['temperature_cutoff_of_energy_flexibility_p2h'] = 30.0
+      end
+    end
+  end
+end
