90cfcf6df697399bc3f151843d0376b2aa51270e
louispt1 <louisparkestalbot1@gmail.com>
2025-11-07 14:04:34 +0100

    Comment explaining share groups method

--- app/models/scenario_updater/share_groups.rb
+++ app/models/scenario_updater/share_groups.rb
@@ -3,6 +3,9 @@
 class ScenarioUpdater
 
   class ShareGroups
+
+    # This method resolves each incoming key and then processes the share groups associated with those values, so
+    # it only processes the relevant share groups associated with the update being processed.
     def self.each(values)
       group_names = values.keys.filter_map do |key|
         Input.get(key)&.share_group&.presence
