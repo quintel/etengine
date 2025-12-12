68bb34c2e029c99d4dc58d32ea7e38dfbb044cf4
Kyra de Haan <kyra.dehaan@quintel.com>
2025-12-12 10:53:10 +0100

    Rewrite migration

--- db/migrate/20250804081157_update_reformer_capacity.rb
+++ db/migrate/20250804081157_update_reformer_capacity.rb
@@ -1,26 +1,22 @@
 require 'etengine/scenario_migration'
 
-# The full load hours of ammonia reformers and Liquid H2 regasifiers have been updated from 4690 to 7884.
-#
-# This migration scales ammonia reformers and Liquid H2 regasifiers in existing scenarios to account for the change.
-#
-
 class UpdateReformerCapacity < ActiveRecord::Migration[7.0]
   include ETEngine::ScenarioMigration
 
+  KEYS = [
+    'capacity_of_energy_hydrogen_lohc_reformer',
+    'capacity_of_energy_hydrogen_liquid_hydrogen_regasifier'
+  ].freeze
 
-  def do_migrate()
+  SCALE_FACTOR = 4690.0 / 7884.0
+
+  # The full load hours of LOHC reformers and LH2 regasifiers have been updated from 4690 to 7884.
+  # Capacity is corrected to keep hydrogen output the same.
+  def up
     migrate_scenarios do |scenario|
-      if scenario.user_values.key?('capacity_of_energy_hydrogen_lohc_reformer')
-        scenario.user_values['capacity_of_energy_hydrogen_lohc_reformer'] *= (4690.0 / 7884.0)
-      end
-      if scenario.user_values.key?('capacity_of_energy_hydrogen_liquid_hydrogen_regasifier')
-        scenario.user_values['capacity_of_energy_hydrogen_liquid_hydrogen_regasifier'] *= (4690.0 / 7884.0)
+      KEYS.each do |key|
+        scenario.user_values[key] *= SCALE_FACTOR if scenario.user_values.key?(key)
       end
     end
-  end
-
-  def up
-    do_migrate()
   end
 end
