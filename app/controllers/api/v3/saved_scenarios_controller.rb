9a03bf064fb9e72ea8feed0fedd27e6046ccf5b3
louispt1 <81332401+louispt1@users.noreply.github.com>
2025-12-16 15:37:00 +0100

    Authorisation and params corrections for saved scenario create (#1682)
    Handle authorisation for saved scenario create correctly, handle params properly for myetm

--- app/controllers/api/v3/saved_scenarios_controller.rb
+++ app/controllers/api/v3/saved_scenarios_controller.rb
@@ -7,8 +7,12 @@ module Api
         authorize!(:read, Scenario)
       end
 
-      before_action(only: %i[create update]) do
-        authorize!(:write, Scenario)
+      before_action(only: %i[create]) do
+        authorize!(:create, Scenario)
+      end
+
+      before_action(only: %i[update]) do
+        authorize!(:update, Scenario)
       end
 
       def index
@@ -32,7 +36,7 @@ module Api
 
       def create
         CreateSavedScenario.new.call(
-          params: params.permit!.to_h,
+          params: saved_scenario_params.to_h,
           ability: current_ability,
           client: my_etm_client
         ).either(
@@ -46,7 +50,7 @@ module Api
       def update
         UpdateSavedScenario.new.call(
           id: params.require(:id),
-          params: params.permit!.to_h,
+          params: saved_scenario_params.to_h,
           ability: current_ability,
           client: my_etm_client
         ).either(
@@ -110,7 +114,7 @@ module Api
       end
 
       def saved_scenario_params
-        params.permit(:scenario_id, :title, :description, :private)
+        params.require(:saved_scenario).permit(:scenario_id, :title, :description, :private)
       end
 
       def service_error_response(failure)
