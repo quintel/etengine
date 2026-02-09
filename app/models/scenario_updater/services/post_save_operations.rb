25d71cfdc7a6538a6e05cab7a37d90df1a42e9b6
louispt1 <81332401+louispt1@users.noreply.github.com>
2026-02-09 15:18:00 +0100

    Sync Saved Scenario Users (#1703)
    * Add Scenario#sync_users_from_params for SavedScenario role sync
    
    * Permit and authorize saved_scenario_users in scenarios controller
    
    * Wire saved_scenario_users through ScenarioUpdater to PostSaveOperations

--- app/models/scenario_updater/services/post_save_operations.rb
+++ app/models/scenario_updater/services/post_save_operations.rb
@@ -8,8 +8,9 @@ class ScenarioUpdater
 
       TRUTHY_VALUES = Set.new([true, 'true', '1']).freeze
 
-      def call(scenario, set_preset_roles, current_user)
+      def call(scenario, set_preset_roles, saved_scenario_users, current_user)
         copy_preset_roles_if_requested(scenario, set_preset_roles)
+        sync_saved_scenario_users(scenario, saved_scenario_users)
         update_version_tag(scenario, current_user)
 
         Success(scenario)
@@ -20,6 +21,12 @@ class ScenarioUpdater
       def copy_preset_roles_if_requested(scenario, set_preset_roles)
         should_copy = TRUTHY_VALUES.include?(set_preset_roles)
         scenario.copy_preset_roles if should_copy
+      end
+
+      def sync_saved_scenario_users(scenario, saved_scenario_users)
+        return if saved_scenario_users.blank?
+
+        scenario.sync_users_from_params(saved_scenario_users)
       end
 
       def update_version_tag(scenario, current_user)
