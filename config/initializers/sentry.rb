c168a241b6b760baef3b5d7826bfb2f1913192ba
louispt1 <louisparkestalbot1@gmail.com>
2026-02-27 10:03:19 +0100

    Revert "Use otel for tracing with Sentry"
    This reverts commit 5d53925e0f0fe7cf94d682589d52ce1240163bc7.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,9 +7,6 @@ Sentry.init do |config|
   config.dsn = ENV.fetch('SENTRY_DSN', nil)
   config.enabled_environments = %w[production staging]
 
-  # Use OpenTelemetry for instrumentation instead of Sentry's native instrumentation
-  config.instrumenter = :otel
-
   # This sends information such as the query params and request body, and IP address. Sentry is
   # configured to filter out and not store the IP address since we only want the params and body.
   config.send_default_pii = true
