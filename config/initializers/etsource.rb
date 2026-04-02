1e24df833207500713def078f35cf89dd3ab999f
Nora Schinkel <ncschinkel@gmail.com>
2026-04-02 12:48:55 +0200

    Revert remove etsoucre initializer

--- config/initializers/etsource.rb
+++ config/initializers/etsource.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.reloader.to_prepare do
-  # unless defined?(ETSOURCE_DIR)
-  #   # Sort out the ETSource paths
-  #   ETSOURCE_DIR = Etsource::Base.clean_path(Settings.etsource_working_copy)
-  #   ETSOURCE_EXPORT_DIR = Etsource::Base.clean_path(Settings.etsource_export || 'etsource')
+  unless defined?(ETSOURCE_DIR)
+    # Sort out the ETSource paths
+    ETSOURCE_DIR = Etsource::Base.clean_path(Settings.etsource_working_copy)
+    ETSOURCE_EXPORT_DIR = Etsource::Base.clean_path(Settings.etsource_export || 'etsource')
 
-  #   Atlas.data_dir = ETSOURCE_EXPORT_DIR
+    Atlas.data_dir = ETSOURCE_EXPORT_DIR
 
-  #   # Ensure a user in development does not overwrite their ETSource repo by doing an import via the
-  #   # admin UI.
-  #   Settings.etsource_disable_export = true if ETSOURCE_DIR == ETSOURCE_EXPORT_DIR
+    # Ensure a user in development does not overwrite their ETSource repo by doing an import via the
+    # admin UI.
+    Settings.etsource_disable_export = true if ETSOURCE_DIR == ETSOURCE_EXPORT_DIR
 
-  #   # Ensure any old lazy files (from the previous time the application was loaded) are removed.
-  #   Etsource::Dataset::Import.loader.expire_all! if Settings.etsource_lazy_load_dataset
-  # end
+    # Ensure any old lazy files (from the previous time the application was loaded) are removed.
+    Etsource::Dataset::Import.loader.expire_all! if Settings.etsource_lazy_load_dataset
+  end
 
-  # # We store objects in the cache which are no longer valid if the code is reloaded.
-  # Rails.cache.clear
+  # We store objects in the cache which are no longer valid if the code is reloaded.
+  Rails.cache.clear
 end
