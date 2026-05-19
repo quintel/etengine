bb2db8728ef665c085552c2ba41830b9dfc3a3c1
aaccensi <andreu.accensi@gmail.com>
2026-05-19 16:55:47 +0200

    Use NastyCache instead of Rails.cache for MeritOrder and Molecules
    NastyCache stores data in process memory, avoiding a cache round-trip on
    every access, and is automatically cleared on ETSource import.

--- app/models/etsource/molecules.rb
+++ app/models/etsource/molecules.rb
@@ -3,11 +3,6 @@
 module Etsource
   # Loads data relating to the calculation of molecule flows based on the energy graph.
   module Molecules
-    CACHE_KEYS = %w[
-      molecules.from_energy_keys
-      molecules.from_molecules_keys
-    ].freeze
-
     module_function
 
     # Internal: Computes the list of molecule graph nodes which have a molecule_conversion
@@ -17,7 +12,9 @@ module Etsource
     #
     # Returns an Array of Symbols.
     def from_energy_keys
-      molecule_cache['molecules.from_energy_keys']
+      NastyCache.instance.fetch('molecules.from_energy_keys') do
+        Atlas::MoleculeNode.all.select(&:from_energy).map(&:key).sort
+      end
     end
 
     # Internal: Computes the list of molecule graph nodes which have a molecule_conversion
@@ -27,21 +24,8 @@ module Etsource
     #
     # Returns an Array of Symbols.
     def from_molecules_keys
-      molecule_cache['molecules.from_molecules_keys']
-    end
-
-    def reset_cache!
-      @molecule_cache = nil
-    end
-
-    # Batches all Molecules cache reads into one SQL query per request, so
-    # subsequent fetch calls are served from memory instead of the database.
-    def molecule_cache
-      @molecule_cache ||= Rails.cache.fetch_multi(*CACHE_KEYS) do |key|
-        case key
-        when 'molecules.from_energy_keys'    then Atlas::MoleculeNode.all.select(&:from_energy).map(&:key).sort
-        when 'molecules.from_molecules_keys' then Atlas::EnergyNode.all.select(&:from_molecules).map(&:key).sort
-        end
+      NastyCache.instance.fetch('molecules.from_molecules_keys') do
+        Atlas::EnergyNode.all.select(&:from_molecules).map(&:key).sort
       end
     end
   end
