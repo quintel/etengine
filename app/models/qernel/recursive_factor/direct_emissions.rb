be7c0510d0ec36b329c2826d36fea9e9d8cf8ef2
louispt1 <louisparkestalbot1@gmail.com>
2026-05-21 20:49:17 +0200

    Resolve biogenic recursion issue and return to lazy memoization for ccus_captured?

--- app/models/qernel/recursive_factor/direct_emissions.rb
+++ app/models/qernel/recursive_factor/direct_emissions.rb
@@ -33,7 +33,7 @@ module Qernel::RecursiveFactor::DirectEmissions
     # mixed biogenic carriers represent a blend of different biogenic sources.
     if edge.emissions_skip_crude_oil_mix?
       # Continue traversing right to calculate weighted composition
-    elsif edge.carrier.potential_co2_conversion_per_mj || edge.carrier.co2_conversion_per_mj || right_dead_end?
+    elsif !edge.carrier.potential_co2_conversion_per_mj.nil? || !edge.carrier.co2_conversion_per_mj.nil? || right_dead_end?
       edge.carrier.potential_co2_conversion_per_mj || 0.0
     end
 
