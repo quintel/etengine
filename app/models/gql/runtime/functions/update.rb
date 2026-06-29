de700eecca014df1daddbbb943a0ca8cccd63eb1
aaccensi <andreu.accensi@gmail.com>
2026-06-29 10:57:49 +0200

    Fix GQL UPDATE regression for carrier-keyed Hash targets

--- app/models/gql/runtime/functions/update.rb
+++ app/models/gql/runtime/functions/update.rb
@@ -145,7 +145,11 @@ module Gql::Runtime
 
       # @private
       def update_element_with(object, attribute_name, value)
-        object.send("#{attribute_name}=", value)
+        if object.is_a?(Hash) && object.key?(attribute_name.to_sym)
+          object[attribute_name.to_sym] = value
+        else
+          object.send("#{attribute_name}=", value)
+        end
       end
 
       # Private: at the moment only takes care of percentages and absolute numbers.
